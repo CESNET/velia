400687cc87e1920e816ba6610afa6f170733ce52
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-06-09 21:11:20 +0200

    input: DBus semaphore listener
    Adds an input source responsible for listening to changes that happens
    on specified dbus object which provides a semaphore-status health.
    We will use this for monitoring cla-sysrepod health status.
    
    Change-Id: Ib1b82efa181d445ad779f2b181146c9d5ab7722a
    Depends-on: https://cesnet-gerrit-public/c/CzechLight/dependencies/+/2778
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/2778

---
+++ src/inputs/DbusSemaphoreInput.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <memory>
+#include <sdbus-c++/sdbus-c++.h>
+#include "inputs/AbstractInput.h"
+#include "manager/StateManager.h"
+
+namespace velia {
+
+class DbusSemaphoreInput : public AbstractInput {
+public:
+    DbusSemaphoreInput(std::shared_ptr<AbstractManager> mx, sdbus::IConnection& connection, const std::string& bus, const std::string& objectPath, const std::string& propertyName, const std::string propertyInterface);
+    ~DbusSemaphoreInput() override;
+
+private:
+    std::shared_ptr<sdbus::IProxy> m_dbusObjectProxy;
+    std::string m_propertyName;
+    std::string m_propertyInterface;
+    velia::Log m_log;
+};
+}
--- tests/fake.h
+++ tests/fake.h
@@ -7,6 +7,7 @@
 
 #include "trompeloeil_doctest.h"
 #include "inputs/AbstractInput.h"
+#include "manager/AbstractManager.h"
 
 class ManuallyInvokableInput : public velia::AbstractInput {
 public:
@@ -23,5 +24,11 @@ public:
     MAKE_MOCK1(update, void(velia::State));
 };
 
-
 #define REQUIRE_STATE_OUTPUT(STATE) REQUIRE_CALL(*o1, update(velia::State::STATE)).IN_SEQUENCE(seq1)
+
+class FakeManager : public velia::AbstractManager {
+public:
+    MAKE_MOCK2(updateState, void(void*, velia::State), override);
+    MAKE_MOCK2(registerInput, void(void*, velia::State), override);
+    MAKE_MOCK1(unregisterInput, void(void*), override);
+};

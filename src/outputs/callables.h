61edbf22c42a9f38020f2496c2fdd300eb7d72cb
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-06-17 15:32:01 +0200

    output: callback for LEDs
    This commit adds a functionality to light up specific LEDs when
    notified by manager.
    
    Change-Id: Ifecdcbd2d42724bbabeed17753983977a97ccc33

---
+++ src/outputs/callables.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+#pragma once
+
+#include <memory>
+#include "State.h"
+
+namespace velia {
+
+class LedSysfsDriver;
+
+class LedOutputCallback {
+public:
+    LedOutputCallback(std::shared_ptr<LedSysfsDriver> red, std::shared_ptr<LedSysfsDriver> green, std::shared_ptr<LedSysfsDriver> blue);
+    void operator()(State state);
+
+private:
+    std::shared_ptr<LedSysfsDriver> m_redLed, m_greenLed, m_blueLed;
+};
+
+}

a10b7c66fc605854b8d308743bbf35250777ba1c
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-06-22 14:44:32 +0200

    input: Systemd unit monitor input
    Change-Id: Ia22b4cf620a58624357549bd903455fc94c76931

---
+++ src/inputs/DbusSystemdInput.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+#pragma once
+
+#include <memory>
+#include <sdbus-c++/sdbus-c++.h>
+#include <set>
+#include "inputs/AbstractInput.h"
+#include "manager/StateManager.h"
+
+namespace velia {
+
+/**
+ * Register
+ */
+class DbusSystemdInput : public AbstractInput {
+public:
+    DbusSystemdInput(std::shared_ptr<AbstractManager> manager, sdbus::IConnection& connection);
+    DbusSystemdInput(std::shared_ptr<AbstractManager> manager, sdbus::IConnection& connection, const std::string& busname, const std::string& managerObjectPath, const std::string& managerIface, const std::string& unitIface);
+    ~DbusSystemdInput() override;
+
+private:
+    velia::Log m_log;
+
+    std::string m_busName;
+    std::string m_unitIface;
+    std::unique_ptr<sdbus::IProxy> m_proxyManager;
+
+    /** List of registered unit watchers */
+    std::map<sdbus::ObjectPath, std::unique_ptr<sdbus::IProxy>> m_proxyUnits;
+
+    /** List of units in failed state. */
+    std::set<std::string> m_failedUnits;
+
+    void registerSystemdUnit(sdbus::IConnection& connection, const std::string& unitName, const sdbus::ObjectPath& unitObjectPath);
+    void onUnitStateChange(const std::string& name, const std::string& activeState, const std::string& nSubState);
+};
+
+}
---
+++ tests/input-systemd.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+
+#include "trompeloeil_doctest.h"
+#include "dbus-helpers/dbus_systemd_server.h"
+#include "fake.h"
+#include "inputs/DbusSystemdInput.h"
+#include "test_log_setup.h"
+#include "utils/log-init.h"
+#include "utils/log.h"
+
+TEST_CASE("Systemd monitor")
+{
+    TEST_INIT_LOGS;
+    trompeloeil::sequence seq1;
+
+    // Create and setuo separate connections for both client and server. Could be done using a single connection but this way it is more generic
+    auto clientConnection = sdbus::createSessionBusConnection();
+    auto serverConnection = sdbus::createSessionBusConnection("cz.cesnet.systemd1");
+    clientConnection->enterEventLoopAsync();
+    serverConnection->enterEventLoopAsync();
+
+    auto mx = std::make_shared<FakeManager>();
+    auto server = DbusSystemdServer(*serverConnection);
+
+    // i1 gets constructed which means:
+    //  - a registration is performed, along with an updateState call (State::OK)
+    //  - i1's constructor queries the current state and performs updateState
+    REQUIRE_CALL(*mx, registerInput(ANY(void*), velia::State::OK)).LR_SIDE_EFFECT(mx->updateState(_1, _2)).IN_SEQUENCE(seq1);
+    REQUIRE_CALL(*mx, updateState(ANY(void*), velia::State::OK)).IN_SEQUENCE(seq1);
+
+    // create units. Unit2 and Unit3 are in states that we consider failed
+    // therefore the DbusSystemdInput will report ERROR after loading the second unit
+    // FailedUnits: {unit2, unit3} -> ERROR
+    server.createUnit(*serverConnection, "/cz/cesnet/systemd1/unit/unit1", "active", "running");
+    server.createUnit(*serverConnection, "/cz/cesnet/systemd1/unit/unit2", "activating", "auto-restart");
+    server.createUnit(*serverConnection, "/cz/cesnet/systemd1/unit/unit3", "failed", "failed");
+
+    REQUIRE_CALL(*mx, updateState(ANY(void*), velia::State::OK)).IN_SEQUENCE(seq1);
+    REQUIRE_CALL(*mx, updateState(ANY(void*), velia::State::ERROR)).IN_SEQUENCE(seq1);
+    REQUIRE_CALL(*mx, updateState(ANY(void*), velia::State::ERROR)).IN_SEQUENCE(seq1);
+    auto i1 = std::make_shared<velia::DbusSystemdInput>(mx, *clientConnection, "cz.cesnet.systemd1", "/cz/cesnet/systemd1", "cz.cesnet.systemd1.Manager", "cz.cesnet.systemd1.Unit");
+    // i1 now listens for dbus events, we can start the semaphore server
+
+    // FailedUnits: {unit3} -> ERROR
+    REQUIRE_CALL(*mx, updateState(i1.get(), velia::State::ERROR)).IN_SEQUENCE(seq1);
+    server.changeUnitState("/cz/cesnet/systemd1/unit/unit2", "active", "running");
+
+    // FailedUnits: {} -> OK
+    REQUIRE_CALL(*mx, updateState(i1.get(), velia::State::OK)).IN_SEQUENCE(seq1);
+    server.changeUnitState("/cz/cesnet/systemd1/unit/unit3", "active", "running");
+
+    // add new unit with failed/failed, DbusSystemdInput should receive UnitNew signal and monitor this unit too
+    // FailedUnits: {unit4} -> OK
+    REQUIRE_CALL(*mx, updateState(i1.get(), velia::State::ERROR)).IN_SEQUENCE(seq1);
+    server.createUnit(*serverConnection, "/cz/cesnet/systemd1/unit/unit4", "failed", "failed");
+
+    waitForCompletionAndBitMore(seq1);
+
+    // FailedUnits: {} -> OK
+    REQUIRE_CALL(*mx, updateState(i1.get(), velia::State::OK)).IN_SEQUENCE(seq1);
+    server.changeUnitState("/cz/cesnet/systemd1/unit/unit4", "active", "running");
+
+    waitForCompletionAndBitMore(seq1);
+
+    REQUIRE_CALL(*mx, unregisterInput(i1.get())).IN_SEQUENCE(seq1);
+    i1.reset();
+}
+
+TEST_CASE("Real systemd monitor")
+{
+    TEST_INIT_LOGS;
+
+    auto clientConnection = sdbus::createSystemBusConnection();
+
+    auto mx = std::make_shared<velia::StateManager>();
+    auto i1 = std::make_shared<velia::DbusSystemdInput>(mx, *clientConnection);
+
+    clientConnection->enterEventLoop();
+}

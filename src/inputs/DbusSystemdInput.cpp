CESNET/velia

commit 59efe7e6e357d9f25b90771f78be3386a2e6ecda

input: Systemd unit monitor input
Change-Id: Ia22b4cf620a58624357549bd903455fc94c76931

src/inputs/DbusSystemdInput.cpp
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+#include "DbusSystemdInput.h"
+#include "utils/log.h"
+
+namespace velia {
+
+/** @brief Construct the systemd unit watcher for arbitrary dbus object. Mainly for tests. */
+DbusSystemdInput::DbusSystemdInput(std::shared_ptr<AbstractManager> manager, sdbus::IConnection& connection, const std::string& busname, const std::string& managerObjectPath, const std::string& managerIface, const std::string& unitIface)
+    : AbstractInput(std::move(manager))
+    , m_log(spdlog::get("input"))
+    , m_busName(busname)
+    , m_unitIface(unitIface)
+    , m_proxyManager(sdbus::createProxy(connection, m_busName, managerObjectPath))
+{
+    // Subscribe to systemd events. Systemd may not generate signals unless explicitly called
+    m_proxyManager->callMethod("Subscribe").onInterface(managerIface).withArguments().dontExpectReply();
+
+    // Register to a signal introducing new unit
+    m_proxyManager->uponSignal("UnitNew").onInterface(managerIface).call([&](const std::string& unitName, const sdbus::ObjectPath& unitObjectPath) {
+        if (m_proxyUnits.find(unitObjectPath) == m_proxyUnits.end()) {
+            registerSystemdUnit(connection, unitName, unitObjectPath);
+        }
+    });
+    m_proxyManager->finishRegistration();
+
+    // Track all current units
+    std::vector<sdbus::Struct<std::string, std::string, std::string, std::string, std::string, std::string, sdbus::ObjectPath, uint32_t, std::string, sdbus::ObjectPath>> units;
+    m_proxyManager->callMethod("ListUnits").onInterface(managerIface).storeResultsTo(units);
+    for (const auto& unit : units) {
+        registerSystemdUnit(connection, unit.get<0>(), unit.get<6>());
+    }
+}
+
+/** @brief Construct the systemd watcher for well-known systemd paths. */
+DbusSystemdInput::DbusSystemdInput(std::shared_ptr<AbstractManager> manager, sdbus::IConnection& connection)
+    : DbusSystemdInput(std::move(manager), connection, "org.freedesktop.systemd1", "/org/freedesktop/systemd1", "org.freedesktop.systemd1.Manager", "org.freedesktop.systemd1.Unit")
+{
+}
+
+/** @brief Registers a systemd unit by its unit name and unit dbus objectpath. */
+void DbusSystemdInput::registerSystemdUnit(sdbus::IConnection& connection, const std::string& unitName, const sdbus::ObjectPath& unitObjectPath)
+{
+    auto proxyUnit = sdbus::createProxy(connection, m_busName, unitObjectPath);
+    proxyUnit->uponSignal("PropertiesChanged").onInterface("org.freedesktop.DBus.Properties").call([&, unitName](const std::string& iface, const std::map<std::string, sdbus::Variant>& changed, [[maybe_unused]] const std::vector<std::string>& invalidated) {
+        if (iface != m_unitIface) {
+            return;
+        }
+
+        std::string nActiveState, nSubState;
+        if (auto it = changed.find("ActiveState"); it != changed.end()) {
+            nActiveState = it->second.get<std::string>();
+        }
+        if (auto it = changed.find("SubState"); it != changed.end()) {
+            nSubState = it->second.get<std::string>();
+        }
+
+        onUnitStateChange(unitName, nActiveState, nSubState);
+        m_log->trace("Systemd unit '{}' changed state ({} {})", unitName, nActiveState, nSubState);
+    });
+    proxyUnit->finishRegistration();
+
+    // Query the current state of this unit
+    std::string nActiveState = proxyUnit->getProperty("ActiveState").onInterface(m_unitIface);
+    std::string nSubState = proxyUnit->getProperty("SubState").onInterface(m_unitIface);
+    onUnitStateChange(unitName, nActiveState, nSubState);
+
+    m_proxyUnits.emplace(std::make_pair(unitObjectPath, std::move(proxyUnit)));
+    m_log->trace("Registered systemd unit watcher for '{}' ({} {})", unitName, nActiveState, nSubState);
+}
+
+/** @brief Callback for unit state change */
+void DbusSystemdInput::onUnitStateChange(const std::string& name, const std::string& activeState, const std::string& subState)
+{
+    if (activeState == "failed" || (activeState == "activating" && subState == "auto-restart")) {
+        m_failedUnits.insert(name); // this unit is in failed state
+    } else {
+        m_failedUnits.erase(name); // this unit is now OK
+    }
+
+    updateState(m_failedUnits.empty() ? State::OK : State::ERROR);
+}
+
+DbusSystemdInput::~DbusSystemdInput() = default;
+
+}

CMakeLists.txt
@@ -37,6 +37,7 @@ target_link_libraries(velia-utils PUBLIC spdlog::spdlog)
 add_library(velia-state-manager STATIC
     src/inputs/AbstractInput.cpp
     src/inputs/DbusSemaphoreInput.cpp
+    src/inputs/DbusSystemdInput.cpp
     src/manager/AbstractManager.cpp
     src/manager/StateManager.cpp
     src/outputs/LedSysfsDriver.cpp
@@ -70,6 +71,7 @@ if(BUILD_TESTING)
 
     add_library(DbusTesting STATIC
         tests/dbus-helpers/dbus_semaphore_server.cpp
+        tests/dbus-helpers/dbus_systemd_server.cpp
     )
     target_link_libraries(DbusTesting PUBLIC SDBusCpp::sdbus-c++)
 
@@ -85,6 +87,7 @@ if(BUILD_TESTING)
     endfunction()
     velia_test(state-manager velia-state-manager)
     velia_test(input-semaphore velia-state-manager DbusTesting)
+    velia_test(input-systemd velia-state-manager DbusTesting)
     velia_test(output-led velia-state-manager)
 endif()
 

src/inputs/DbusSystemdInput.h
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+#pragma once
+
+#include <memory>
+#include <sdbus-c++/sdbus-c++.h>
+#include <set>
+#include "inputs/AbstractInput.h"
+#include "manager/StateManager.h"
+
+namespace velia {
+
+/**
+ * Register
+ */
+class DbusSystemdInput : public AbstractInput {
+public:
+    DbusSystemdInput(std::shared_ptr<AbstractManager> manager, sdbus::IConnection& connection);
+    DbusSystemdInput(std::shared_ptr<AbstractManager> manager, sdbus::IConnection& connection, const std::string& busname, const std::string& managerObjectPath, const std::string& managerIface, const std::string& unitIface);
+    ~DbusSystemdInput() override;
+
+private:
+    velia::Log m_log;
+
+    std::string m_busName;
+    std::string m_unitIface;
+    std::unique_ptr<sdbus::IProxy> m_proxyManager;
+
+    /** List of registered unit watchers */
+    std::map<sdbus::ObjectPath, std::unique_ptr<sdbus::IProxy>> m_proxyUnits;
+
+    /** List of units in failed state. */
+    std::set<std::string> m_failedUnits;
+
+    void registerSystemdUnit(sdbus::IConnection& connection, const std::string& unitName, const sdbus::ObjectPath& unitObjectPath);
+    void onUnitStateChange(const std::string& name, const std::string& activeState, const std::string& nSubState);
+};
+
+}

tests/dbus-helpers/dbus_systemd_server.cpp
@@ -1,0 +1,75 @@
+#include <sdbus-c++/sdbus-c++.h>
+#include "dbus_systemd_server.h"
+#include "utils/log-init.h"
+
+using namespace std::literals;
+
+namespace {
+static const std::string interfaceUnit = "cz.cesnet.systemd1.Unit";
+static const std::string interfaceManager = "cz.cesnet.systemd1.Manager";
+static const std::string objectPathManager = "/cz/cesnet/systemd1";
+}
+
+/** @brief Create a dbus server on the connection */
+DbusSystemdServer::DbusSystemdServer(sdbus::IConnection& connection)
+    : m_manager(sdbus::createObject(connection, objectPathManager))
+{
+    // create manager object
+    m_manager->registerMethod("Subscribe").onInterface(interfaceManager).implementedAs([]() {}).withNoReply(); // no-op for us
+    m_manager->registerMethod("ListUnits").onInterface(interfaceManager).implementedAs([&]() { return ListUnits(); });
+    m_manager->registerSignal("UnitNew").onInterface(interfaceManager).withParameters<std::string, sdbus::ObjectPath>();
+    m_manager->finishRegistration();
+}
+
+/** @brief Creates a unit inside the test server. Registers dbus object and emits UnitNew signal. **/
+void DbusSystemdServer::createUnit(sdbus::IConnection& connection, const sdbus::ObjectPath& objPath, const std::string& activeState, const std::string& subState)
+{
+    m_units.insert(std::make_pair(objPath, Unit(sdbus::createObject(connection, objPath), activeState, subState)));
+
+    Unit& unit = m_units.at(objPath);
+
+    unit.m_object->registerProperty("ActiveState").onInterface(interfaceUnit).withGetter([&unit]() {
+        return unit.m_activeState;
+    });
+    unit.m_object->registerProperty("SubState").onInterface(interfaceUnit).withGetter([&unit]() {
+        return unit.m_subState;
+    });
+    unit.m_object->finishRegistration();
+
+    m_manager->emitSignal("UnitNew").onInterface(interfaceManager).withArguments(std::string(objPath), objPath);
+}
+
+/**
+ * @brief Implementation of ListUnit method.
+ *
+ * The unit is represented as a (objectPath, activeState, subState) triplet only. Nothing more is required now.
+ * Therefore, the not interesting properties are unused.
+ * In real systemd, there are more properties (see ListUnits method in https://www.freedesktop.org/wiki/Software/systemd/dbus/)
+ */
+std::vector<DbusSystemdServer::UnitStruct> DbusSystemdServer::ListUnits()
+{
+    std::vector<UnitStruct> res;
+
+    for (const auto& [name, unit] : m_units) {
+        res.push_back(UnitStruct(name, ""s, ""s, ""s, ""s, ""s, unit.m_object->getObjectPath(), 0, ""s, "/dummy"s));
+    }
+
+    return res;
+}
+
+/** @brief Changes unit state of the unit identified by object path (@p objPath) */
+void DbusSystemdServer::changeUnitState(const sdbus::ObjectPath& objPath, const std::string& activeState, const std::string& subState)
+{
+    if (auto it = m_units.find(objPath); it != m_units.end()) {
+        it->second.m_activeState = activeState;
+        it->second.m_subState = subState;
+        it->second.m_object->emitPropertiesChangedSignal(interfaceUnit, {"ActiveState", "SubState"});
+    }
+}
+
+DbusSystemdServer::Unit::Unit(std::unique_ptr<sdbus::IObject> object, std::string activeState, std::string subState)
+    : m_object(std::move(object))
+    , m_activeState(std::move(activeState))
+    , m_subState(std::move(subState))
+{
+}

tests/dbus-helpers/dbus_systemd_server.h
@@ -1,0 +1,31 @@
+#pragma once
+
+#include <memory>
+#include <mutex>
+#include <sdbus-c++/sdbus-c++.h>
+#include <string>
+
+/** @brief Mimics the systemd dbus behaviour */
+class DbusSystemdServer {
+public:
+    using UnitStruct = sdbus::Struct<std::string, std::string, std::string, std::string, std::string, std::string, sdbus::ObjectPath, uint32_t, std::string, sdbus::ObjectPath>;
+
+    explicit DbusSystemdServer(sdbus::IConnection& connection);
+
+    void createUnit(sdbus::IConnection& connection, const sdbus::ObjectPath& objPath, const std::string& activeState, const std::string& subState);
+    void changeUnitState(const sdbus::ObjectPath& objPath, const std::string& activeState, const std::string& subState);
+
+private:
+    struct Unit {
+        Unit(std::unique_ptr<sdbus::IObject> object, std::string activeState, std::string subState);
+
+        std::unique_ptr<sdbus::IObject> m_object;
+        std::string m_activeState;
+        std::string m_subState;
+    };
+
+    std::unique_ptr<sdbus::IObject> m_manager;
+    std::map<sdbus::ObjectPath, Unit> m_units;
+
+    std::vector<UnitStruct> ListUnits();
+};

tests/input-systemd.cpp
@@ -1,0 +1,86 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+
+#include "trompeloeil_doctest.h"
+#include "dbus-helpers/dbus_systemd_server.h"
+#include "fake.h"
+#include "inputs/DbusSystemdInput.h"
+#include "test_log_setup.h"
+#include "utils/log-init.h"
+#include "utils/log.h"
+
+TEST_CASE("Systemd monitor")
+{
+    TEST_INIT_LOGS;
+    trompeloeil::sequence seq1;
+
+    // Create and setuo separate connections for both client and server. Could be done using a single connection but this way it is more generic
+    auto clientConnection = sdbus::createSessionBusConnection();
+    auto serverConnection = sdbus::createSessionBusConnection("cz.cesnet.systemd1");
+    clientConnection->enterEventLoopAsync();
+    serverConnection->enterEventLoopAsync();
+
+    auto mx = std::make_shared<FakeManager>();
+    auto server = DbusSystemdServer(*serverConnection);
+
+    // i1 gets constructed which means:
+    //  - a registration is performed, along with an updateState call (State::OK)
+    //  - i1's constructor queries the current state and performs updateState
+    REQUIRE_CALL(*mx, registerInput(ANY(void*), velia::State::OK)).LR_SIDE_EFFECT(mx->updateState(_1, _2)).IN_SEQUENCE(seq1);
+    REQUIRE_CALL(*mx, updateState(ANY(void*), velia::State::OK)).IN_SEQUENCE(seq1);
+
+    // create units. Unit2 and Unit3 are in states that we consider failed
+    // therefore the DbusSystemdInput will report ERROR after loading the second unit
+    // FailedUnits: {unit2, unit3} -> ERROR
+    server.createUnit(*serverConnection, "/cz/cesnet/systemd1/unit/unit1", "active", "running");
+    server.createUnit(*serverConnection, "/cz/cesnet/systemd1/unit/unit2", "activating", "auto-restart");
+    server.createUnit(*serverConnection, "/cz/cesnet/systemd1/unit/unit3", "failed", "failed");
+
+    REQUIRE_CALL(*mx, updateState(ANY(void*), velia::State::OK)).IN_SEQUENCE(seq1);
+    REQUIRE_CALL(*mx, updateState(ANY(void*), velia::State::ERROR)).IN_SEQUENCE(seq1);
+    REQUIRE_CALL(*mx, updateState(ANY(void*), velia::State::ERROR)).IN_SEQUENCE(seq1);
+    auto i1 = std::make_shared<velia::DbusSystemdInput>(mx, *clientConnection, "cz.cesnet.systemd1", "/cz/cesnet/systemd1", "cz.cesnet.systemd1.Manager", "cz.cesnet.systemd1.Unit");
+    // i1 now listens for dbus events, we can start the semaphore server
+
+    // FailedUnits: {unit3} -> ERROR
+    REQUIRE_CALL(*mx, updateState(i1.get(), velia::State::ERROR)).IN_SEQUENCE(seq1);
+    server.changeUnitState("/cz/cesnet/systemd1/unit/unit2", "active", "running");
+
+    // FailedUnits: {} -> OK
+    REQUIRE_CALL(*mx, updateState(i1.get(), velia::State::OK)).IN_SEQUENCE(seq1);
+    server.changeUnitState("/cz/cesnet/systemd1/unit/unit3", "active", "running");
+
+    // add new unit with failed/failed, DbusSystemdInput should receive UnitNew signal and monitor this unit too
+    // FailedUnits: {unit4} -> OK
+    REQUIRE_CALL(*mx, updateState(i1.get(), velia::State::ERROR)).IN_SEQUENCE(seq1);
+    server.createUnit(*serverConnection, "/cz/cesnet/systemd1/unit/unit4", "failed", "failed");
+
+    waitForCompletionAndBitMore(seq1);
+
+    // FailedUnits: {} -> OK
+    REQUIRE_CALL(*mx, updateState(i1.get(), velia::State::OK)).IN_SEQUENCE(seq1);
+    server.changeUnitState("/cz/cesnet/systemd1/unit/unit4", "active", "running");
+
+    waitForCompletionAndBitMore(seq1);
+
+    REQUIRE_CALL(*mx, unregisterInput(i1.get())).IN_SEQUENCE(seq1);
+    i1.reset();
+}
+
+#if 0
+TEST_CASE("Real systemd monitor")
+{
+    TEST_INIT_LOGS;
+
+    auto clientConnection = sdbus::createSystemBusConnection();
+
+    auto mx = std::make_shared<velia::StateManager>();
+    auto i1 = std::make_shared<velia::DbusSystemdInput>(mx, *clientConnection);
+
+    clientConnection->enterEventLoop();
+}
+#endif

commit 392af76543232b864fd654be8d5ce9a7dda0e3a7

input: workaround for compilation on g++8.3
For some reason sdbus::ObjectPath is missing some constructors and therefore std::map::emplace does not work.
I'm extracting this to a separate commit so it can be easily reverted
later.

Fixes: 59efe7e6 (input: Systemd unit monitor input)
Change-Id: Ieb6bf1c6a5f012490ad53060ebc8ff90fb6c1b83

src/inputs/DbusSystemdInput.cpp
@@ -72,7 +72,8 @@ void DbusSystemdInput::registerSystemdUnit(sdbus::IConnection& connection, const
     std::string nSubState = proxyUnit->getProperty("SubState").onInterface(m_unitIface);
     onUnitStateChange(unitName, nActiveState, nSubState);
 
-    m_proxyUnits.emplace(std::make_pair(unitObjectPath, std::move(proxyUnit)));
+    // m_proxyUnits.emplace(unitObjectPath, std::move(proxyUnit)); // TODO doesn't compile on buildroot's g++8.3
+    m_proxyUnits[static_cast<std::string>(unitObjectPath)] = std::move(proxyUnit);
     m_log->trace("Registered systemd unit watcher for '{}' ({} {})", unitName, nActiveState, nSubState);
 }
 
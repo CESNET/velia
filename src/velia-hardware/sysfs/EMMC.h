4a794abcc7ef7f1df0aca8e4ea45a1423f81101b
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-11-05 15:44:00 +0100

    HardwareState: EMMC data reader
    Ported EMMC driver from
    cla-sysrepo@19163e50ab062a8b5b75f754b30c22fdbd62b03a.
    
    In cla-sysrepo, these drivers provided a property-based API, i.e., you
    asked for a specific property (i.e., a filename) and the contents of the
    file was returned.
    
    In velia, we do not need this one-by-one property access. We provide all
    attributes with a single call.
    
    Change-Id: Icdb1129e84c2c7d8db50e192caa7c6ddce1441d7

---
+++ src/velia-hardware/sysfs/EMMC.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+
+#pragma once
+
+#include <filesystem>
+#include <map>
+#include "utils/log-fwd.h"
+
+namespace velia::hardware::sysfs {
+
+class EMMC {
+public:
+    explicit EMMC(std::filesystem::path hwmonDir);
+    ~EMMC();
+
+    std::map<std::string, std::string> attributes() const;
+
+private:
+    velia::Log m_log;
+
+    /** @brief path to the emmc sysfs directory */
+    std::filesystem::path m_root;
+};
+}

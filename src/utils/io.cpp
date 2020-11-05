e864e7a101e36416e7079a35f9735c62c6fe0e19
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-11-05 15:44:00 +0100

    HardwareState: EMMC data reader
    Ported EMMC driver from
    cla-sysrepo@19163e50ab062a8b5b75f754b30c22fdbd62b03a.
    
    In cla-sysrepo, these drivers provided a property-based API, i.e., you
    asked for a specific property (i.e., a filename) and the contents of the
    file was returned.
    
    In velia, we do not need this one-by-one property access. We provide all
    attributes with a single call.
    
    Change-Id: Icdb1129e84c2c7d8db50e192caa7c6ddce1441d7

---
+++ src/utils/io.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+
+#include <fstream>
+#include "io.h"
+
+namespace velia::utils {
+
+namespace impl {
+
+std::ifstream openStream(const std::filesystem::path& path)
+{
+    std::ifstream ifs(path);
+    if (!ifs.is_open())
+        throw std::invalid_argument("File '" + std::string(path) + "' does not exist.");
+    return ifs;
+}
+}
+
+/** @brief Reads a string from a file */
+std::string readFileString(const std::filesystem::path& path)
+{
+    std::ifstream ifs(impl::openStream(path));
+    std::string res;
+
+    if (ifs >> res) {
+        return res;
+    }
+
+    throw std::domain_error("Could not read '" + std::string(path) + "'.");
+}
+
+/** @brief Reads @p valuesCount 32bit values from a file */
+std::vector<uint32_t> readFileWords(const std::filesystem::path& path, int valuesCount)
+{
+    std::ifstream ifs(impl::openStream(path));
+    std::vector<uint32_t> bytes; // no interesting data are wider than 32 bits (for now)
+
+    uint32_t byte;
+    ifs >> std::hex;
+    while (valuesCount-- && ifs >> byte) {
+        bytes.push_back(byte);
+    }
+
+    if (!ifs) {
+        throw std::domain_error("Could not read hex data from '" + std::string(path) + "'.");
+    }
+
+    return bytes;
+}
+
+}
---
+++ tests/pretty_printers.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#pragma once
+
+#include <doctest/doctest.h>
+#include <map>
+#include <sstream>
+#include <trompeloeil.hpp>
+
+
+namespace doctest {
+
+template <>
+struct StringMaker<std::map<std::string, std::string>> {
+    static String convert(const std::map<std::string, std::string>& map)
+    {
+        std::ostringstream os;
+        os << "{" << std::endl;
+        for (const auto& [key, value] : map) {
+            os << "  \"" << key << "\": \"" << value << "\"," << std::endl;
+        }
+        os << "}";
+        return os.str().c_str();
+    }
+};
+
+}

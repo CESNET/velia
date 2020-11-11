41d428b7b32aeed352f579080369e48f19fa7a0d
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-11-11 15:59:03 +0100

    wip hwstate
    port from cla-sysrepo
    add yang models
    
    Change-Id: I49111977309672c6e8318672dc516c5c421beb96

---
+++ src/velia-hardware/sysrepo/OpsCallback.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+
+#pragma once
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <optional>
+#include <sysrepo-cpp/Session.hpp>
+#include "HardwareState.h"
+
+namespace velia::hardware::sysrepo {
+
+class OpsCallback {
+public:
+    explicit OpsCallback(std::shared_ptr<HardwareState> driver);
+    int operator()(std::shared_ptr<::sysrepo::Session> session, const char* module_name, const char* path, const char* request_xpath, uint32_t request_id, std::shared_ptr<libyang::Data_Node>& parent);
+
+private:
+    std::shared_ptr<HardwareState> m_hwState;
+    uint64_t m_lastRequestId;
+};
+
+}

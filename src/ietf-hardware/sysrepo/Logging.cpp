98ad18de51c44385e878bafe09ac429890267803
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-11-13 15:39:55 +0100

    HardwareState: Add Sysrepo operational data callback
    Implement Sysrepo operational data callback class.
    
    Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/br2-external/+/3279
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/br2-external/+/3279
    Change-Id: I6987555dfc3541f6433e1892c62f7c4638aef4dd

---
+++ src/ietf-hardware/sysrepo/Logging.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+extern "C" {
+#include <sysrepo.h>
+}
+
+#include "Logging.h"
+#include "utils/log.h"
+
+extern "C" {
+/** @short Propagate sysrepo events to spdlog */
+static void spdlog_sr_log_cb(sr_log_level_t level, const char* message)
+{
+    // Thread safety note: this is, as far as I know, thread safe:
+    // - the static initialization itself is OK
+    // - all loggers which we instantiate are thread-safe
+    // - std::shared_ptr::operator-> is const, and all const members of that class are documented to be thread-safe
+    static auto log = spdlog::get("sysrepo");
+    assert(log);
+    switch (level) {
+    case SR_LL_NONE:
+    case SR_LL_ERR:
+        log->error(message);
+        break;
+    case SR_LL_WRN:
+        log->warn(message);
+        break;
+    case SR_LL_INF:
+        log->info(message);
+        break;
+    case SR_LL_DBG:
+        log->debug(message);
+        break;
+    }
+}
+}
+
+namespace velia::ietf_hardware::sysrepo {
+
+/** @short Setup sysrepo log forwarding
+You must call cla::utils::initLogs prior to this function.
+*/
+void initLogs()
+{
+    sr_log_set_cb(spdlog_sr_log_cb);
+}
+}

f20f1999483d88fc17875e5fb3ba0c3d7b3c30a0
Tomáš Pecka <Tomas.Pecka@cesnet.cz>
2021-01-11 10:03:14 +0100

    RAUC: Query RAUC slot status via D-Bus
    We are going to provide some of the RAUC features via sysrepo (i.e.,
    query the current slot status, install software update, ...).
    
    This commit adds support for reading slot status from RAUC D-Bus API.
    
    [1] https://rauc.readthedocs.io/en/v1.4/using.html#using-the-d-bus-api
    
    Change-Id: I5d3a6b703e7f5dad3a3c3b432392816a84d884d2

---
+++ tests/dbus-helpers/dbus_rauc_server.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory>
+#include <sdbus-c++/sdbus-c++.h>
+#include <string>
+#include "ietf-system/RAUC.h"
+
+/** @brief Mimics the RAUC DBus behaviour */
+class DBusRAUCServer {
+public:
+    explicit DBusRAUCServer(sdbus::IConnection& connection, std::string primarySlot, const std::map<std::string, velia::ietf_system::RAUC::SlotProperties>& status);
+
+private:
+    using DBusSlotStatus = sdbus::Struct<std::string, std::map<std::string, sdbus::Variant>>;
+
+    std::unique_ptr<sdbus::IObject> m_manager;
+    std::string m_primarySlot;
+    std::vector<DBusSlotStatus> m_status;
+};

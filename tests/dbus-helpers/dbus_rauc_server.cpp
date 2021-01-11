c54f96167ca473ea211da24ea5a2b388f39d7b31
Tomáš Pecka <Tomas.Pecka@cesnet.cz>
2021-01-11 10:03:14 +0100

    RAUC: Query RAUC slot status via D-Bus
    We are going to implement some parts of ietf-system in the next
    commits, starting with announcing system version through sysrepo.
    
    The system version can be read through RAUC D-Bus API [1] or via reading
    /etc/os-release file. We choose the RAUC API as we are going to need it
    anyway soon for the sysrepo-powered system updates.
    
    This commit adds support for reading slot status from RAUC D-Bus API.
    
    [1] https://rauc.readthedocs.io/en/v1.4/using.html#using-the-d-bus-api
    
    Change-Id: I5d3a6b703e7f5dad3a3c3b432392816a84d884d2

---
+++ tests/dbus-helpers/dbus_rauc_server.cpp
@@ -0,0 +1,34 @@
+#include <sdbus-c++/sdbus-c++.h>
+#include "dbus_rauc_server.h"
+#include "utils/log-init.h"
+
+using namespace std::literals;
+
+namespace {
+const std::string interfaceManager = "de.pengutronix.rauc.Installer";
+const std::string objectPathManager = "/";
+}
+
+/** @brief Create a dbus server on the connection */
+DBusRAUCServer::DBusRAUCServer(sdbus::IConnection& connection, std::string primarySlot, const std::map<std::string, velia::ietf_system::RAUC::SlotProperties>& status)
+    : m_manager(sdbus::createObject(connection, objectPathManager))
+    , m_primarySlot(std::move(primarySlot))
+{
+    for (const auto& [slotName, slotStatus] : status) {
+        std::map<std::string, sdbus::Variant> m;
+        for (auto it = slotStatus.begin(); it != slotStatus.end(); ++it) {
+            // NOTE: I wanted a for-range loop over the map with structured binding [key, val] but this did not compile with clang++.
+            // According to http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/p0588r1.html:
+            //  "If a lambda-expression [...] captures a structured binding (explicitly or implicitly), the program is ill-formed."
+
+            const auto& k = it->first;
+            std::visit([&k, &m](auto&& arg) { m.insert(std::make_pair(k, sdbus::Variant(arg))); }, it->second);
+        }
+        m_status.emplace_back(slotName, m);
+    }
+
+    // create manager object
+    m_manager->registerMethod("GetSlotStatus").onInterface(interfaceManager).implementedAs([this]() { return m_status; });
+    m_manager->registerMethod("GetPrimary").onInterface(interfaceManager).implementedAs([this]() { return m_primarySlot; });
+    m_manager->finishRegistration();
+}
---
+++ tests/system_rauc.cpp
@@ -0,0 +1,103 @@
+#include "trompeloeil_doctest.h"
+#include "dbus-helpers/dbus_rauc_server.h"
+#include "ietf-system/RAUC.h"
+#include "pretty_printers.h"
+#include "test_log_setup.h"
+
+using namespace std::chrono_literals;
+
+TEST_CASE("Fetch RAUC data over DBus")
+{
+    using namespace std::literals::chrono_literals;
+
+    TEST_INIT_LOGS;
+    trompeloeil::sequence seq1;
+
+    // setup separate connections for both client and server. Can be done using one connection only but this way it is more generic
+    auto serverConnection = sdbus::createSessionBusConnection("de.pengutronix.rauc");
+    auto clientConnection = sdbus::createSessionBusConnection();
+
+    // enter client and servers event loops
+    clientConnection->enterEventLoopAsync();
+    serverConnection->enterEventLoopAsync();
+
+    std::string primarySlot;
+    std::map<std::string, velia::ietf_system::RAUC::SlotProperties> status;
+
+    SECTION("real data")
+    {
+        primarySlot = "rootfs.1";
+        status = {
+            {"rootfs.1", {
+                             {"activated.count", uint32_t {39}},
+                             {"activated.timestamp", "2021-01-13T17:20:18Z"},
+                             {"bootname", "B"},
+                             {"boot-status", "good"},
+                             {"bundle.compatible", "czechlight-clearfog"},
+                             {"bundle.version", "v4-103-g34d2f48"},
+                             {"class", "rootfs"},
+                             {"device", "/dev/mmcblk0p3"},
+                             {"installed.count", uint32_t {39}},
+                             {"installed.timestamp", "2021-01-13T17:20:15Z"},
+                             {"mountpoint", "/"},
+                             {"sha256", "07b30d065c7aad64d2006ce99fd339c929d3ca97b666fca4584b9ef726469fc4"},
+                             {"size", uint64_t {45601892}},
+                             {"state", "booted"},
+                             {"status", "ok"},
+                             {"type", "ext4"},
+                         }},
+            {"rootfs.0", {
+                             {"activated.count", uint32_t {41}},
+                             {"activated.timestamp", "2021-01-13T17:15:54Z"},
+                             {"bootname", "A"},
+                             {"boot-status", "bad"},
+                             {"bundle.compatible", "czechlight-clearfog"},
+                             {"bundle.version", "v4-104-ge80fcd4"},
+                             {"class", "rootfs"},
+                             {"device", "/dev/mmcblk0p1"},
+                             {"installed.count", uint32_t {41}},
+                             {"installed.timestamp", "2021-01-13T17:15:50Z"},
+                             {"sha256", "6d81e8f341edd17c127811f7347c7e23d18c2fc25c0bdc29ac56999cc9c25629"},
+                             {"size", uint64_t {45647664}},
+                             {"state", "inactive"},
+                             {"status", "ok"},
+                             {"type", "ext4"},
+                         }},
+            {"cfg.1", {
+                          {"bundle.compatible", "czechlight-clearfog"},
+                          {"bundle.version", "v4-103-g34d2f48"},
+                          {"class", "cfg"},
+                          {"device", "/dev/mmcblk0p4"},
+                          {"installed.count", uint32_t {39}},
+                          {"installed.timestamp", "2021-01-13T17:20:18Z"},
+                          {"mountpoint", "/cfg"},
+                          {"parent", "rootfs.1"},
+                          {"sha256", "5ca1b6c461fc194055d52b181f57c63dc1d34c19d041f6395e6f6abc039692bb"},
+                          {"size", uint64_t {108}},
+                          {"state", "active"},
+                          {"status", "ok"},
+                          {"type", "ext4"},
+                      }},
+            {"cfg.0", {
+                          {"bundle.compatible", "czechlight-clearfog"},
+                          {"bundle.version", "v4-104-ge80fcd4"},
+                          {"class", "cfg"},
+                          {"device", "/dev/mmcblk0p2"},
+                          {"installed.count", uint32_t {41}},
+                          {"installed.timestamp", "2021-01-13T17:15:54Z"},
+                          {"parent", "rootfs.0"},
+                          {"sha256", "5ca1b6c461fc194055d52b181f57c63dc1d34c19d041f6395e6f6abc039692bb"},
+                          {"size", uint64_t {108}},
+                          {"state", "inactive"},
+                          {"status", "ok"},
+                          {"type", "ext4"},
+                      }},
+        };
+    }
+
+    auto server = DBusRAUCServer(*serverConnection, primarySlot, status);
+    auto rauc = std::make_shared<velia::ietf_system::RAUC>(*clientConnection);
+
+    REQUIRE(rauc->primarySlot() == primarySlot);
+    REQUIRE(rauc->slotStatus() == status);
+}

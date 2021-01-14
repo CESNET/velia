90d98b54c8cfc5dcf87c48d93cd0762df0f7d836
Tomáš Pecka <Tomas.Pecka@cesnet.cz>
2021-01-14 22:45:10 +0100

    rauc: Install bundle
    Implement installation of a new bundle via RAUC D-Bus API [1].
    
    The D-Bus interface is asynchronous, i.e., after the InstallBundle
    method is invoked, it returns immediately and status of the installation
    is provided to the caller (or anyone) via property changes (see [2]).
    After the installation is completed, a D-Bus signal Completed is
    invoked.
    
    In order to capture these notifications (property changes and signals)
    we introduce two callback functions that needs to be passed to the
    RAUC::install method. The first callback is invoked when installation
    progresses and the second one is invoked when the completion signal is
    processed. Caller can be notified about the installation progress
    using those functions.
    
    [1] https://rauc.readthedocs.io/en/v1.4/using.html#using-the-d-bus-api
    [2] https://rauc.readthedocs.io/en/v1.4/using.html#installing-a-bundle
    
    Change-Id: Id8114edc3a271b2fa40b7bcb1bd46b776c3584e1

--- src/system/RAUC.cpp
+++ src/system/RAUC.cpp
@@ -1,10 +1,12 @@
 /*
  * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
  *
- * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
  *
  */
 #include "RAUC.h"
+
+#include <utility>
 #include "utils/log.h"
 
 namespace {
@@ -38,6 +40,36 @@ RAUC::RAUC(sdbus::IConnection& connection)
     : m_dbusObjectProxy(sdbus::createProxy(connection, BUS, OBJPATH))
     , m_log(spdlog::get("system"))
 {
+    m_dbusObjectProxy->uponSignal("Completed").onInterface(INTERFACE).call([this](int32_t returnValue) {
+        std::string lastError = m_dbusObjectProxy->getProperty("LastError").onInterface(INTERFACE);
+        m_log->warn("InstallBundle completed. Return value {}, last error: {}", returnValue, lastError);
+
+        std::lock_guard<std::mutex> lck(m_mtx);
+        m_installNotifier->m_completedCallback(returnValue, lastError);
+    });
+
+    m_dbusObjectProxy->uponSignal("PropertiesChanged").onInterface("org.freedesktop.DBus.Properties").call([this](const std::string& iface, const std::map<std::string, sdbus::Variant>& changed, [[maybe_unused]] const std::vector<std::string>& invalidated) {
+        if (iface != INTERFACE) {
+            return;
+        }
+
+        if (auto itProgress = changed.find("Progress"); itProgress != changed.end()) {
+            // https://rauc.readthedocs.io/en/v1.4/using.html#sec-processing-progress
+
+            auto progress = itProgress->second.get<sdbus::Struct<int32_t, std::string, int32_t>>();
+
+            int32_t percentage = progress.get<0>();
+            std::string message = progress.get<1>();
+            int32_t depth = progress.get<2>();
+
+            m_log->debug("InstallBundle progress changed: {} {} {}", percentage, message, depth);
+
+            std::lock_guard<std::mutex> lck(m_mtx);
+            m_installNotifier->m_progressCallback(percentage, message, depth);
+        }
+    });
+
+    m_dbusObjectProxy->finishRegistration();
 }
 
 /** @brief Get current primary slot.
@@ -77,4 +109,30 @@ std::map<std::string, RAUC::SlotProperties> RAUC::slotStatus() const
     return res;
 }
 
+/** @brief Install new bundle.
+ *
+ * RAUC's DBus InstallBundle method wrapper.
+ * This method is non-blocking. The status of the installation progress is announced via DBus properties (LastError, Progress)
+ * and after the installation finishes, the Completed signal is triggered.
+ * (see https://rauc.readthedocs.io/en/v1.4/reference.html#gdbus-method-de-pengutronix-rauc-installer-installbundle)
+ */
+void RAUC::install(const std::string& source, std::shared_ptr<InstallNotifier> installNotifier)
+{
+    // we don't want to overwrite installed installNotifier when installation in progress
+    if (std::string operation = m_dbusObjectProxy->getProperty("Operation").onInterface(INTERFACE); operation == "idle") {
+        {
+            std::lock_guard<std::mutex> lck(m_mtx);
+            m_installNotifier = std::move(installNotifier);
+        }
+        m_dbusObjectProxy->callMethod("InstallBundle").onInterface(INTERFACE).withArguments(source); // this will result in sdbus::Error if another operation in progress
+    } else {
+        throw std::logic_error("Another RAUC operation in progress");
+    }
+}
+
+RAUC::InstallNotifier::InstallNotifier(std::function<void(int32_t, std::string, int32_t)> progress, std::function<void(int32_t, std::string)> completed)
+    : m_progressCallback(std::move(progress))
+    , m_completedCallback(std::move(completed))
+{
+}
 }

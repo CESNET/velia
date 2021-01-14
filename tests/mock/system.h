9cc0094f77e53fd4fc627de5052bb254466d872a
Tomáš Pecka <Tomas.Pecka@cesnet.cz>
2021-01-14 22:45:10 +0100

    system: implement Install bundle in RAUC
    Implement installation of a new bundle via RAUC D-Bus API [1].
    
    The D-Bus interface is asynchronous, i.e., after the InstallBundle
    method is invoked, it returns immediately and status of the installation
    is provided to the caller (or anyone) via property changes (see [2]).
    After the installation is completed, a D-Bus signal Completed is
    invoked.
    
    In order to capture these notifications (property changes and signals)
    we introduce three callback functions that needs to be passed to the
    RAUC constructor. These callbacks notify the user of this class in the
    following situations:
    - operation changes (RAUC can perform only one operation at once, so the
      user can see when the RAUC is busy or idle),
    - installation progresses, and
    - installation is completed.
    
    [1] https://rauc.readthedocs.io/en/v1.4/using.html#using-the-d-bus-api
    [2] https://rauc.readthedocs.io/en/v1.4/using.html#installing-a-bundle
    
    Change-Id: Id8114edc3a271b2fa40b7bcb1bd46b776c3584e1

---
+++ tests/mock/system.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+*/
+
+#include "trompeloeil_doctest.h"
+#include "system/RAUC.h"
+
+struct FakeRAUCInstallCb {
+public:
+    MAKE_CONST_MOCK2(completedCallback, void(int32_t, const std::string&));
+    MAKE_CONST_MOCK1(operationCallback, void(const std::string&));
+    MAKE_CONST_MOCK2(progressCallback, void(int32_t, const std::string&));
+};
+
+#define FAKE_RAUC_OPERATION(OP) REQUIRE_CALL(fakeRaucInstallCb, operationCallback(OP)).IN_SEQUENCE(seq1)
+#define FAKE_RAUC_PROGRESS(PERCENT, MSG) REQUIRE_CALL(fakeRaucInstallCb, progressCallback(PERCENT, MSG)).IN_SEQUENCE(seq1)
+#define FAKE_RAUC_COMPLETED(RETVAL, LASTERROR) REQUIRE_CALL(fakeRaucInstallCb, completedCallback(RETVAL, LASTERROR)).IN_SEQUENCE(seq1)

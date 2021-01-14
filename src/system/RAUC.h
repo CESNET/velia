4aaa2e0011b01240b6181477af27b916974a61d7
Tomáš Pecka <Tomas.Pecka@cesnet.cz>
2021-01-14 22:45:10 +0100

    rauc: Install bundle
    Implement installation of a new bundle via RAUC D-Bus API [1].
    
    The D-Bus interface is asynchronous, i.e., after the InstallBundle
    method is invoked, it returns immediately and status of the installation
    is provided to the caller (or anyone) via property changes (see [2]).
    After the installation is completed, a D-Bus signal Completed is
    invoked.
    
    In order to capture these notifications (property changes and signals)
    we introduce two callback functions that needs to be passed to the
    RAUC::install method. The first callback is invoked when installation
    progresses and the second one is invoked when the completion signal is
    processed. Caller can be notified about the installation progress
    using those functions.
    
    [1] https://rauc.readthedocs.io/en/v1.4/using.html#using-the-d-bus-api
    [2] https://rauc.readthedocs.io/en/v1.4/using.html#installing-a-bundle
    
    Change-Id: Id8114edc3a271b2fa40b7bcb1bd46b776c3584e1

--- src/system/RAUC.h
+++ src/system/RAUC.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <map>
+#include <mutex>
 #include <sdbus-c++/sdbus-c++.h>
 #include <string>
 #include <variant>
@@ -18,14 +19,23 @@ namespace velia::system {
 class RAUC {
 public:
     using SlotProperties = std::map<std::string, std::variant<std::string, uint64_t, uint32_t>>;
+    struct InstallNotifier {
+        InstallNotifier(std::function<void(int32_t, std::string, int32_t)> progress, std::function<void(int32_t, std::string)> completed);
+
+        std::function<void(int32_t, std::string, int32_t)> m_progressCallback;
+        std::function<void(int32_t, std::string)> m_completedCallback;
+    };
 
     explicit RAUC(sdbus::IConnection& connection);
     std::string primarySlot() const;
     std::map<std::string, SlotProperties> slotStatus() const;
+    void install(const std::string& source, std::shared_ptr<InstallNotifier> cb);
 
 private:
     std::shared_ptr<sdbus::IProxy> m_dbusObjectProxy;
+    std::shared_ptr<InstallNotifier> m_installNotifier;
     velia::Log m_log;
+    std::mutex m_mtx;
 };
 
 }

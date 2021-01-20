d3427182951c19fe7720fe3b0f1b4be9596ed586
Tomáš Pecka <Tomas.Pecka@cesnet.cz>
2021-01-20 15:12:00 +0100

    sysrepo: RAUC install
    Implement invoking rauc install via sysrepo.
    RAUC h
    
    Change-Id: I4d19a2d3a18e9301ba967c534ef404894e8e01f7

--- src/system/Sysrepo.h
+++ src/system/Sysrepo.h
@@ -8,16 +8,20 @@
 
 #include <filesystem>
 #include <sysrepo-cpp/Session.hpp>
+#include "RAUC.h"
 #include "utils/log-fwd.h"
 
 namespace velia::system {
 
 class Sysrepo {
 public:
-    explicit Sysrepo(std::shared_ptr<::sysrepo::Session> srSession, const std::filesystem::path& osRelease);
+    explicit Sysrepo(std::shared_ptr<::sysrepo::Connection> srConn, const std::filesystem::path& osRelease, std::shared_ptr<RAUC> rauc);
 
 private:
+    std::shared_ptr<::sysrepo::Connection> m_srConn;
     std::shared_ptr<::sysrepo::Session> m_srSession;
+    std::shared_ptr<::sysrepo::Subscribe> m_srSubscribe;
+    std::shared_ptr<RAUC> m_rauc;
     velia::Log m_log;
 };
 }

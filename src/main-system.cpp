d6d78ddab682bf3b258e91ed631dc2de6a3db9ce
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2021-01-23 20:55:51 +0100

    veliad-system: sysrepo daemon for managing system
    This patch factors out sysrepo system management from velia. We are
    going to implement more system stuff (firmware updates, hostname changes,
    etc.) and it would be probably too much for a single daemon which aim
    should have been only the system health monitoring and LED manipulation.
    
    Change-Id: I9c0aa01f48f71bc439405f0533541964d328ac51

--- src/main-system.cpp
+++ src/main-system.cpp
@@ -1,16 +1,12 @@
 #include <csignal>
 #include <docopt.h>
-#include <future>
 #include <sdbus-c++/sdbus-c++.h>
 #include <spdlog/sinks/ansicolor_sink.h>
 #include <spdlog/spdlog.h>
 #include <sysrepo-cpp/Session.hpp>
-#include "Factory.h"
+#include <thread>
 #include "VELIA_VERSION.h"
-#include "health/inputs/DbusSystemdInput.h"
-#include "health/manager/StateManager.h"
-#include "health/outputs/callables.h"
-#include "ietf-hardware/sysrepo/Sysrepo.h"
+#include "system/Sysrepo.h"
 #include "utils/exceptions.h"
 #include "utils/journal.h"
 #include "utils/log-init.h"
@@ -33,30 +29,24 @@ spdlog::level::level_enum parseLogLevel(const std::string& name, const docopt::v
 }
 
 static const char usage[] =
-    R"(Monitor system health status.
+    R"(Sysrepo-powered system management.
 
 Usage:
-  veliad
-    [--appliance=<Model>]
+  veliad-system
     [--log-level=<Level>]
-    [--health-log-level=<Level>]
     [--sysrepo-log-level=<Level>]
-    [--hardware-log-level=<Level>]
-    [--systemd-ignore-unit=<Unit>]...
-  veliad (-h | --help)
-  veliad --version
+    [--system-log-level=<Level>]
+  veliad-system (-h | --help)
+  veliad-system --version
 
 Options:
   -h --help                         Show this screen.
   --version                         Show version.
-  --appliance=<Model>               Initialize IETF Hardware and outputs for specific appliance.
   --log-level=<N>                   Log level for everything [default: 3]
                                     (0 -> critical, 1 -> error, 2 -> warning, 3 -> info,
                                     4 -> debug, 5 -> trace)
-  --health-log-level=<N>            Log level for the health monitoring [default: 3]
   --sysrepo-log-level=<N>           Log level for the sysrepo library [default: 3]
-  --hardware-log-level=<N>          Log level for the hardware drivers [default: 3]
-  --systemd-ignore-unit=<Unit>      Ignore state of systemd's unit in systemd state tracker. Can be specified multiple times.
+  --system-log-level=<N>            Log level for the system stuff [default: 3]
 )";
 
 std::unique_ptr<sdbus::IConnection> g_dbusConnection;
@@ -70,7 +60,7 @@ int main(int argc, char* argv[])
         loggingSink = std::make_shared<spdlog::sinks::ansicolor_stderr_sink_mt>();
     }
 
-    auto args = docopt::docopt(usage, {argv + 1, argv + argc}, true, "veliad " VELIA_VERSION, true);
+    auto args = docopt::docopt(usage, {argv + 1, argv + argc}, true, "veliad-system " VELIA_VERSION, true);
 
     velia::utils::initLogs(loggingSink);
     spdlog::set_level(spdlog::level::info);
@@ -79,7 +69,6 @@ int main(int argc, char* argv[])
 
     try {
         spdlog::set_level(parseLogLevel("Generic", args["--log-level"]));
-        spdlog::get("hardware")->set_level(parseLogLevel("Hardware loggers", args["--hardware-log-level"]));
         spdlog::get("sysrepo")->set_level(parseLogLevel("Sysrepo library", args["--sysrepo-log-level"]));
 
         spdlog::get("main")->debug("Opening DBus connection");
@@ -88,19 +77,10 @@ int main(int argc, char* argv[])
         spdlog::get("main")->debug("Opening Sysrepo connection");
         auto srConn = std::make_shared<sysrepo::Connection>();
         auto srSess = std::make_shared<sysrepo::Session>(srConn);
-        auto srSubscription = std::make_shared<sysrepo::Subscribe>(srSess);
 
-        // initialize ietf-hardware
-        spdlog::get("main")->debug("Initializing IETFHardware module");
-        std::shared_ptr<velia::ietf_hardware::IETFHardware> ietfHardware;
-        if (const auto& appliance = args["--appliance"]) {
-            ietfHardware = velia::ietf_hardware::create(appliance.asString());
-        } else {
-            ietfHardware = std::make_shared<velia::ietf_hardware::IETFHardware>();
-        }
-
-        spdlog::get("main")->debug("Initializing Sysrepo ietf-hardware callback");
-        auto sysrepoIETFHardware = velia::ietf_hardware::sysrepo::Sysrepo(srSubscription, ietfHardware);
+        // initialize ietf-system
+        spdlog::get("main")->debug("Initializing Sysrepo for system models");
+        auto sysrepoIETFSystem = velia::system::Sysrepo(srSess, "/etc/os-release");
 
         // Gracefully leave dbus event loop on SIGTERM
         struct sigaction sigact;
@@ -111,27 +91,6 @@ int main(int argc, char* argv[])
 
         spdlog::get("main")->debug("Starting DBus event loop");
         eventLoop = std::thread([] { g_dbusConnection->enterEventLoop(); });
-
-        // health
-        auto manager = std::make_shared<velia::health::StateManager>();
-
-        // output configuration
-        spdlog::get("main")->debug("Initializing LED drivers");
-        if (const auto& appliance = args["--appliance"]) {
-            manager->m_outputSignal.connect(velia::health::createOutput(appliance.asString()));
-        }
-
-        spdlog::get("main")->debug("All outputs initialized.");
-
-        // input configuration
-        std::set<std::string> ignoredUnits(args["--systemd-ignore-unit"].asStringList().begin(), args["--systemd-ignore-unit"].asStringList().end());
-        spdlog::get("main")->debug("Starting DBus systemd watcher");
-        if (!ignoredUnits.empty()) {
-            spdlog::get("main")->debug("Systemd input will ignore changes of the following units: {}", args["--systemd-ignore-unit"]);
-        }
-        auto inputSystemdDbus = std::make_shared<velia::health::DbusSystemdInput>(manager, ignoredUnits, *g_dbusConnection);
-
-        spdlog::get("main")->debug("All inputs initialized.");
 
         eventLoop.join();
 

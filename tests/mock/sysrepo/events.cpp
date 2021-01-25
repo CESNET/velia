a18702d10bd0f3727d17a6fe751c140ce335a6b2
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2021-01-25 18:07:23 +0100

    tests: Sysrepo event watcher
    Add a helper class EventWatcher that catches Sysrepo events, including
    Sysrepo notifications. The next patch will implement notifications
    in czechlight-system model implementation and this code will be used for
    testing that the notifications are invoked.
    
    The EventWatcher class is reused from cla-sysrepo. The current
    implementation is copied from commit
    1fb9138c10aee6c6f67d30522c14696d3d01b8be.
    
    Change-Id: I3ee0a14a9d1198f2669cb1dd97594c4d864d5a43

---
+++ tests/mock/sysrepo/events.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#include "events.h"
+
+namespace {
+std::string sr_ev_notif_type_to_string(const sr_ev_notif_type_t notif_type)
+{
+    switch (notif_type) {
+    case SR_EV_NOTIF_REALTIME:
+        return "SR_EV_NOTIF_REALTIME";
+    case SR_EV_NOTIF_REPLAY:
+        return "SR_EV_NOTIF_REPLAY";
+    case SR_EV_NOTIF_REPLAY_COMPLETE:
+        return "SR_EV_NOTIF_REPLAY_COMPLETE";
+    case SR_EV_NOTIF_STOP:
+        return "SR_EV_NOTIF_STOP";
+    default:
+        return "[unknown event type]";
+    }
+}
+}
+
+EventWatcher::~EventWatcher()
+{
+}
+
+void EventWatcher::operator()(
+    [[maybe_unused]] ::sysrepo::S_Session session,
+    const sr_ev_notif_type_t notif_type,
+    const char* xpath,
+    const ::sysrepo::S_Vals vals,
+    time_t timestamp)
+{
+    Event e;
+    e.xPath = xpath;
+    e.received = std::chrono::steady_clock::now();
+
+    auto log = spdlog::get("main");
+    log->info("SR event {} {} {}", sr_ev_notif_type_to_string(notif_type), timestamp, xpath);
+
+    for (size_t i = 0; i < vals->val_cnt(); ++i) {
+        const auto& v = vals->val(i);
+        auto s = v->val_to_string();
+        log->debug(" {}: {}", v->xpath(), s);
+        e.data[v->xpath()] = s;
+    }
+
+    std::lock_guard<std::mutex> lock(*mutex);
+    events->push_back(e);
+}
+
+std::vector<EventWatcher::Event>::size_type EventWatcher::count() const
+{
+    std::lock_guard<std::mutex> lock(*mutex);
+    return events->size();
+}
+
+EventWatcher::Event EventWatcher::peek(const std::vector<EventWatcher::Event>::size_type index) const
+{
+    std::lock_guard<std::mutex> lock(*mutex);
+    return (*events)[index];
+}
---
+++ tests/mock/sysrepo/events.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#pragma once
+
+#include <chrono>
+#include <mutex>
+#include <sysrepo-cpp/Session.hpp>
+#include "test_log_setup.h"
+
+class EventWatcher {
+public:
+    ~EventWatcher();
+    void operator()(::sysrepo::S_Session session, const sr_ev_notif_type_t notif_type, const char* xpath, const ::sysrepo::S_Vals vals, time_t timestamp);
+
+    struct Event {
+        std::string xPath;
+        std::map<std::string, std::string> data;
+        std::chrono::time_point<std::chrono::steady_clock> received;
+    };
+
+    Event peek(std::vector<Event>::size_type index) const;
+    std::vector<Event>::size_type count() const;
+
+private:
+    mutable std::shared_ptr<std::mutex> mutex = std::make_shared<std::mutex>();
+    std::shared_ptr<std::vector<Event>> events = std::make_shared<std::vector<Event>>();
+};

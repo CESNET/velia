562bab9f8e91d61df684acc8b50ddc4c2ba93f7d
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2021-01-25 19:57:00 +0100

    daemons: Add daemonize option without sdbus++
    Current daemons use sdbus++ library to start an event loop and keep
    going until SIGTERM against the process is issued.
    This is fine so far, however, in the next patch we introduce
    veliad-hardware daemon, which (so far) does not use sdbus++ and therefore
    it does not depend on the library.
    
    We need the veliad-hardware daemon just to keep itself running, all the
    Sysrepo background stuff is happening in the Sysrepo threads.
    
    This is the same use case we encountered in the
    lldp-systemd-networkd-sysrepo project and this patch introduces the same
    code that achieves that. The code is copied from the commit
    52f2fe693d20c6da803df6a64f29d0a209b75454.
    
    Change-Id: I76ef6bf119361b0c6b3f763acb6bce4f8c620a2f

--- src/main.h
+++ src/main.h
@@ -25,3 +25,29 @@
 #define DBUS_EVENTLOOP_END \
     g_eventLoop.join();      \
     spdlog::get("main")->debug("Shutting down");
+
+#define SIMPLE_DAEMONIZE_INIT \
+    volatile sig_atomic_t g_exit_application = 0;
+
+#define SIMPLE_DAEMONIZE                                                                                                                  \
+    /* Install sighandler for SIGTERM */                                                                                           \
+    struct sigaction sigact;                                                                                                       \
+    memset(&sigact, 0, sizeof(sigact));                                                                                            \
+    sigact.sa_handler = [](int) { g_exit_application = 1; };                                                                       \
+    sigact.sa_flags = SA_SIGINFO;                                                                                                  \
+    sigaction(SIGTERM, &sigact, nullptr);                                                                                          \
+                                                                                                                                   \
+    /*  Block SIGTERM */                                                                                                           \
+    sigset_t sigset, oldset;                                                                                                       \
+    sigemptyset(&sigset);                                                                                                          \
+    sigaddset(&sigset, SIGTERM);                                                                                                   \
+    sigprocmask(SIG_BLOCK, &sigset, &oldset);                                                                                      \
+                                                                                                                                   \
+    while (!g_exit_application) {                                                                                                  \
+        fd_set fd;                                                                                                                 \
+        FD_ZERO(&fd);                                                                                                              \
+                                                                                                                                   \
+        /* if SIGTERM received at this point, it is deffered until pselect is entered which enables the signal processing again */ \
+        pselect(0, &fd, NULL, NULL, NULL, &oldset);                                                                                \
+    }                                                                                                                              \
+    spdlog::get("main")->debug("Shutting down");

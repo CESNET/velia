d6475c3c6f5be8c0bbf9f79f671f6d235cc93b6f
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2021-01-27 17:33:11 +0100

    system: implement firmware status in czechlight-system
    This patch implements filling the data in the
    czechlight-system:firmware/firmware-slot list. The list is supposed to
    hold the basic information about the current firmware slot OS version
    and boot status. All the data are provided by RAUC.
    
    Because RAUC provides the slot data vis D-Bus only when explicitly asked
    by GetSlotStatus function over and there are no signals (or signals about
    property changes), we have to actually call the function to retrieve the
    data.
    
    However, there is one catch. RAUC will not provide us with the data when
    there is another operation in progress (i.e., when somebody invoked
    installation). Therefore, we cache the data and we provide the caller
    with the cached data whenever we can't fetch fresh data.
    
    There is, however, another catch. The data can change any time, not only
    after installation of new bundle. They can also change whenever somebody
    marks a slot bad/good. We can't capture that change unless we are
    querying the RAUC periodically, which we do not want to.
    
    Let's say, somebody marks the slot bad and then run an installation.
    During the installation he/she asks for the slot data. However, RAUC
    won't let us fetch the data (because it is fully devoted to the
    installation process), so we would return the old data, where the slot
    is still good.
    
    So, at least, we update the cache also before and after any installation,
    because that is the only long-running operation in RAUC.
    
    Change-Id: I890bf6813f15f9629a0c6acd516216e88d6cd986

--- src/system/Firmware.cpp
+++ src/system/Firmware.cpp
@@ -15,7 +15,40 @@ namespace {
 
 const auto CZECHLIGHT_SYSTEM_MODULE_NAME = "czechlight-system"s;
 const auto CZECHLIGHT_SYSTEM_FIRMWARE_MODULE_PREFIX = "/"s + CZECHLIGHT_SYSTEM_MODULE_NAME + ":firmware/"s;
+const auto FIRMWARE_SLOTS = {"rootfs.0"s, "rootfs.1"s};
 
+/** @brief Fetches the slot status data from RAUC. Returns an empty map if the fetch fails (RAUC busy, D-Bus error, ...). */
+std::map<std::string, velia::system::RAUC::SlotProperties> fetchSlotStatus(std::weak_ptr<velia::system::RAUC> rauc, velia::Log log)
+{
+    auto r = rauc.lock();
+    if (!r) {
+        log->trace("Could not fetch RAUC slot status data: RAUC object already destroyed");
+        return {};
+    }
+
+    try {
+        return r->slotStatus();
+    } catch (sdbus::Error& e) {
+        log->warn("Could not fetch RAUC slot status data: {}", e.getMessage());
+        return {};
+    }
+}
+
+/** @brief Updates the slot status cache with the new data. The function manipulates local cache shared among multiple thread. */
+void updateSlotStatus([[maybe_unused]] std::unique_lock<std::mutex>& mtx, std::map<std::string, std::string>& slotStatus, const std::map<std::string, velia::system::RAUC::SlotProperties>& raucStatus)
+{
+    for (const auto& slotName : FIRMWARE_SLOTS) {
+        if (auto it = raucStatus.find(slotName); it != raucStatus.end()) {
+            const auto& props = it->second;
+            auto xpathPrefix = CZECHLIGHT_SYSTEM_FIRMWARE_MODULE_PREFIX + "firmware-slot[name='" + std::get<std::string>(props.at("bootname")) + "']/";
+
+            slotStatus[xpathPrefix + "state"] = std::get<std::string>(props.at("state"));
+            slotStatus[xpathPrefix + "version"] = std::get<std::string>(props.at("bundle.version"));
+            slotStatus[xpathPrefix + "installed"] = std::get<std::string>(props.at("installed.timestamp"));
+            slotStatus[xpathPrefix + "boot-status"] = std::get<std::string>(props.at("boot-status"));
+        }
+    }
+}
 }
 
 namespace velia::system {
@@ -48,7 +81,10 @@ Firmware::Firmware(std::shared_ptr<::sysrepo::Connection> srConn, sdbus::IConnec
               session->event_notif_send(dataNode);
           },
           [this](int32_t retVal, const std::string& lastError) {
-              std::lock_guard<std::mutex> lck(m_mtx);
+              auto slotStatus = fetchSlotStatus(m_rauc, m_log);
+
+              std::unique_lock<std::mutex> lck(m_mtx);
+              updateSlotStatus(lck, m_slotStatus, slotStatus);
               m_installStatus = retVal == 0 ? "succeeded" : "failed";
               m_installMessage = lastError;
           }))
@@ -57,8 +93,9 @@ Firmware::Firmware(std::shared_ptr<::sysrepo::Connection> srConn, sdbus::IConnec
     {
         auto raucOperation = m_rauc->operation();
         auto raucLastError = m_rauc->lastError();
+        auto slotStatus = fetchSlotStatus(m_rauc, m_log);
 
-        std::lock_guard<std::mutex> lck(m_mtx);
+        std::unique_lock<std::mutex> lck(m_mtx);
 
         m_installMessage = raucLastError;
 
@@ -69,11 +106,19 @@ Firmware::Firmware(std::shared_ptr<::sysrepo::Connection> srConn, sdbus::IConnec
         } else {
             m_installStatus = "none";
         }
+
+        updateSlotStatus(lck, m_slotStatus, slotStatus);
     }
 
     m_srSubscribeRPC->rpc_subscribe(
         (CZECHLIGHT_SYSTEM_FIRMWARE_MODULE_PREFIX + "installation/install").c_str(),
         [this](::sysrepo::S_Session session, [[maybe_unused]] const char* op_path, const ::sysrepo::S_Vals input, [[maybe_unused]] sr_event_t event, [[maybe_unused]] uint32_t request_id, [[maybe_unused]] ::sysrepo::S_Vals_Holder output) {
+            auto slotStatus = fetchSlotStatus(m_rauc, m_log);
+            {
+                std::unique_lock<std::mutex> lck(m_mtx);
+                updateSlotStatus(lck, m_slotStatus, slotStatus);
+            }
+
             try {
                 std::string source = input->val(0)->val_to_string();
                 m_rauc->install(source);
@@ -91,12 +136,16 @@ Firmware::Firmware(std::shared_ptr<::sysrepo::Connection> srConn, sdbus::IConnec
         CZECHLIGHT_SYSTEM_MODULE_NAME.c_str(),
         [this](::sysrepo::S_Session session, [[maybe_unused]] const char* module_name, [[maybe_unused]] const char* path, [[maybe_unused]] const char* request_xpath, [[maybe_unused]] uint32_t request_id, libyang::S_Data_Node& parent) {
             std::map<std::string, std::string> data;
+
+            auto slotStatus = fetchSlotStatus(m_rauc, m_log);
+
             {
-                std::lock_guard<std::mutex> lck(m_mtx);
-                data = {
-                    {CZECHLIGHT_SYSTEM_FIRMWARE_MODULE_PREFIX + "installation/status", m_installStatus},
-                    {CZECHLIGHT_SYSTEM_FIRMWARE_MODULE_PREFIX + "installation/message", m_installMessage},
-                };
+                std::unique_lock<std::mutex> lck(m_mtx);
+                updateSlotStatus(lck, m_slotStatus, slotStatus);
+
+                data.insert(m_slotStatus.begin(), m_slotStatus.end());
+                data[CZECHLIGHT_SYSTEM_FIRMWARE_MODULE_PREFIX + "installation/status"] = m_installStatus;
+                data[CZECHLIGHT_SYSTEM_FIRMWARE_MODULE_PREFIX + "installation/message"] = m_installMessage;
             }
 
             utils::valuesToYang(data, session, parent);

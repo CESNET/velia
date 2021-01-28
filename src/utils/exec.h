6d9357ed1a6948403231097c256c13bc559855bd
Václav Kubernát <kubernat@cesnet.cz>
2021-01-28 15:38:24 +0100

    Refactor nft execing to a function
    Change-Id: I59dad3d62a400e82a6ea7a8a0cf9eff46cb579a1

---
+++ src/utils/exec.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+*/
+
+#pragma once
+#include <set>
+#include <string>
+#include "log-fwd.h"
+
+namespace velia::utils {
+/**
+ * Spawns a new process and waits until it returns. stdout is thrown away. Throws if the program has a non-zero exit
+ * code with a message containing the stderr of the process.
+ *
+ * @param logger Logger to use.
+ * @param program The name of the program to spawn. PATH is searched for this program.
+ * @param args Arguments to pass to the program. Can be {} if no arguments should be passed.
+ * @param std_in stdin input fo the program.
+ */
+enum class ExecOptions {
+    DropRoot
+};
+void execAndWait(velia::Log logger, const std::string& program, std::initializer_list<std::string> args, std::string_view std_in, const std::set<ExecOptions> opts = {});
+}

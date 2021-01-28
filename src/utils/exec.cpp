b2b5a2eb586f02e9b964118e10dea20d74f04668
Václav Kubernát <kubernat@cesnet.cz>
2021-01-28 15:38:24 +0100

    Refactor nft execing to a function
    Change-Id: I59dad3d62a400e82a6ea7a8a0cf9eff46cb579a1

---
+++ src/utils/exec.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+*/
+
+#include <boost/algorithm/string/join.hpp>
+#include <boost/process.hpp>
+#include <boost/process/extend.hpp>
+#include "exec.h"
+#include "log.h"
+
+void velia::utils::execAndWait(
+        velia::Log logger,
+        const std::string& program,
+        std::initializer_list<std::string> args,
+        std::string_view std_in,
+        const std::set<ExecOptions> opts)
+{
+    namespace bp = boost::process;
+    bp::pipe stdinPipe;
+    bp::ipstream stderrStream;
+
+    auto onExecSetup = [opts] (const auto&) {
+        if (opts.count(ExecOptions::DropRoot) == 1) {
+            if (getuid() == 0) {
+                if (setgid(1) == -1) {
+                    // FIXME: not sure if these work maybe write something to stderr? idk, can I use the logger?
+                    exit(1);
+                }
+
+                if (setuid(1) == -1) {
+                    exit(1);
+                }
+            }
+        }
+    };
+
+    logger->trace("exec: {} {}", program, boost::algorithm::join(args, " "));
+    bp::child c(
+            bp::search_path(program),
+            boost::process::args=std::move(args),
+            bp::std_in < stdinPipe, bp::std_out > bp::null, bp::std_err > stderrStream,
+            bp::extend::on_exec_setup=onExecSetup);
+
+    stdinPipe.write(std_in.data(), std_in.size());
+    stdinPipe.close();
+
+    c.wait();
+    logger->trace("{} exited", program);
+
+    if (c.exit_code()) {
+        std::istreambuf_iterator<char> begin(stderrStream), end;
+        std::string stderrOutput(begin, end);
+        logger->critical("{} ended with a non-zero exit code. stderr: {}", program, stderrOutput);
+
+        throw std::runtime_error(program + " returned non-zero exit code " + std::to_string(c.exit_code()));
+    }
+}

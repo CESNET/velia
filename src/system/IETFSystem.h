f1c466da8fc6ffc53a4dce2678cd5f6146ccb889
Václav Kubernát <kubernat@cesnet.cz>
2021-02-10 08:33:54 +0100

    Implement hostname
    Change-Id: I676d7cf5c821debf59c62ddad6a06264d57918ec

--- src/system/IETFSystem.h
+++ src/system/IETFSystem.h
@@ -14,11 +14,13 @@ namespace velia::system {
 
 class IETFSystem {
 public:
-    IETFSystem(std::shared_ptr<::sysrepo::Session> srSession, const std::filesystem::path& osRelease);
+    IETFSystem(std::shared_ptr<::sysrepo::Connection> srSession, const std::filesystem::path& osRelease);
 
 private:
-    std::shared_ptr<::sysrepo::Session> m_srSession;
-    std::shared_ptr<::sysrepo::Subscribe> m_srSubscribe;
+    std::shared_ptr<::sysrepo::Session> m_srSessionRunning;
+    std::shared_ptr<::sysrepo::Session> m_srSessionStartup;
+    std::shared_ptr<::sysrepo::Subscribe> m_srSubscribeRunning;
+    std::shared_ptr<::sysrepo::Subscribe> m_srSubscribeStartup;
     velia::Log m_log;
 };
 }

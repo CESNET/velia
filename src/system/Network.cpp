aa829777a5ab9ab1cfb01e773f7c1c81373eb783
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-02-15 20:41:29 +0100

    system: Configure eth1 interface (on running system)
    This commit is a part of implementation of network configuration for eth1
    interface through Sysrepo and velia-system. It implements
    /czechlight-system:networking/standalone-eth1 presence container in
    running datastore.
    
    If the presence container is missing, the network configuration for eth1
    interface resets to the "old" default configuration of eth1, i.e., add
    it to the br0 bridge.
    If the container is present, the interface is removed from the bridge and
    its IP address is obtained via DHCP.
    
    The network is managed by systemd-networkd thus we implement the changes
    via systemd-networkd network files [1]. The files can be placed in /etc,
    /run and /usr directories (and they take precedence in this order). We
    place the new network file in the /run directory and reload network
    configuration via networkctl reload.
    
    There is a slight catch in removing the interface from a bridge.
    Apparently, systemd-networkd does not remove an interface from a bridge
    if the new configuration does not contain Bridge settings [2].
    However, bringing the interface down and then reloading the new
    configuration apparently works.
    
    [1] https://www.freedesktop.org/software/systemd/man/systemd.network.html
    [2] https://github.com/systemd/systemd/issues/8190
    
    Change-Id: I8940985a88903c17ea61e55aced67b17fba4656a

---
+++ src/system/Network.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#include <fmt/core.h>
+#include "Network.h"
+#include "utils/io.h"
+#include "utils/log.h"
+#include "utils/sysrepo.h"
+
+using namespace std::literals;
+using namespace fmt::literals;
+
+namespace {
+
+const auto CZECHLIGHT_SYSTEM_MODULE_NAME = "czechlight-system"s;
+const auto CZECHLIGHT_SYSTEM_STANDALONE_ETH1 = "/"s + CZECHLIGHT_SYSTEM_MODULE_NAME + ":networking/standalone-eth1"s;
+
+const std::string NETWORK_FILE_CONTENT_TEMPLATE = R"([Match]
+Name=eth1
+
+[Network]
+{setting}
+LLDP=true
+EmitLLDP=nearest-bridge
+)";
+
+std::map<std::string, std::string> getNetworkConfiguration(std::shared_ptr<::sysrepo::Session> session, velia::Log log)
+{
+    if (session->get_data(CZECHLIGHT_SYSTEM_STANDALONE_ETH1.c_str()) == nullptr) { // the presence container is missing, bridge eth1
+        log->info("Container eth1-standalone not present. Generating bridge configuration for eth1.");
+        return {{"eth1", fmt::format(NETWORK_FILE_CONTENT_TEMPLATE, "setting"_a = "Bridge=br0")}};
+    } else {
+        log->info("Container eth1-standalone is present. Generating DHCP configuration for eth1.");
+        return {{"eth1", fmt::format(NETWORK_FILE_CONTENT_TEMPLATE, "setting"_a = "DHCP=yes")}};
+    }
+}
+
+}
+
+namespace velia::system {
+
+Network::Network(std::shared_ptr<::sysrepo::Session> srSess, std::filesystem::path networkConfigDirectory, std::function<void(const std::vector<std::string>&)> networkReloadCallback)
+    : m_log(spdlog::get("system"))
+    , m_srSubscribe(std::make_shared<sysrepo::Subscribe>(srSess))
+{
+    utils::ensureModuleImplemented(srSess, CZECHLIGHT_SYSTEM_MODULE_NAME, "2021-01-13");
+
+    m_srSubscribe->module_change_subscribe(
+        CZECHLIGHT_SYSTEM_MODULE_NAME.c_str(),
+        [&, networkConfigDirectory = std::move(networkConfigDirectory), networkReloadCallback = std::move(networkReloadCallback)](sysrepo::S_Session session, [[maybe_unused]] const char* module_name, [[maybe_unused]] const char* xpath, [[maybe_unused]] sr_event_t event, [[maybe_unused]] uint32_t request_id) {
+            auto config = getNetworkConfiguration(session, m_log);
+            for (const auto& [interface, networkFileContents] : config) {
+                velia::utils::safeWriteFile(networkConfigDirectory / (interface + ".network"), networkFileContents);
+            }
+
+            std::vector<std::string> interfaces;
+            std::transform(config.begin(), config.end(), std::back_inserter(interfaces), [](const auto& kv) { return kv.first; });
+            networkReloadCallback(interfaces);
+
+            return SR_ERR_OK;
+        },
+        CZECHLIGHT_SYSTEM_STANDALONE_ETH1.c_str(),
+        0,
+        SR_SUBSCR_DONE_ONLY | SR_SUBSCR_ENABLED);
+}
+
+}

CESNET/velia

commit 3da1467d50dc78be049f4a00de24718a0874aa51

Get rid of the --log-level option
This option was misleading, because it seems that it sets the default
log level for all of the other loggers, but it doesn't. The other
loggers have their own defaults and these would always override
--log-level. However, it does change the log level of the `main` logger.

Change-Id: I6ba6284acda30bb680fab8d0ca912a9fd956eacd

src/main-hardware.cpp
@@ -18,7 +18,6 @@ static const char usage[] =
 Usage:
   veliad-hardware
     [--appliance=<Model>]
-    [--log-level=<Level>]
     [--sysrepo-log-level=<Level>]
     [--hardware-log-level=<Level>]
   veliad-hardware (-h | --help)
@@ -28,11 +27,10 @@ Options:
   -h --help                         Show this screen.
   --version                         Show version.
   --appliance=<Model>               Initialize IETF Hardware and outputs for specific appliance.
-  --log-level=<N>                   Log level for everything [default: 3]
-                                    (0 -> critical, 1 -> error, 2 -> warning, 3 -> info,
-                                    4 -> debug, 5 -> trace)
   --sysrepo-log-level=<N>           Log level for the sysrepo library [default: 3]
   --hardware-log-level=<N>          Log level for the hardware drivers [default: 3]
+                                    (0 -> critical, 1 -> error, 2 -> warning, 3 -> info,
+                                    4 -> debug, 5 -> trace)
 )";
 
 int main(int argc, char* argv[])
@@ -50,7 +48,6 @@ int main(int argc, char* argv[])
     spdlog::set_level(spdlog::level::info);
 
     try {
-        spdlog::set_level(parseLogLevel("Generic", args["--log-level"]));
         spdlog::get("sysrepo")->set_level(parseLogLevel("Sysrepo library", args["--sysrepo-log-level"]));
         spdlog::get("hardware")->set_level(parseLogLevel("Hardware loggers", args["--hardware-log-level"]));
 

src/main-health.cpp
@@ -20,7 +20,6 @@ static const char usage[] =
 Usage:
   veliad-health
     [--appliance=<Model>]
-    [--log-level=<Level>]
     [--health-log-level=<Level>]
     [--systemd-ignore-unit=<Unit>]...
   veliad-health (-h | --help)
@@ -29,10 +28,9 @@ Usage:
 Options:
   -h --help                         Show this screen.
   --version                         Show version.
-  --log-level=<N>                   Log level for everything [default: 3]
+  --health-log-level=<N>            Log level for the health monitoring [default: 3]
                                     (0 -> critical, 1 -> error, 2 -> warning, 3 -> info,
                                     4 -> debug, 5 -> trace)
-  --health-log-level=<N>            Log level for the health monitoring [default: 3]
   --systemd-ignore-unit=<Unit>      Ignore state of systemd's unit in systemd state tracker. Can be specified multiple times.
 )";
 
@@ -53,8 +51,6 @@ int main(int argc, char* argv[])
     spdlog::set_level(spdlog::level::info);
 
     try {
-        spdlog::set_level(parseLogLevel("Generic", args["--log-level"]));
-
         DBUS_EVENTLOOP_START
 
         // health

src/main-system.cpp
@@ -18,7 +18,6 @@ static const char usage[] =
 
 Usage:
   veliad-system
-    [--log-level=<Level>]
     [--sysrepo-log-level=<Level>]
     [--system-log-level=<Level>]
   veliad-system (-h | --help)
@@ -27,11 +26,10 @@ Usage:
 Options:
   -h --help                         Show this screen.
   --version                         Show version.
-  --log-level=<N>                   Log level for everything [default: 3]
-                                    (0 -> critical, 1 -> error, 2 -> warning, 3 -> info,
-                                    4 -> debug, 5 -> trace)
   --sysrepo-log-level=<N>           Log level for the sysrepo library [default: 3]
   --system-log-level=<N>            Log level for the system stuff [default: 3]
+                                    (0 -> critical, 1 -> error, 2 -> warning, 3 -> info,
+                                    4 -> debug, 5 -> trace)
 )";
 
 DBUS_EVENTLOOP_INIT
@@ -51,7 +49,6 @@ int main(int argc, char* argv[])
     spdlog::set_level(spdlog::level::info);
 
     try {
-        spdlog::set_level(parseLogLevel("Generic", args["--log-level"]));
         spdlog::get("sysrepo")->set_level(parseLogLevel("Sysrepo library", args["--sysrepo-log-level"]));
 
         spdlog::get("main")->debug("Opening Sysrepo connection");

commit 2f6bc15f4446dee8b18ccc399c9c7f52cc0b077c

Reduce logging a bit
Now that there's no --log-level flag, a lot of these daemons are using
just two loggers. In case of the "hardware" service, it's the "sysrepo"
one and the "hardware" one, for example. Do not attempt to log
debug/trace messages via "main", that's useless as they are silently
discarded.

The critical exceptions are still logged via "main", that's OK because
of the default log level.

Also remove some log messages which are really not that informative. I
can imagine they were useful when writing this, but let's hope that we
won't be debugging failures such as "not being able to connect to
D-Bus", etc. These logs were now getting silently discarded anyway.

Change-Id: Ic6e7a8f79d1f2d51992b36c9e53fb568a8727e25
Fixes: 3da1467 ("Get rid of the --log-level option")

src/ietf-hardware/sysrepo/Sysrepo.cpp
@@ -30,7 +30,7 @@ Sysrepo::Sysrepo(std::shared_ptr<::sysrepo::Subscribe> srSubscribe, std::shared_
             auto hwStateValues = m_hwState->process();
             utils::valuesToYang(hwStateValues, session, parent);
 
-            spdlog::get("main")->trace("Pushing to sysrepo (JSON): {}", parent->print_mem(LYD_FORMAT::LYD_JSON, 0));
+            spdlog::get("hardware")->trace("Pushing to sysrepo (JSON): {}", parent->print_mem(LYD_FORMAT::LYD_JSON, 0));
 
             return SR_ERR_OK;
         },

src/main-firewall.cpp
@@ -51,7 +51,6 @@ int main(int argc, char* argv[])
         spdlog::get("firewall")->set_level(parseLogLevel("Firewall logging", args["--firewall-log-level"]));
         spdlog::get("sysrepo")->set_level(parseLogLevel("Sysrepo library", args["--sysrepo-log-level"]));
 
-        spdlog::get("main")->debug("Opening Sysrepo connection");
         auto srConn = std::make_shared<sysrepo::Connection>();
         auto srSess = std::make_shared<sysrepo::Session>(srConn);
         velia::firewall::SysrepoFirewall firewall(srSess, [] (const auto& config) {
@@ -62,8 +61,6 @@ int main(int argc, char* argv[])
         });
 
         waitUntilSignaled();
-
-        spdlog::get("main")->info("Exiting.");
 
         return 0;
     } catch (std::exception& e) {

src/main-hardware.cpp
@@ -51,13 +51,11 @@ int main(int argc, char* argv[])
         spdlog::get("sysrepo")->set_level(parseLogLevel("Sysrepo library", args["--sysrepo-log-level"]));
         spdlog::get("hardware")->set_level(parseLogLevel("Hardware loggers", args["--hardware-log-level"]));
 
-        spdlog::get("main")->debug("Opening Sysrepo connection");
         auto srConn = std::make_shared<sysrepo::Connection>();
         auto srSess = std::make_shared<sysrepo::Session>(srConn);
         auto srSubscription = std::make_shared<sysrepo::Subscribe>(srSess);
 
         // initialize ietf-hardware
-        spdlog::get("main")->debug("Initializing IETFHardware module");
         std::shared_ptr<velia::ietf_hardware::IETFHardware> ietfHardware;
         if (const auto& appliance = args["--appliance"]) {
             ietfHardware = velia::ietf_hardware::create(appliance.asString());
@@ -65,7 +63,6 @@ int main(int argc, char* argv[])
             ietfHardware = std::make_shared<velia::ietf_hardware::IETFHardware>();
         }
 
-        spdlog::get("main")->debug("Initializing Sysrepo ietf-hardware callback");
         auto sysrepoIETFHardware = velia::ietf_hardware::sysrepo::Sysrepo(srSubscription, ietfHardware);
 
         waitUntilSignaled();

src/main-health.cpp
@@ -58,21 +58,19 @@ int main(int argc, char* argv[])
 
         // output configuration
         if (const auto& appliance = args["--appliance"]) {
-            spdlog::get("main")->debug("Initializing LED drivers");
+            spdlog::get("health")->debug("Initializing LED drivers");
             manager->m_outputSignal.connect(velia::health::createOutput(appliance.asString()));
         }
 
-        spdlog::get("main")->debug("All outputs initialized.");
+        spdlog::get("health")->debug("All outputs initialized.");
 
         // input configuration
         std::set<std::string> ignoredUnits(args["--systemd-ignore-unit"].asStringList().begin(), args["--systemd-ignore-unit"].asStringList().end());
-        spdlog::get("main")->debug("Starting DBus systemd watcher");
+        spdlog::get("health")->debug("Starting DBus systemd watcher");
         if (!ignoredUnits.empty()) {
-            spdlog::get("main")->debug("Systemd input will ignore changes of the following units: {}", args["--systemd-ignore-unit"]);
+            spdlog::get("health")->debug("Systemd input will ignore changes of the following units: {}", args["--systemd-ignore-unit"]);
         }
         auto inputSystemdDbus = std::make_shared<velia::health::DbusSystemdInput>(manager, ignoredUnits, *g_dbusConnection);
-
-        spdlog::get("main")->debug("All inputs initialized.");
 
         DBUS_EVENTLOOP_END;
 

src/main-system.cpp
@@ -53,14 +53,12 @@ int main(int argc, char* argv[])
     try {
         spdlog::get("sysrepo")->set_level(parseLogLevel("Sysrepo library", args["--sysrepo-log-level"]));
 
-        spdlog::get("main")->debug("Opening Sysrepo connection");
         auto srConn = std::make_shared<sysrepo::Connection>();
         auto srSess = std::make_shared<sysrepo::Session>(srConn);
 
         DBUS_EVENTLOOP_START
 
         // initialize ietf-system
-        spdlog::get("main")->debug("Initializing Sysrepo for system models");
         auto sysrepoIETFSystem = velia::system::IETFSystem(srSess, "/etc/os-release");
 
         auto dbusConnection = sdbus::createConnection(); // second connection for RAUC (for calling methods).

src/main.h
@@ -10,7 +10,6 @@
     std::thread g_eventLoop;
 
 #define DBUS_EVENTLOOP_START                                               \
-    spdlog::get("main")->debug("Opening DBus connection");                 \
     g_dbusConnection = sdbus::createSystemBusConnection();                 \
     /* Gracefully leave dbus event loop on SIGTERM */                      \
     struct sigaction sigact;                                               \
@@ -19,9 +18,7 @@
     sigact.sa_flags = SA_SIGINFO;                                          \
     sigact.sa_handler = [](int) { g_dbusConnection->leaveEventLoop(); };   \
     sigaction(SIGTERM, &sigact, nullptr);                                  \
-    spdlog::get("main")->debug("Starting DBus event loop");                \
     g_eventLoop = std::thread([] { g_dbusConnection->enterEventLoop(); }); \
 
 #define DBUS_EVENTLOOP_END \
-    g_eventLoop.join();      \
-    spdlog::get("main")->debug("Shutting down");
+    g_eventLoop.join();

src/utils/sysrepo.cpp
@@ -54,8 +54,6 @@ void initLogsSysrepo()
 void valuesToYang(const std::map<std::string, std::string>& values, std::shared_ptr<::sysrepo::Session> session, std::shared_ptr<libyang::Data_Node>& parent)
 {
     for (const auto& [propertyName, value] : values) {
-        spdlog::get("main")->trace("propertyName: {}, value: {}", propertyName, value.c_str());
-
         if (!parent) {
             parent = std::make_shared<libyang::Data_Node>(
                 session->get_context(),

src/utils/waitUntilSignalled.cpp
@@ -6,7 +6,6 @@
  */
 
 #include <csignal>
-#include <spdlog/spdlog.h>
 #include <unistd.h>
 #include "waitUntilSignalled.h"
 
@@ -15,5 +14,4 @@ void waitUntilSignaled()
     signal(SIGTERM, [](int) {});
     signal(SIGINT, [](int) {});
     pause();
-    spdlog::get("main")->debug("Shutting down");
 }
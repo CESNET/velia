b3472836e68f4ee0b81d51978d711ce9f1d7b5dc
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-02-23 19:01:35 +0100

    system: Get link info from rtnetlink
    This commit introduces Rtnetlink class. The purpose of the class is to
    communicate with kernel using netlink protocol and provide a method that
    allows to iterate over links so that the caller doesn't have to worry
    about netlink initialization and/or cleanup and focus only on processing
    the rtnl_link struct to obtain the desired data.
    
    The netlink communication is implemented by Netlink Protocol Library
    Suite (libnl) [1]. Its part (libnl-route) also provides some handy
    shortcuts when dealing with NETLINK_ROUTE scokets.
    
    [1] https://www.infradead.org/~tgr/libnl/
    
    Change-Id: I703c69827bb6b974d37b05a18f2af468685ec758

---
+++ src/system/Rtnetlink.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#pragma once
+
+#include <functional>
+#include <mutex>
+#include <netlink/netlink.h>
+#include <netlink/route/link.h>
+#include <thread>
+#include <stdexcept>
+#include "utils/log-fwd.h"
+
+namespace velia::system {
+
+namespace impl {
+class nlCacheMngrWatcher;
+}
+
+/** @brief Wrapper for monitoring changes in NETLINK_ROUTE */
+struct Rtnetlink {
+public:
+    using nlCacheManager = std::shared_ptr<nl_cache_mngr>;
+    using LinkCB = std::function<void(rtnl_link* link, int cacheAction)>; /// cacheAction: NL_ACT_*
+
+    explicit Rtnetlink(LinkCB cbLink);
+    ~Rtnetlink();
+
+private:
+    velia::Log m_log;
+    nlCacheManager m_nlCacheManager;
+    LinkCB m_cbLink; /* (link object ptr, cache action). Cache action specified in libnl's cache.h (NL_ACT_*) */
+    std::unique_ptr<impl::nlCacheMngrWatcher> m_nlCacheMngrWatcher; // first to destroy, because the thread uses m_nlCacheManager and m_log
+};
+
+class RtnetlinkException : public std::runtime_error {
+public:
+    RtnetlinkException(const std::string& msg);
+    RtnetlinkException(const std::string& funcName, int error);
+};
+
+}

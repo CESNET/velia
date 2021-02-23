f10b9308d6e89d4a48a9852820cea496b33bffc5
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-02-23 19:02:02 +0100

    system: Implement ietf-interfaces
    This commit is a start of implementing ietf-interfaces YANG model. It
    initialises list of interfaces (as provided by rtnetlink) and fills
    few leafs (physical address, and mandatory leafs operational status
    and type).
    
    Change-Id: I6c42ae2b3f493256e5b1eef8690cc1ebea21577c

---
+++ src/system/IETFInterfaces.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+#pragma once
+
+#include <sysrepo-cpp/Session.hpp>
+#include "utils/log-fwd.h"
+
+struct rtnl_link;
+
+namespace velia::system {
+
+class Rtnetlink;
+
+class IETFInterfaces {
+public:
+    explicit IETFInterfaces(std::shared_ptr<::sysrepo::Session> srSess);
+
+private:
+    void onLinkUpdate(rtnl_link* link, int action);
+
+    std::shared_ptr<::sysrepo::Session> m_srSession;
+    velia::Log m_log;
+    std::shared_ptr<Rtnetlink> m_rtnetlink; // first to destroy, because the callback to rtnetlink uses m_srSession and m_log
+};
+}

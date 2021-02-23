b61b98b52d590aed74f74ac4a10111fc717c62ee
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-02-23 19:02:02 +0100

    system: Implement ietf-interfaces
    This commit is a start of implementing ietf-interfaces YANG model. It
    initialises list of interfaces (as provided by rtnetlink) and fills
    few leafs (physical address, and mandatory leafs operational status
    and type).
    
    Change-Id: I6c42ae2b3f493256e5b1eef8690cc1ebea21577c

---
+++ tests/sysrepo_system-ietfinterfaces.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+*/
+
+#include "trompeloeil_doctest.h"
+#include "pretty_printers.h"
+#include "system/IETFInterfaces.h"
+#include "test_log_setup.h"
+#include "test_sysrepo_helpers.h"
+#include "tests/mock/system.h"
+
+using namespace std::chrono_literals;
+
+TEST_CASE("ietf-interfaces localhost")
+{
+    TEST_SYSREPO_INIT_LOGS;
+    TEST_SYSREPO_INIT;
+    TEST_SYSREPO_INIT_CLIENT;
+
+    auto network = std::make_shared<velia::system::IETFInterfaces>(srSess);
+
+    // We didn't came up with some way of mocking netlink. At least check that there is the loopback
+    // interface with expected values. It is *probably* safe to assume that there is at least the lo device.
+    REQUIRE(dataFromSysrepo(client, "/ietf-interfaces:interfaces/interface[name='lo']", SR_DS_OPERATIONAL) == std::map<std::string, std::string> {
+                {"/name", "lo"},
+                {"/type", "iana-if-type:softwareLoopback"},
+                {"/phys-address", "00:00:00:00:00:00"},
+                {"/oper-status", "unknown"},
+            });
+}

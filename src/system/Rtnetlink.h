a8d6bb7b778f0c6dd60a333e30faa6e106dda522
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-02-23 19:01:35 +0100

    system: Get link info from rtnetlink
    This commit introduces Rtnetlink class. The purpose of the class is to
    communicate with kernel using netlink protocol and provide a method that
    allows to iterate over links so that the caller doesn't have to worry
    about netlink initialization and/or cleanup and focus only on processing
    the rtnl_link struct to obtain the desired data.
    
    The netlink communication is implemented by Netlink Protocol Library
    Suite (libnl) [1]. Its part (libnl-route) also provides some handy
    shortcuts when dealing with NETLINK_ROUTE scokets.
    
    [1] https://www.infradead.org/~tgr/libnl/
    
    Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/br2-external/+/3978
    Depends-on: https://cesnet-gerrit-public/c/CzechLight/br2-external/+/3978
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/br2-external/+/3978
    Change-Id: I703c69827bb6b974d37b05a18f2af468685ec758

---
+++ src/system/Rtnetlink.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#pragma once
+
+#include <functional>
+#include <mutex>
+#include <thread>
+#include <netlink/netlink.h>
+#include <netlink/route/link.h>
+#include <stdexcept>
+#include "utils/log-fwd.h"
+
+namespace velia::system {
+
+/** @brief Wrapper for monitoring changes in NETLINK_ROUTE */
+class Rtnetlink {
+public:
+    explicit Rtnetlink(std::function<void(rtnl_link*, int)> cbLink);
+
+private:
+    using nlCacheManager = std::shared_ptr<nl_cache_mngr>;
+
+    /** @brief Implementation of a background thread that watches for changes in cache and notifies caller via m_cbLink. */
+    struct nlCacheMngrWatcher {
+        bool m_terminate;
+        std::mutex m_mtx;
+        std::thread m_thr;
+
+        nlCacheMngrWatcher(nlCacheManager manager, velia::Log log);
+        ~nlCacheMngrWatcher();
+        bool shouldTerminate();
+        void run(Rtnetlink::nlCacheManager manager, velia::Log log);
+    };
+
+    velia::Log m_log;
+    nlCacheManager m_nlCacheManager;
+    std::function<void(rtnl_link*, int)> m_cbLink; /* (link object ptr, cache action). Cache action specified in libnl's cache.h (NL_ACT_*) */
+    std::unique_ptr<nlCacheMngrWatcher> m_nlCacheMngrWatcher; // first to destroy, because the thread uses m_nlCacheManager and m_log
+};
+
+class RtnetlinkException : public std::runtime_error {
+public:
+    RtnetlinkException(const std::string& funcName);
+    RtnetlinkException(const std::string& funcName, int error);
+};
+
+}

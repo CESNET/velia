05b30095be0679a6ac0e9f11219ff0a15894769a
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-03-31 20:02:06 +0200

    system: report routing table via ietf-routing model
    Implement filling of the routing table in the ietf-routing YANG model
    [1]. We only fill the data from the main routing table. The main routing table
    can be obtained with `ip [-4|-6] route show table main`.
    
    [1] https://tools.ietf.org/html/rfc8349
    
    Change-Id: I9cacfa47047df9db8d3f90ffbdc151c67532f411

--- src/system/IETFInterfaces.cpp
+++ src/system/IETFInterfaces.cpp
@@ -17,12 +17,23 @@ using namespace std::string_literals;
 
 namespace {
 
+/** @brief Computes the length of the const C-string (array of const char) *including* the terminating zero
+ *
+ * Credits: https://dbj.org/cpp-zero-time-strlen-and-strnlen/
+ */
+template <size_t N>
+inline constexpr size_t arrlen(const char(&)[N]) noexcept
+{
+    return N;
+}
+
 const auto CZECHLIGHT_NETWORK_MODULE_NAME = "czechlight-network"s;
 const auto IETF_IP_MODULE_NAME = "ietf-ip"s;
 const auto IETF_INTERFACES_MODULE_NAME = "ietf-interfaces"s;
 const auto IETF_INTERFACES = "/"s + IETF_INTERFACES_MODULE_NAME + ":interfaces"s;
 
 const auto PHYS_ADDR_BUF_SIZE = 6 * 2 /* 2 chars per 6 bytes in the address */ + 5 /* delimiters (':') between bytes */ + 1 /* \0 */;
+const auto IPV6ADDRSTRLEN_WITH_PREFIX = INET6_ADDRSTRLEN + 1 + 3 /* plus slash and max three-digits prefix */;
 
 std::string operStatusToString(uint8_t operStatus, velia::Log log)
 {
@@ -117,13 +128,15 @@ IETFInterfaces::IETFInterfaces(std::shared_ptr<::sysrepo::Session> srSess)
     , m_log(spdlog::get("system"))
     , m_rtnetlink(std::make_shared<Rtnetlink>(
           [this](rtnl_link* link, int action) { onLinkUpdate(link, action); },
-          [this](rtnl_addr* addr, int action) { onAddrUpdate(addr, action); }))
+          [this](rtnl_addr* addr, int action) { onAddrUpdate(addr, action); },
+          [this](rtnl_route* addr, int action) { onRouteUpdate(addr, action); }))
 {
     utils::ensureModuleImplemented(m_srSession, IETF_INTERFACES_MODULE_NAME, "2018-02-20");
     utils::ensureModuleImplemented(m_srSession, IETF_IP_MODULE_NAME, "2018-02-22");
     utils::ensureModuleImplemented(m_srSession, CZECHLIGHT_NETWORK_MODULE_NAME, "2021-02-22");
 
     m_rtnetlink->invokeInitialCallbacks();
+    // TODO: Implement /ietf-routing:routing/interfaces and /ietf-routing:routing/router-id
 }
 
 void IETFInterfaces::onLinkUpdate(rtnl_link* link, int action)
@@ -183,6 +196,122 @@ void IETFInterfaces::onAddrUpdate(rtnl_addr* addr, int action)
         values[yangPrefix + "/prefix-length"] = std::to_string(rtnl_addr_get_prefixlen(addr));
     } else {
         m_log->warn("Unhandled cache update action {} ({})", action, nlActionToString(action));
+    }
+
+    utils::valuesPush(values, deletePaths, m_srSession, SR_DS_OPERATIONAL);
+}
+
+void IETFInterfaces::onRouteUpdate(rtnl_route*, int)
+{
+    /* NOTE:
+     * We don't know the position of the changed route in the list of routes
+     * Replace the whole subtree (and therefore fetch all routes to publish fresh data)
+     * Unfortunately, this function may be called several times during the "reconstruction" of the routing table.
+     */
+
+    std::map<std::string, std::string> values;
+    std::vector<std::string> deletePaths;
+
+    auto routes = m_rtnetlink->getRoutes();
+    auto links = m_rtnetlink->getLinks();
+
+    // ipv4 and ipv6 routes are in separate lists; keep a track of current index to the list so we correctly append the route to the end of the list
+    std::map<decltype(AF_INET), unsigned> routeIdx {{AF_INET, 1}, {AF_INET6, 1}};
+
+    for (const auto& route : routes) {
+        if (rtnl_route_get_table(route.get()) != RT_TABLE_MAIN) {
+            continue;
+        }
+
+        if (rtnl_route_get_type(route.get()) != RTN_UNICAST) {
+            continue;
+        }
+
+        auto family = rtnl_route_get_family(route.get());
+        if (family != AF_INET && family != AF_INET6) {
+            continue;
+        }
+
+        auto proto = rtnl_route_get_protocol(route.get());
+        if (proto != RTPROT_KERNEL && proto != RTPROT_RA && proto != RTPROT_DHCP && proto != RTPROT_STATIC) {
+            std::array<char, arrlen("redirect")> buf; /* "redirect" is the longest value (libnl/lib/route/route_utils.c, init_proto_names) */
+            m_log->warn("Unimplemented routing protocol {} '{}'", proto, rtnl_route_proto2str(proto, buf.data(), buf.size()));
+            continue;
+        }
+
+        const auto ribName = family == AF_INET ? "ipv4-master"s : "ipv6-master"s;
+        const auto yangPrefix = "/ietf-routing:routing/ribs/rib[name='" + ribName + "']/routes/route["s + std::to_string(routeIdx[family]++) + "]/";
+        const auto familyYangPrefix = family == AF_INET ? "ietf-ipv4-unicast-routing"s : "ietf-ipv6-unicast-routing"s;
+
+        std::string destPrefix;
+        if (auto* addr = rtnl_route_get_dst(route.get()); addr != nullptr) {
+            if (nl_addr_iszero(addr)) {
+                destPrefix = family == AF_INET ? "0.0.0.0/0" : "::/0";
+            } else {
+                std::array<char, IPV6ADDRSTRLEN_WITH_PREFIX> data;
+                destPrefix = nl_addr2str(addr, data.data(), data.size());
+
+                // append prefix len if nl_addr2str fails to do that (when prefix length is 32 in ipv4 or 128 in ipv6)
+                if (destPrefix.find_first_of('/') == std::string::npos) {
+                    destPrefix += "/" + std::to_string(nl_addr_get_prefixlen(addr));
+                }
+            }
+        }
+
+        values[yangPrefix + familyYangPrefix + ":destination-prefix"] = destPrefix;
+
+        auto scope = rtnl_route_get_scope(route.get());
+        std::string protoStr;
+        switch (proto) {
+        case RTPROT_KERNEL:
+            protoStr = scope == RT_SCOPE_LINK ? "direct" : "static";
+            break;
+        case RTPROT_STATIC:
+            protoStr = "static";
+            break;
+        case RTPROT_DHCP:
+            protoStr = "czechlight-network:dhcp";
+            break;
+        case RTPROT_RA:
+            protoStr = "czechlight-network:ra";
+            break;
+        default:
+            throw std::invalid_argument("Unexpected route protocol ("s + std::to_string(proto) + ")");
+        }
+
+        values[yangPrefix + "source-protocol"] = protoStr;
+
+        const auto hops = rtnl_route_get_nnexthops(route.get());
+        const bool multihop = hops > 1;
+        for (auto i = 0; i < hops; i++) {
+            rtnl_nexthop* nh = rtnl_route_nexthop_n(route.get(), i);
+
+            if (nl_addr* addr = rtnl_route_nh_get_gateway(nh); addr) {
+                std::string yangKey;
+                if (!multihop) {
+                    yangKey = yangPrefix + "next-hop/" + familyYangPrefix + ":next-hop-address";
+                } else {
+                    yangKey = yangPrefix + "next-hop/next-hop-list/next-hop[" + std::to_string(i + 1) + "]/" + familyYangPrefix + ":address";
+                }
+
+                std::array<char, IPV6ADDRSTRLEN_WITH_PREFIX> buf;
+                values[yangKey] = nl_addr2str(addr, buf.data(), buf.size());
+            }
+
+            auto if_index = rtnl_route_nh_get_ifindex(nh);
+            if (auto linkIt = std::find_if(links.begin(), links.end(), [if_index](const Rtnetlink::nlLink& link) { return rtnl_link_get_ifindex(link.get()) == if_index; }); linkIt != links.end()) {
+                if (char* ifname = rtnl_link_get_name(linkIt->get()); ifname) {
+                    std::string yangKey;
+                    if (!multihop) {
+                        yangKey = yangPrefix + "next-hop/outgoing-interface";
+                    } else {
+                        yangKey = yangPrefix + "next-hop/next-hop-list/next-hop[" + std::to_string(i + 1) + "]/outgoing-interface";
+                    }
+
+                    values[yangKey] = rtnl_link_get_name(linkIt->get());
+                }
+            }
+        }
     }
 
     utils::valuesPush(values, deletePaths, m_srSession, SR_DS_OPERATIONAL);

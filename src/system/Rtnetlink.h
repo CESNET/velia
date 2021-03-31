1ea7e313dcd2a2103d000715e77abd222f6cd7c0
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-03-31 20:00:03 +0200

    system: rtnetlink fetch routes
    Get current network routes from kernel.
    Unfortunately, for some reason some routes may be missing when the route
    cache is resynced. This, however, doesn't happen when the cache is
    reconstructed from scratch.
    
    Change-Id: Idd77090789630742f234894f5bc9eedb506a06b7

--- src/system/Rtnetlink.h
+++ src/system/Rtnetlink.h
@@ -13,6 +13,7 @@
 #include <netlink/route/addr.h>
 #include <netlink/route/link.h>
 #include <netlink/route/neighbour.h>
+#include <netlink/route/route.h>
 #include <stdexcept>
 #include <thread>
 #include "utils/log-fwd.h"
@@ -30,6 +31,7 @@ public:
     using nlCache = std::unique_ptr<nl_cache, std::function<void(nl_cache*)>>;
     using nlLink = std::unique_ptr<rtnl_link, std::function<void(rtnl_link*)>>;
     using nlNeigh = std::unique_ptr<rtnl_neigh, std::function<void(rtnl_neigh*)>>;
+    using nlRoute = std::unique_ptr<rtnl_route, std::function<void(rtnl_route*)>>;
 
     using LinkCB = std::function<void(rtnl_link* link, int cacheAction)>; /// cacheAction: NL_ACT_*
     using AddrCB = std::function<void(rtnl_addr* addr, int cacheAction)>; /// cacheAction: NL_ACT_*
@@ -37,6 +39,7 @@ public:
     Rtnetlink(LinkCB cbLink, AddrCB cbAddr);
     ~Rtnetlink();
     std::vector<nlLink> getLinks();
+    std::vector<nlRoute> getRoutes();
     std::vector<std::pair<nlNeigh, nlLink>> getNeighbours();
 
     void invokeInitialCallbacks();
@@ -49,8 +52,10 @@ private:
     nlCacheManager m_nlCacheManager; // for updates
     nl_cache* m_nlManagedCacheLink;
     nl_cache* m_nlManagedCacheAddr;
+    nl_cache* m_nlManagedCacheRoute;
     nlCache m_nlCacheLink; // for getLinks, getNeighbours
     nlCache m_nlCacheNeighbour; // for getNeighbours
+    nlCache m_nlCacheRoute; // for getRoutes
     LinkCB m_cbLink;
     AddrCB m_cbAddr;
     std::unique_ptr<impl::nlCacheMngrWatcher> m_nlCacheMngrWatcher; // first to destroy, because the thread uses m_nlCacheManager and m_log

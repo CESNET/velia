CESNET/velia

commit f10b9308d6e89d4a48a9852820cea496b33bffc5

system: Implement ietf-interfaces
This commit is a start of implementing ietf-interfaces YANG model. It
initialises list of interfaces (as provided by rtnetlink) and fills
few leafs (physical address, and mandatory leafs operational status
and type).

Change-Id: I6c42ae2b3f493256e5b1eef8690cc1ebea21577c

src/system/IETFInterfaces.cpp
@@ -1,0 +1,129 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#include <linux/if_arp.h>
+#include <linux/netdevice.h>
+#include "IETFInterfaces.h"
+#include "Rtnetlink.h"
+#include "utils/log.h"
+#include "utils/sysrepo.h"
+
+using namespace std::string_literals;
+
+namespace {
+
+const auto CZECHLIGHT_NETWORK_MODULE_NAME = "czechlight-network"s;
+const auto IETF_IP_MODULE_NAME = "ietf-ip"s;
+const auto IETF_INTERFACES_MODULE_NAME = "ietf-interfaces"s;
+const auto IETF_INTERFACES = "/"s + IETF_INTERFACES_MODULE_NAME + ":interfaces"s;
+
+const auto PHYS_ADDR_BUF_SIZE = 6 * 2 /* 2 chars per 6 bytes in the address */ + 5 /* delimiters (':') between bytes */ + 1 /* \0 */;
+
+std::string operStatusToString(uint8_t operStatus, velia::Log log)
+{
+    // unfortunately we can't use libnl's rtnl_link_operstate2str, because it creates different strings than the YANG model expects
+    switch (operStatus) {
+    case IF_OPER_UP:
+        return "up";
+    case IF_OPER_DOWN:
+        return "down";
+    case IF_OPER_TESTING:
+        return "testing";
+    case IF_OPER_DORMANT:
+        return "dormant";
+    case IF_OPER_NOTPRESENT:
+        return "not-present";
+    case IF_OPER_LOWERLAYERDOWN:
+        return "lower-layer-down";
+    case IF_OPER_UNKNOWN:
+        return "unknown";
+    default:
+        log->warn("Encountered unknown operational status {}, using 'unknown'", operStatus);
+        return "unknown";
+    }
+}
+
+std::string arpTypeToString(unsigned int arptype, velia::Log log)
+{
+    switch (arptype) {
+    case ARPHRD_ETHER:
+        return "iana-if-type:ethernetCsmacd";
+    case ARPHRD_LOOPBACK:
+        return "iana-if-type:softwareLoopback";
+    case ARPHRD_SIT:
+        return "iana-if-type:sixToFour";
+    default:
+        log->warn("Encountered unknown interface type {}, using 'iana-if-type:other'", arptype);
+        return "iana-if-type:other";
+    }
+}
+
+std::string nlActionToString(int action)
+{
+    switch (action) {
+    case NL_ACT_NEW:
+        return "NEW";
+    case NL_ACT_DEL:
+        return "DEL";
+    case NL_ACT_CHANGE:
+        return "CHANGE";
+    case NL_ACT_UNSPEC:
+        return "UNSPEC";
+    case NL_ACT_GET:
+        return "GET";
+    case NL_ACT_SET:
+        return "SET";
+    default:
+        return "<unknown action>";
+    }
+}
+
+}
+
+namespace velia::system {
+
+IETFInterfaces::IETFInterfaces(std::shared_ptr<::sysrepo::Session> srSess)
+    : m_srSession(std::move(srSess))
+    , m_log(spdlog::get("system"))
+    , m_rtnetlink(std::make_shared<Rtnetlink>([this](rtnl_link* link, int action) { onLinkUpdate(link, action); }))
+{
+    utils::ensureModuleImplemented(m_srSession, IETF_INTERFACES_MODULE_NAME, "2018-02-20");
+    utils::ensureModuleImplemented(m_srSession, IETF_IP_MODULE_NAME, "2018-02-22");
+    utils::ensureModuleImplemented(m_srSession, CZECHLIGHT_NETWORK_MODULE_NAME, "2021-02-22");
+}
+
+void IETFInterfaces::onLinkUpdate(rtnl_link* link, int action)
+{
+    char* name = rtnl_link_get_name(link);
+    m_log->trace("Netlink update on link '{}', action {}", name, nlActionToString(action));
+
+    if (action == NL_ACT_DEL) {
+        utils::valuesPush({}, {IETF_INTERFACES + "/interface[name='" + name + "']/"}, m_srSession, SR_DS_OPERATIONAL);
+    } else if (action == NL_ACT_CHANGE || action == NL_ACT_NEW) {
+        std::map<std::string, std::string> values;
+        std::vector<std::string> deletePaths;
+
+        std::array<char, PHYS_ADDR_BUF_SIZE> buf;
+        if (auto physAddr = nl_addr2str(rtnl_link_get_addr(link), buf.data(), buf.size()); physAddr != "none"s) { // set physical address if the link has one
+            values[IETF_INTERFACES + "/interface[name='" + name + "']/phys-address"] = physAddr;
+        } else {
+            // delete physical address from sysrepo if not provided by rtnetlink
+            // Note: During testing I have noticed that my wireless interface loses a physical address. There were several change callbacks invoked
+            // when simply bringing the interface down and up. In some of those, nl_addr2str returned "none".
+            deletePaths.push_back({IETF_INTERFACES + "/interface[name='" + name + "']/phys-address"});
+        }
+
+        values[IETF_INTERFACES + "/interface[name='" + name + "']/type"] = arpTypeToString(rtnl_link_get_arptype(link), m_log);
+        values[IETF_INTERFACES + "/interface[name='" + name + "']/oper-status"] = operStatusToString(rtnl_link_get_operstate(link), m_log);
+
+        utils::valuesPush(values, deletePaths, m_srSession, SR_DS_OPERATIONAL);
+    } else {
+        m_log->warn("Unhandled cache update action {} ({})", action, nlActionToString(action));
+    }
+}
+
+}

CMakeLists.txt
@@ -208,6 +208,8 @@ add_library(velia-system STATIC
         src/system/Network.h
         src/system/Rtnetlink.cpp
         src/system/Rtnetlink.h
+        src/system/IETFInterfaces.cpp
+        src/system/IETFInterfaces.h
         )
 target_link_libraries(velia-system
     PUBLIC
@@ -410,6 +412,18 @@ if(BUILD_TESTING)
     set_tests_properties(
             test-sysrepo_system-network
             PROPERTIES FIXTURES_REQUIRED sysrepo:env:sysrepo-czechlight-system
+            RESOURCE_LOCK sysrepo
+    )
+
+    sysrepo_fixture_env(sysrepo-czechlight-network
+            YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/iana-if-type@2017-01-19.yang
+            YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-interfaces@2018-02-20.yang
+            YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-ip@2018-02-22.yang
+            YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/czechlight-network@2021-02-22.yang)
+    velia_test(sysrepo_system-ietfinterfaces velia-system)
+    set_tests_properties(
+            test-sysrepo_system-ietfinterfaces
+            PROPERTIES FIXTURES_REQUIRED sysrepo:env:sysrepo-czechlight-network
             RESOURCE_LOCK sysrepo
     )
 

src/system/IETFInterfaces.h
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+#pragma once
+
+#include <sysrepo-cpp/Session.hpp>
+#include "utils/log-fwd.h"
+
+struct rtnl_link;
+
+namespace velia::system {
+
+class Rtnetlink;
+
+class IETFInterfaces {
+public:
+    explicit IETFInterfaces(std::shared_ptr<::sysrepo::Session> srSess);
+
+private:
+    void onLinkUpdate(rtnl_link* link, int action);
+
+    std::shared_ptr<::sysrepo::Session> m_srSession;
+    velia::Log m_log;
+    std::shared_ptr<Rtnetlink> m_rtnetlink; // first to destroy, because the callback to rtnetlink uses m_srSession and m_log
+};
+}

tests/sysrepo_system-ietfinterfaces.cpp
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+*/
+
+#include "trompeloeil_doctest.h"
+#include "pretty_printers.h"
+#include "system/IETFInterfaces.h"
+#include "test_log_setup.h"
+#include "test_sysrepo_helpers.h"
+#include "tests/mock/system.h"
+
+TEST_CASE("ietf-interfaces localhost")
+{
+    TEST_SYSREPO_INIT_LOGS;
+    TEST_SYSREPO_INIT;
+    TEST_SYSREPO_INIT_CLIENT;
+
+    auto network = std::make_shared<velia::system::IETFInterfaces>(srSess);
+
+    // We didn't came up with some way of mocking netlink. At least check that there is the loopback
+    // interface with expected values. It is *probably* safe to assume that there is at least the lo device.
+    REQUIRE(dataFromSysrepo(client, "/ietf-interfaces:interfaces/interface[name='lo']", SR_DS_OPERATIONAL) == std::map<std::string, std::string> {
+                {"/name", "lo"},
+                {"/type", "iana-if-type:softwareLoopback"},
+                {"/phys-address", "00:00:00:00:00:00"},
+                {"/oper-status", "unknown"},
+            });
+}

commit c9a57c3393edba3ce62e5c2fb9542218d1390d8b

system: fix path for node removal
Fix wrong path for node deletion when a link is deleted. Remove the
trailing slash which resulted in an error in libyang and the program
crashed because of an uncaught exception.

 16.777 [393546 system T] Netlink update on link 'veth1', action DEL
 16.777 [393546 main T] Processing node deletion /ietf-interfaces:interfaces/interface[name='veth1']/
 libyang[0]: Unexpected character(s) '
 terminate called after throwing an instance of 'std::runtime_error'
  what():  Unexpected character(s) '

Fixes: f10b930 ("system: Implement ietf-interfaces")
Change-Id: I06abd2f67f6ca6d5ee8bfb871526ccb9a40ee675

src/system/IETFInterfaces.cpp
@@ -130,7 +130,7 @@ void IETFInterfaces::onLinkUpdate(rtnl_link* link, int action)
     m_log->trace("Netlink update on link '{}', action {}", name, nlActionToString(action));
 
     if (action == NL_ACT_DEL) {
-        utils::valuesPush({}, {IETF_INTERFACES + "/interface[name='" + name + "']/"}, m_srSession, SR_DS_OPERATIONAL);
+        utils::valuesPush({}, {IETF_INTERFACES + "/interface[name='" + name + "']"}, m_srSession, SR_DS_OPERATIONAL);
     } else if (action == NL_ACT_CHANGE || action == NL_ACT_NEW) {
         std::map<std::string, std::string> values;
         std::vector<std::string> deletePaths;
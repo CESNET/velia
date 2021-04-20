44dcb7c77aeddcd4dbde64cd057cb1d7740e713b
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-04-20 20:14:13 +0200

    system: Report LED state via sysrepo
    In the previous commits czechlight-system YANG model was extended with
    container for LED status reporting. This commit introduces
    implementation of the model.
    
    Change-Id: Ie2d257737f144085306d6c157d14ac500b3dee71

---
+++ src/system/LED.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+#pragma once
+
+#include <filesystem>
+#include <sysrepo-cpp/Session.hpp>
+#include "utils/log-fwd.h"
+
+namespace velia::system {
+
+class LED {
+public:
+    LED(const std::shared_ptr<::sysrepo::Connection>& srConn, std::filesystem::path sysfsLeds);
+
+private:
+    velia::Log m_log;
+    std::filesystem::path m_sysfsLeds;
+    std::shared_ptr<::sysrepo::Session> m_srSession;
+    std::shared_ptr<::sysrepo::Subscribe> m_srSubscribe;
+};
+}
---
+++ tests/sysrepo_system-leds.cpp
@@ -0,0 +1,43 @@
+#include "trompeloeil_doctest.h"
+#include "dbus-helpers/dbus_rauc_server.h"
+#include "pretty_printers.h"
+#include "system/LED.h"
+#include "test_log_setup.h"
+#include "test_sysrepo_helpers.h"
+#include "tests/configure.cmake.h"
+
+using namespace std::literals;
+
+TEST_CASE("Sysrepo reports system LEDs")
+{
+    trompeloeil::sequence seq1;
+
+    TEST_SYSREPO_INIT_LOGS;
+    TEST_SYSREPO_INIT;
+    TEST_SYSREPO_INIT_CLIENT;
+
+    auto fakeSysfsDir = std::filesystem::path {CMAKE_CURRENT_SOURCE_DIR + "/tests/sysfs/leds/"s};
+    velia::system::LED led(srConn, fakeSysfsDir);
+
+    std::this_thread::sleep_for(10ms);
+
+    REQUIRE(dataFromSysrepo(client, "/czechlight-system:leds", SR_DS_OPERATIONAL) == std::map<std::string, std::string> {
+                {"/led[name='line:green']", ""},
+                {"/led[name='line:green']/brightness", "1"},
+                {"/led[name='line:green']/max_brightness", "1"},
+                {"/led[name='line:green']/name", "line:green"},
+                {"/led[name='uid:blue']", ""},
+                {"/led[name='uid:blue']/brightness", "0"},
+                {"/led[name='uid:blue']/max_brightness", "255"},
+                {"/led[name='uid:blue']/name", "uid:blue"},
+                {"/led[name='uid:green']", ""},
+                {"/led[name='uid:green']/brightness", "0"},
+                {"/led[name='uid:green']/max_brightness", "255"},
+                {"/led[name='uid:green']/name", "uid:green"},
+                {"/led[name='uid:red']", ""},
+                {"/led[name='uid:red']/brightness", "255"},
+                {"/led[name='uid:red']/max_brightness", "255"},
+                {"/led[name='uid:red']/name", "uid:red"},
+
+            });
+}

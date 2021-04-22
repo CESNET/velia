d92245835b28caf1e4a500ff46444e6e2eb001cc
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-04-22 17:44:30 +0200

    health: status LED maximum brightness is 256
    As per /sys/class/leds/status:*/max_brightness.
    
    Change-Id: I6c2b6b2d31a0744da3ad089b341473d6e2613ff1

--- src/health/outputs/callables.cpp
+++ src/health/outputs/callables.cpp
@@ -34,18 +34,18 @@ void LedOutputCallback::operator()(State state)
 {
     switch (state) {
     case State::ERROR:
-        m_redLed->set(255);
+        m_redLed->set(256);
         m_greenLed->set(0);
         m_blueLed->set(0);
         break;
     case State::WARNING:
-        m_redLed->set(255);
+        m_redLed->set(256);
         m_greenLed->set(160);
         m_blueLed->set(0);
         break;
     case State::OK:
         m_redLed->set(0);
-        m_greenLed->set(255);
+        m_greenLed->set(256);
         m_blueLed->set(0);
         break;
     }

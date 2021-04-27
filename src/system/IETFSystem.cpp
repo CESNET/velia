CESNET/velia

commit 51f1d6d8e790149e76c14a61b257ef339f78af55

ietf-system: show current wall clock
Change-Id: Ide62d5fb91ab7c0b5490a106f2694da8613ebffd

src/system/IETFSystem.cpp
@@ -13,6 +13,7 @@
 #include "utils/io.h"
 #include "utils/log.h"
 #include "utils/sysrepo.h"
+#include "utils/time.h"
 
 using namespace std::literals;
 
@@ -21,6 +22,7 @@ namespace {
 const auto IETF_SYSTEM_MODULE_NAME = "ietf-system"s;
 const auto IETF_SYSTEM_STATE_MODULE_PREFIX = "/"s + IETF_SYSTEM_MODULE_NAME + ":system-state/"s;
 const auto IETF_SYSTEM_HOSTNAME_PATH = "/ietf-system:system/hostname";
+const auto IETF_SYSTEM_STATE_CLOCK_PATH = "/ietf-system:system-state/clock";
 
 /** @brief Returns key=value pairs from (e.g. /etc/os-release) as a std::map */
 std::map<std::string, std::string> parseKeyValueFile(const std::filesystem::path& path)
@@ -159,6 +161,23 @@ void IETFSystem::initDummies()
     }
 }
 
+/** @short Time and clock callbacks */
+void IETFSystem::initClock()
+{
+    m_srSubscribe->oper_get_items_subscribe(IETF_SYSTEM_MODULE_NAME.c_str(),
+            [this] (auto session, auto, auto, auto, auto, auto& parent) {
+                m_log->critical("yay");
+                parent->new_path(session->get_context(),
+                        (IETF_SYSTEM_STATE_CLOCK_PATH + "/current-datetime"s).c_str(),
+                        utils::yangTimeFormat(std::chrono::system_clock::now()).c_str(),
+                        LYD_ANYDATA_CONSTSTRING,
+                        0);
+                return SR_ERR_OK;
+            },
+            IETF_SYSTEM_STATE_CLOCK_PATH,
+            SR_SUBSCR_OPER_MERGE);
+}
+
 /** This class handles multiple system properties and publishes them via the ietf-system model:
  * - OS-identification data from osRelease file
  * - Rebooting
@@ -173,5 +192,6 @@ IETFSystem::IETFSystem(std::shared_ptr<::sysrepo::Session> srSession, const std:
     initSystemRestart();
     initHostname();
     initDummies();
+    initClock();
 }
 }

src/system/IETFSystem.h
@@ -21,6 +21,7 @@ private:
     void initSystemRestart();
     void initHostname();
     void initDummies();
+    void initClock();
 
     std::shared_ptr<::sysrepo::Session> m_srSession;
     std::shared_ptr<::sysrepo::Subscribe> m_srSubscribe;

tests/sysrepo_system-ietfsystem.cpp
@@ -28,11 +28,9 @@ TEST_CASE("Sysrepo ietf-system")
             {
                 file = CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release";
                 expected = {
-                    {"/clock", ""},
-                    {"/platform", ""},
-                    {"/platform/os-name", "CzechLight"},
-                    {"/platform/os-release", "v4-105-g8294175-dirty"},
-                    {"/platform/os-version", "v4-105-g8294175-dirty"},
+                    {"/os-name", "CzechLight"},
+                    {"/os-release", "v4-105-g8294175-dirty"},
+                    {"/os-version", "v4-105-g8294175-dirty"},
                 };
             }
 
@@ -40,11 +38,9 @@ TEST_CASE("Sysrepo ietf-system")
             {
                 file = CMAKE_CURRENT_SOURCE_DIR "/tests/system/missing-equal";
                 expected = {
-                    {"/clock", ""},
-                    {"/platform", ""},
-                    {"/platform/os-name", ""},
-                    {"/platform/os-release", ""},
-                    {"/platform/os-version", ""},
+                    {"/os-name", ""},
+                    {"/os-release", ""},
+                    {"/os-version", ""},
                 };
             }
 
@@ -52,16 +48,14 @@ TEST_CASE("Sysrepo ietf-system")
             {
                 file = CMAKE_CURRENT_SOURCE_DIR "/tests/system/empty-values";
                 expected = {
-                    {"/clock", ""},
-                    {"/platform", ""},
-                    {"/platform/os-name", ""},
-                    {"/platform/os-release", ""},
-                    {"/platform/os-version", ""},
+                    {"/os-name", ""},
+                    {"/os-release", ""},
+                    {"/os-version", ""},
                 };
             }
 
             auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess, file);
-            REQUIRE(dataFromSysrepo(client, modulePrefix, SR_DS_OPERATIONAL) == expected);
+            REQUIRE(dataFromSysrepo(client, modulePrefix + "/platform", SR_DS_OPERATIONAL) == expected);
         }
 
         SECTION("Invalid data (missing VERSION and NAME keys)")
@@ -90,6 +84,13 @@ TEST_CASE("Sysrepo ietf-system")
         client->set_item_str(xpath, "lamparna");
 
         REQUIRE(!!client->get_item(xpath));
+    }
+
+    SECTION("clock")
+    {
+        auto sys = std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release");
+        client->session_switch_ds(SR_DS_OPERATIONAL);
+        REQUIRE(!!client->get_item("/ietf-system:system-state/clock/current-datetime"));
     }
 
 #ifdef TEST_RPC_SYSTEM_REBOOT

commit eff77e60011f866f824c3b47da472792a7b8b24a

Remove forgotten debugging
Fixes: 51f1d6d ("ietf-system: show current wall clock")
Change-Id: I5acb36836e21b825bc109c4360fc4bd4ca37098f

src/system/IETFSystem.cpp
@@ -166,7 +166,6 @@ void IETFSystem::initClock()
 {
     m_srSubscribe->oper_get_items_subscribe(IETF_SYSTEM_MODULE_NAME.c_str(),
             [this] (auto session, auto, auto, auto, auto, auto& parent) {
-                m_log->critical("yay");
                 parent->new_path(session->get_context(),
                         (IETF_SYSTEM_STATE_CLOCK_PATH + "/current-datetime"s).c_str(),
                         utils::yangTimeFormat(std::chrono::system_clock::now()).c_str(),
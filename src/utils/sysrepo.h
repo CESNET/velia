1d75cd25eef0f63b0e1311701c618b163ef935ad
Tomáš Pecka <tomas.pecka@cesnet.cz>
2021-04-28 12:38:11 +0200

    utils: add valuesPush/valuesToYang overloads with data stored in std::vector
    We have encountered an issue [1] with key-less lists in libyang where
    passing the data to utils::valuesPush and utils::valuesToYang with
    map<string, string> was causing problems because of the default
    comparator on keys (strings). It did not worked as expected with
    numerical values inside the XPaths (e.g., /a:b/lst[10] is sorted before
    /a:b/lst[1]).
    
    Let's introduce overloads for utils::valuesPush and utils::valuesToYang
    which work with the data sorted by us by using std::vector instead of
    std::map.
    
    See next commit for the usage.
    
    [1] https://github.com/CESNET/velia/issues/2
    
    Change-Id: Idec62cab1ccb3b3d30a50d563f20fcb08121c208

--- src/utils/sysrepo.h
+++ src/utils/sysrepo.h
@@ -12,9 +12,19 @@
 
 namespace velia::utils {
 
+struct YANGPair {
+    std::string xpath;
+    std::string value;
+};
+
+void valuesToYang(const std::vector<YANGPair>& values, const std::vector<std::string>& removePaths, std::shared_ptr<::sysrepo::Session> session, std::shared_ptr<libyang::Data_Node>& parent);
 void valuesToYang(const std::map<std::string, std::string>& values, const std::vector<std::string>& removePaths, std::shared_ptr<::sysrepo::Session> session, std::shared_ptr<libyang::Data_Node>& parent);
+
 void valuesPush(const std::map<std::string, std::string>& values, const std::vector<std::string>& removePaths, std::shared_ptr<::sysrepo::Session> session);
+void valuesPush(const std::vector<YANGPair>& values, const std::vector<std::string>& removePaths, std::shared_ptr<::sysrepo::Session> session);
 void valuesPush(const std::map<std::string, std::string>& values, const std::vector<std::string>& removePaths, std::shared_ptr<::sysrepo::Session> session, sr_datastore_t datastore);
+void valuesPush(const std::vector<YANGPair>& values, const std::vector<std::string>& removePaths, std::shared_ptr<::sysrepo::Session> session, sr_datastore_t datastore);
+
 void initLogsSysrepo();
 void ensureModuleImplemented(std::shared_ptr<::sysrepo::Session> session, const std::string& module, const std::string& revision);
 

281af6befffa10d0def0d2705418fe4e57e8cfd1
Václav Kubernát <kubernat@cesnet.cz>
2021-05-14 03:07:29 +0200

    Initialize PSUs for g1 devices
    Change-Id: I89be10756c1748f1b049ee69b19998e9e6a22fc8

--- src/ietf-hardware/Factory.cpp
+++ src/ietf-hardware/Factory.cpp
@@ -5,15 +5,47 @@
 #include "ietf-hardware/sysfs/HWMon.h"
 
 namespace velia::ietf_hardware {
+using velia::ietf_hardware::data_reader::StaticData;
+using velia::ietf_hardware::data_reader::Fans;
+using velia::ietf_hardware::data_reader::SysfsValue;
+using velia::ietf_hardware::data_reader::EMMC;
+using velia::ietf_hardware::data_reader::SensorType;
+
+void createPower(std::shared_ptr<velia::ietf_hardware::IETFHardware> ietfHardware)
+{
+    auto pdu = std::make_shared<velia::ietf_hardware::sysfs::HWMon>("/sys/bus/i2c/devices/2-0025/hwmon");
+    ietfHardware->registerDataReader(StaticData("ne:pdu", "ne", {{"class", "iana-hardware:power-supply"}}));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-12V", "ne:pdu", pdu, 1));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-5V", "ne:pdu", pdu, 2));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-3V3", "ne:pdu", pdu, 3));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-12V", "ne:pdu", pdu, 1));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-5V", "ne:pdu", pdu, 2));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-3V3", "ne:pdu", pdu, 3));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-1", "ne:pdu", pdu, 1));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-2", "ne:pdu", pdu, 2));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-3", "ne:pdu", pdu, 3));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-12V", "ne:pdu", pdu, 1));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-5V", "ne:pdu", pdu, 2));
+    ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-3V3", "ne:pdu", pdu, 3));
+
+    ietfHardware->registerDataReader([psu =
+            std::make_shared<velia::ietf_hardware::FspYhPsu>("/sys/bus/i2c/devices/2-0058/hwmon",
+                                                             "psu1",
+                                                             std::make_shared<TransientI2C>(2, 0x58, "ym2151e"))] {
+        return psu->readValues();
+    });
+
+    ietfHardware->registerDataReader([psu =
+            std::make_shared<velia::ietf_hardware::FspYhPsu>("/sys/bus/i2c/devices/2-0059/hwmon",
+                                                             "psu2",
+                                                             std::make_shared<TransientI2C>(2, 0x59, "ym2151e"))] {
+        return psu->readValues();
+    });
+}
 
 std::shared_ptr<IETFHardware> create(const std::string& applianceName)
 {
     auto ietfHardware = std::make_shared<velia::ietf_hardware::IETFHardware>();
-    using velia::ietf_hardware::data_reader::StaticData;
-    using velia::ietf_hardware::data_reader::Fans;
-    using velia::ietf_hardware::data_reader::SysfsValue;
-    using velia::ietf_hardware::data_reader::EMMC;
-    using velia::ietf_hardware::data_reader::SensorType;
 
     if (applianceName == "czechlight-clearfog") {
         auto hwmonFans = std::make_shared<velia::ietf_hardware::sysfs::HWMon>("/sys/bus/i2c/devices/1-002e/hwmon/");
@@ -31,6 +63,8 @@ std::shared_ptr<IETFHardware> create(const std::string& applianceName)
         ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:ctrl:temperature-internal-0", "ne:ctrl", sysfsTempMII0, 1));
         ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:ctrl:temperature-internal-1", "ne:ctrl", sysfsTempMII1, 1));
         ietfHardware->registerDataReader(EMMC("ne:ctrl:emmc", "ne:ctrl", emmc));
+
+        createPower(ietfHardware);
     } else if (applianceName == "czechlight-clearfog-g2") {
         auto fans = std::make_shared<velia::ietf_hardware::sysfs::HWMon>("/sys/bus/i2c/devices/1-0020/hwmon/");
         auto tempMainBoard = std::make_shared<velia::ietf_hardware::sysfs::HWMon>("/sys/bus/i2c/devices/1-0048/hwmon/");
@@ -55,38 +89,7 @@ std::shared_ptr<IETFHardware> create(const std::string& applianceName)
         ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:ctrl:temperature-internal-1", "ne:ctrl", tempMII1, 1));
         ietfHardware->registerDataReader(EMMC("ne:ctrl:emmc", "ne:ctrl", emmc));
 
-        using velia::ietf_hardware::data_reader::StaticData;
-        using velia::ietf_hardware::data_reader::SysfsValue;
-        using velia::ietf_hardware::data_reader::Fans;
-        using velia::ietf_hardware::data_reader::SensorType;
-        auto pdu = std::make_shared<velia::ietf_hardware::sysfs::HWMon>("/sys/bus/i2c/devices/2-0025/hwmon");
-        ietfHardware->registerDataReader(StaticData("ne:pdu", "ne", {{"class", "iana-hardware:power-supply"}}));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-12V", "ne:pdu", pdu, 1));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-5V", "ne:pdu", pdu, 2));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-3V3", "ne:pdu", pdu, 3));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-12V", "ne:pdu", pdu, 1));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-5V", "ne:pdu", pdu, 2));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-3V3", "ne:pdu", pdu, 3));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-1", "ne:pdu", pdu, 1));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-2", "ne:pdu", pdu, 2));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-3", "ne:pdu", pdu, 3));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-12V", "ne:pdu", pdu, 1));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-5V", "ne:pdu", pdu, 2));
-        ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-3V3", "ne:pdu", pdu, 3));
-
-        ietfHardware->registerDataReader([psu =
-                std::make_shared<velia::ietf_hardware::FspYhPsu>("/sys/bus/i2c/devices/2-0058/hwmon",
-                                                                 "psu1",
-                                                                 std::make_shared<TransientI2C>(2, 0x58, "ym2151e"))] {
-            return psu->readValues();
-        });
-
-        ietfHardware->registerDataReader([psu =
-                std::make_shared<velia::ietf_hardware::FspYhPsu>("/sys/bus/i2c/devices/2-0059/hwmon",
-                                                                 "psu2",
-                                                                 std::make_shared<TransientI2C>(2, 0x59, "ym2151e"))] {
-            return psu->readValues();
-        });
+        createPower(ietfHardware);
     } else {
         throw std::runtime_error("Unknown appliance '" + applianceName + "'");
     }

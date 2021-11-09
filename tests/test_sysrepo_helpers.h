137a2a990ec5a9e1c6f60946631e73ccf08b76fa
Václav Kubernát <kubernat@cesnet.cz>
2021-11-09 01:31:11 +0100

    Migrate to libyang2
    Change-Id: I388536269e790b8b74ea7791c79b180adc5d80a6

--- tests/test_sysrepo_helpers.h
+++ tests/test_sysrepo_helpers.h
@@ -8,71 +8,69 @@
 #include "trompeloeil_doctest.h"
 #include <boost/algorithm/string/predicate.hpp>
 #include <map>
-#include <sysrepo-cpp/Session.hpp>
+#include <sysrepo-cpp/Connection.hpp>
 #include "test_log_setup.h"
 #include "utils/sysrepo.h"
 
 /** @short Return a subtree from sysrepo, compacting the XPath */
-auto dataFromSysrepo(const std::shared_ptr<sysrepo::Session>& session, const std::string& xpath)
+auto dataFromSysrepo(const sysrepo::Session& session, const std::string& xpath)
 {
     spdlog::get("main")->error("dataFrom {}", xpath);
     std::map<std::string, std::string> res;
-    auto vals = session->get_items((xpath + "//*").c_str());
-    REQUIRE(!!vals);
-    for (size_t i = 0; i < vals->val_cnt(); ++i) {
-        const auto& v = vals->val(i);
-        const auto briefXPath = std::string(v->xpath()).substr(boost::algorithm::ends_with(xpath, ":*") ? xpath.size() - 1 : xpath.size());
-        res.emplace(briefXPath, v->val_to_string());
+    auto data = session.getData((xpath + "//*").c_str());
+    REQUIRE(data);
+    for (const auto& sibling : data->siblings()) {
+        for (const auto& node : sibling.childrenDfs()) {
+            if (node.schema().nodeType() == libyang::NodeType::Leaf) {
+                const auto briefXPath = std::string(node.path()).substr(boost::algorithm::ends_with(xpath, ":*") ? xpath.size() - 1 : xpath.size());
+                res.emplace(briefXPath, node.asTerm().valueStr());
+            }
+
+        }
     }
     return res;
 }
 
 /** @short Execute an RPC or action, return result, compacting the XPath. The rpcPath and input gets concatenated. */
-auto rpcFromSysrepo(const std::shared_ptr<sysrepo::Session>& session, const std::string& rpcPath, std::map<std::string, std::string> input)
+auto rpcFromSysrepo(sysrepo::Session session, const std::string& rpcPath, std::map<std::string, std::string> input)
 {
     spdlog::get("main")->info("rpcFromSysrepo {}", rpcPath);
-    auto inputNode = std::make_shared<libyang::Data_Node>(session->get_context(), rpcPath.c_str(), nullptr, LYD_ANYDATA_CONSTSTRING, 0);
+    auto inputNode = session.getContext().newPath(rpcPath.c_str(), nullptr);
     for (const auto& [k, v] : input) {
-        inputNode->new_path(session->get_context(), (rpcPath + "/" + k).c_str(), v.c_str(), LYD_ANYDATA_CONSTSTRING, 0);
+        inputNode.newPath((rpcPath + "/" + k).c_str(), v.c_str());
     }
 
-    auto output = session->rpc_send(inputNode);
-    REQUIRE(!!output);
-
-    // Sysrepo returns a top-level node. I need the node that contains the action and iterate over that instead of the
-    // top-level node.
-    auto actualOutput = output->find_path(rpcPath.c_str())->data().front();
+    auto output = session.sendRPC(inputNode);
 
     std::map<std::string, std::string> res;
-    for (const auto& node : actualOutput->tree_dfs()) {
-        if (node->schema()->nodetype() == LYS_LEAF) {
-            auto leaf = std::make_shared<libyang::Data_Node_Leaf_List>(node);
-            auto path = node->path();
+    for (const auto& node : output.childrenDfs()) {
+        if (node.schema().nodeType() == libyang::NodeType::Leaf) {
+            auto path = std::string{node.path()};
             const auto briefXPath = path.substr(rpcPath.size());
-            res.emplace(briefXPath, leaf->value_str());
+            res.emplace(briefXPath, node.asTerm().valueStr());
         }
     }
     return res;
 }
 
 /** @short Return a subtree from specified sysrepo's datastore, compacting the XPath*/
-auto dataFromSysrepo(const std::shared_ptr<sysrepo::Session>& session, const std::string& xpath, sr_datastore_t datastore)
+auto dataFromSysrepo(sysrepo::Session session, const std::string& xpath, sysrepo::Datastore datastore)
 {
-    sr_datastore_t oldDatastore = session->session_get_ds();
-    session->session_switch_ds(datastore);
+    auto oldDatastore = session.activeDatastore();
+    session.switchDatastore(datastore);
 
     auto res = dataFromSysrepo(session, xpath);
 
-    session->session_switch_ds(oldDatastore);
+    session.switchDatastore(oldDatastore);
     return res;
 }
 
-#define TEST_SYSREPO_INIT                                     \
-    auto srConn = std::make_shared<sysrepo::Connection>();    \
-    auto srSess = std::make_shared<sysrepo::Session>(srConn); \
-    auto srSubs = std::make_shared<sysrepo::Subscribe>(srSess);
+#define TEST_SYSREPO_INIT                      \
+    auto srConn = sysrepo::Connection{};       \
+    auto srSess = srConn.sessionStart();       \
+    std::optional<sysrepo::Subscription> srSubs;
 
-#define TEST_SYSREPO_INIT_CLIENT                                  \
-    auto clientConn = std::make_shared<sysrepo::Connection>();    \
-    auto client = std::make_shared<sysrepo::Session>(clientConn); \
-    auto subscription = std::make_shared<sysrepo::Subscribe>(client);
+#define TEST_SYSREPO_INIT_CLIENT                     \
+    auto clientConn = sysrepo::Connection{};         \
+    auto client = clientConn.sessionStart();         \
+    std::optional<sysrepo::Subscription> subscription;

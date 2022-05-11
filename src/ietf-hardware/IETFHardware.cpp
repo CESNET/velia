CESNET/velia

commit 97e5ea16619c15b1afcf036b3579f3aad2d50d69

Add support for PSUs
Change-Id: Iae77354dc1335286e07cbdaf64d1380b2dddaccc

src/ietf-hardware/IETFHardware.cpp
@@ -150,6 +150,13 @@ std::string getSysfsFilename(const SensorType type, int sysfsChannelNr)
     switch (type) {
         case SensorType::Temperature:
             return "temp"s + std::to_string(sysfsChannelNr) + "_input";
+        case SensorType::Current:
+            return "curr"s + std::to_string(sysfsChannelNr) + "_input";
+        case SensorType::Power:
+            return "power"s + std::to_string(sysfsChannelNr) + "_input";
+        case SensorType::VoltageAC:
+        case SensorType::VoltageDC:
+            return "in"s + std::to_string(sysfsChannelNr) + "_input";
     }
 
     __builtin_unreachable();
@@ -162,6 +169,34 @@ template <> const DataTree sysfsStaticData<SensorType::Temperature> = {
     {"sensor-data/value-scale", "milli"},
     {"sensor-data/value-precision", "0"},
     {"sensor-data/oper-status", "ok"},
+};
+template <> const DataTree sysfsStaticData<SensorType::Current> = {
+    {"class", "iana-hardware:sensor"},
+    {"sensor-data/value-type", "amperes"},
+    {"sensor-data/value-scale", "milli"},
+    {"sensor-data/value-precision", "0"},
+    {"sensor-data/oper-status", "ok"},
+};
+template <> const DataTree sysfsStaticData<SensorType::Power> = {
+    {"class", "iana-hardware:sensor"},
+    {"sensor-data/value-type", "watts"},
+    {"sensor-data/value-scale", "micro"},
+    {"sensor-data/value-precision", "0"},
+    {"sensor-data/oper-status", "ok"},
+};
+template <> const DataTree sysfsStaticData<SensorType::VoltageAC> = {
+    {"class", "iana-hardware:sensor"},
+    {"sensor-data/value-type", "volts-AC"},
+    {"sensor-data/value-scale", "micro"},
+    {"sensor-data/value-precision", "0"},
+    {"sensor-data/oper-status", "ok"}
+};
+template <> const DataTree sysfsStaticData<SensorType::VoltageDC> = {
+    {"class", "iana-hardware:sensor"},
+    {"sensor-data/value-type", "volts-DC"},
+    {"sensor-data/value-scale", "micro"},
+    {"sensor-data/value-precision", "0"},
+    {"sensor-data/oper-status", "ok"}
 };
 
 template <SensorType TYPE>
@@ -187,7 +222,11 @@ DataTree SysfsValue<TYPE>::operator()() const
     return res;
 }
 
+template struct SysfsValue<SensorType::Current>;
+template struct SysfsValue<SensorType::Power>;
 template struct SysfsValue<SensorType::Temperature>;
+template struct SysfsValue<SensorType::VoltageAC>;
+template struct SysfsValue<SensorType::VoltageDC>;
 
 EMMC::EMMC(std::string componentName, std::optional<std::string> parent, std::shared_ptr<sysfs::EMMC> emmc)
     : DataReader(std::move(componentName), std::move(parent))

CMakeLists.txt
@@ -124,6 +124,8 @@ add_library(velia-ietf-hardware STATIC
     src/ietf-hardware/sysfs/HWMon.h
     src/ietf-hardware/IETFHardware.cpp
     src/ietf-hardware/IETFHardware.h
+    src/ietf-hardware/FspYhPsu.cpp
+    src/ietf-hardware/FspYhPsu.h
     )
 target_link_libraries(velia-ietf-hardware
     PUBLIC
@@ -434,7 +436,7 @@ if(BUILD_TESTING)
     )
 
     sysrepo_fixture_env(sysrepo-ietf-hardware YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/iana-hardware@2018-03-13.yang YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-hardware@2018-03-13.yang FEATURE hardware-sensor)
-    velia_test(hardware_ietf-hardware velia-ietf-hardware velia-ietf-hardware-sysrepo)
+    velia_test(hardware_ietf-hardware velia-ietf-hardware velia-ietf-hardware-sysrepo FsTestUtils)
     set_tests_properties(
             test-hardware_ietf-hardware
             PROPERTIES FIXTURES_REQUIRED sysrepo:env:sysrepo-ietf-hardware

src/ietf-hardware/Factory.cpp
@@ -1,4 +1,5 @@
 #include "Factory.h"
+#include "FspYhPsu.h"
 #include "ietf-hardware/IETFHardware.h"
 #include "ietf-hardware/sysfs/EMMC.h"
 #include "ietf-hardware/sysfs/HWMon.h"
@@ -53,6 +54,39 @@ std::shared_ptr<IETFHardware> create(const std::string& applianceName)
         ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:ctrl:temperature-internal-0", "ne:ctrl", tempMII0, 1));
         ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:ctrl:temperature-internal-1", "ne:ctrl", tempMII1, 1));
         ietfHardware->registerDataReader(EMMC("ne:ctrl:emmc", "ne:ctrl", emmc));
+
+        using velia::ietf_hardware::data_reader::StaticData;
+        using velia::ietf_hardware::data_reader::SysfsValue;
+        using velia::ietf_hardware::data_reader::Fans;
+        using velia::ietf_hardware::data_reader::SensorType;
+        auto pdu = std::make_shared<velia::ietf_hardware::sysfs::HWMon>("/sys/bus/i2c/devices/2-0025/hwmon");
+        ietfHardware->registerDataReader(StaticData("ne:pdu", "ne", {{"class", "iana-hardware:power-supply"}}));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-12V", "ne:pdu", pdu, 1));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-5V", "ne:pdu", pdu, 2));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::VoltageDC>("ne:pdu:voltage-3V3", "ne:pdu", pdu, 3));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-12V", "ne:pdu", pdu, 1));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-5V", "ne:pdu", pdu, 2));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Current>("ne:pdu:current-3V3", "ne:pdu", pdu, 3));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-1", "ne:pdu", pdu, 1));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-2", "ne:pdu", pdu, 2));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:pdu:temperature-3", "ne:pdu", pdu, 3));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-12V", "ne:pdu", pdu, 1));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-5V", "ne:pdu", pdu, 2));
+        ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:pdu:power-3V3", "ne:pdu", pdu, 3));
+
+        ietfHardware->registerDataReader([psu =
+                std::make_shared<velia::ietf_hardware::FspYhPsu>("/sys/bus/i2c/devices/2-0058/hwmon",
+                                                                 "psu1",
+                                                                 std::make_shared<TransientI2C>(2, 0x58, "ym2151e"))] {
+            return psu->readValues();
+        });
+
+        ietfHardware->registerDataReader([psu =
+                std::make_shared<velia::ietf_hardware::FspYhPsu>("/sys/bus/i2c/devices/2-0059/hwmon",
+                                                                 "psu2",
+                                                                 std::make_shared<TransientI2C>(2, 0x59, "ym2151e"))] {
+            return psu->readValues();
+        });
     } else {
         throw std::runtime_error("Unknown appliance '" + applianceName + "'");
     }

src/ietf-hardware/FspYhPsu.cpp
@@ -1,0 +1,164 @@
+#include <cstring>
+#include <fcntl.h>
+#include <fstream>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include "FspYhPsu.h"
+#include "utils/log.h"
+#include "utils/UniqueResource.h"
+
+namespace velia::ietf_hardware {
+
+TransientI2C::TransientI2C(const uint8_t bus, const uint8_t address, const std::string& driverName)
+    : m_address(address)
+    , m_driverName(driverName)
+    , m_isPresentPath("/dev/i2c-" + std::to_string(bus))
+    , m_bindPath("/sys/bus/i2c/devices/i2c-" + std::to_string(bus) + "/new_device")
+    , m_unbindPath("/sys/bus/i2c/devices/i2c-" + std::to_string(bus) + "/delete_device")
+{
+    std::ostringstream addressString;
+    addressString << std::showbase << std::hex << int{m_address};
+    m_addressString = addressString.str();
+}
+
+TransientI2C::~TransientI2C() = default;
+
+bool TransientI2C::isPresent() const
+{
+    auto file = open(m_isPresentPath.c_str(), O_RDWR);
+    if (file < 0) {
+        throw std::system_error(errno, std::system_category(), "TransientI2C::isPresent: open()");
+    }
+
+    auto fdClose = make_unique_resource([] {}, [file] {
+        close(file);
+    });
+
+    if (ioctl(file, I2C_SLAVE_FORCE, m_address) < 0) {
+        throw std::system_error(errno, std::system_category(), "TransientI2C::isPresent: ioctl()");
+    }
+
+    char bufferIn[1];
+    return read(file, bufferIn, 1) != -1;
+
+}
+
+void TransientI2C::bind() const
+{
+    spdlog::get("hardware")->info("Registering PSU at {}", m_addressString);
+    std::ofstream ofs(m_bindPath);
+    if (!ofs.is_open()) {
+        throw std::runtime_error("TransientI2C::bind(): can't open file '" + m_bindPath + "'");
+    }
+    ofs << m_driverName << " " << m_addressString;
+    if (ofs.bad()) {
+        throw std::runtime_error("TransientI2C::bind(): can't write file '" + m_bindPath + "'");
+    }
+}
+void TransientI2C::unbind() const
+{
+    spdlog::get("hardware")->info("Deregistering PSU from {}", m_addressString);
+    std::ofstream ofs(m_unbindPath);
+    if (!ofs.is_open()) {
+        throw std::runtime_error("TransientI2C::unbind(): can't open file '" + m_unbindPath + "'");
+    }
+    ofs << m_addressString;
+    if (ofs.bad()) {
+        throw std::runtime_error("TransientI2C::unbind(): can't write file '" + m_unbindPath + "'");
+    }
+}
+
+FspYhPsu::FspYhPsu(const std::filesystem::path& hwmonDir, const std::string& psuName, std::shared_ptr<TransientI2C> i2c)
+    : m_i2c(i2c)
+    , m_hwmonDir(hwmonDir)
+    , m_psuName(psuName)
+{
+    m_exit = false;
+    m_psuWatcher = std::thread([this] {
+
+        while (!m_exit) {
+            if (m_i2c->isPresent()) {
+                if (!std::filesystem::is_directory(m_hwmonDir)) {
+                    m_i2c->bind();
+                }
+
+                // The driver might already be loaded before the program starts. This ensures that the properties still
+                // get initialized if that's the case.
+                if (!m_hwmon) {
+                    std::lock_guard lk(m_mtx);
+                    createPower();
+                }
+            } else if (std::filesystem::is_directory(m_hwmonDir)) {
+                {
+                    std::lock_guard lk(m_mtx);
+                    m_hwmon = nullptr;
+                    m_properties.clear();
+                }
+
+                m_i2c->unbind();
+            }
+
+            // There is a bug, where TSan falsely reports "double lock of a mutex", one here, in the watcher thread and
+            // another, in readValues(). These locks are in a different thread. The report can be fixed by using
+            // different mutexes for this condition variable and for the reading/binding/unbinding. Work around this by
+            // using different mutexes (instead of creating a suppression).
+            // FIXME: This false positive is fixed in LLVM 12, so remove this after it's available.
+            // // https://github.com/google/sanitizers/issues/1259
+            std::unique_lock lock(m_condMtx);
+            m_cond.wait_for(lock, std::chrono::seconds(3));
+        }
+    });
+}
+
+FspYhPsu::~FspYhPsu()
+{
+    m_exit = true;
+    m_psuWatcher.join();
+}
+
+void FspYhPsu::createPower()
+{
+    m_hwmon = std::make_shared<velia::ietf_hardware::sysfs::HWMon>(m_hwmonDir);
+    const auto prefix = "ne:"s + m_psuName;
+    using velia::ietf_hardware::data_reader::StaticData;
+    using velia::ietf_hardware::data_reader::SysfsValue;
+    using velia::ietf_hardware::data_reader::Fans;
+    using velia::ietf_hardware::data_reader::SensorType;
+    m_properties.push_back(StaticData(prefix, "ne", {{"class", "iana-hardware:power-supply"}}));
+    m_properties.push_back(SysfsValue<SensorType::Temperature>(prefix + ":temperature-1", prefix, m_hwmon, 1));
+    m_properties.push_back(SysfsValue<SensorType::Temperature>(prefix + ":temperature-2", prefix, m_hwmon, 2));
+    m_properties.push_back(SysfsValue<SensorType::Current>(prefix + ":current-in", prefix, m_hwmon, 1));
+    m_properties.push_back(SysfsValue<SensorType::Current>(prefix + ":current-12V", prefix, m_hwmon, 2));
+    m_properties.push_back(SysfsValue<SensorType::Current>(prefix + ":current-5Vsb", prefix, m_hwmon, 3));
+    m_properties.push_back(SysfsValue<SensorType::VoltageAC>(prefix + ":voltage-in", prefix, m_hwmon, 1));
+    m_properties.push_back(SysfsValue<SensorType::VoltageDC>(prefix + ":voltage-12V", prefix, m_hwmon, 2));
+    m_properties.push_back(SysfsValue<SensorType::VoltageDC>(prefix + ":voltage-5Vsb", prefix, m_hwmon, 3));
+    m_properties.push_back(SysfsValue<SensorType::Power>(prefix + ":power-in", prefix, m_hwmon, 1));
+    m_properties.push_back(SysfsValue<SensorType::Power>(prefix + ":power-out", prefix, m_hwmon, 2));
+    m_properties.push_back(Fans(prefix + ":fan", prefix, m_hwmon, 1));
+}
+
+DataTree FspYhPsu::readValues()
+{
+    std::map<std::string, std::string> res;
+
+    std::unique_lock lock(m_mtx);
+    for (auto& reader : m_properties) {
+        try {
+            res.merge(reader());
+        } catch (std::logic_error& ex) {
+            // The PSU might get disconnected before the watcher thread is able to react. Because of this, the sysfs
+            // read can fail. We must react to this and catch the exception from readFileInt64. If we cannot get all
+            // data, we'll consider the data we got as invalid, so we'll return an empty map.
+            spdlog::get("hardware")->warn("Couldn't read PSU sysfs data (maybe the PSU was just ejected?): "s + ex.what());
+            lock.unlock();
+            m_cond.notify_all();
+            return {};
+        }
+
+    }
+
+    return res;
+}
+}

src/ietf-hardware/FspYhPsu.h
@@ -1,0 +1,51 @@
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include "IETFHardware.h"
+#include "ietf-hardware/sysfs/HWMon.h"
+
+namespace velia::ietf_hardware {
+class TransientI2C {
+public:
+    TransientI2C(const uint8_t bus, const uint8_t address, const std::string& driverName);
+    virtual ~TransientI2C();
+    virtual bool isPresent() const;
+    virtual void bind() const;
+    virtual void unbind() const;
+private:
+    uint8_t m_address;
+    std::string m_driverName;
+    std::string m_isPresentPath;
+    std::string m_bindPath;
+    std::string m_unbindPath;
+    std::string m_addressString;
+};
+
+/**
+ * This class manages two things:
+ * 1) dynamic loading/unloading of the driver for the PSUs
+ * 2) reading of hwmon values for the PSUs
+ */
+struct FspYhPsu {
+public:
+    FspYhPsu(const std::filesystem::path& hwmonDir, const std::string& psuName, std::shared_ptr<TransientI2C> i2c);
+    ~FspYhPsu();
+    velia::ietf_hardware::DataTree readValues();
+private:
+    std::mutex m_mtx;
+    std::mutex m_condMtx;
+    std::condition_variable m_cond;
+    std::thread m_psuWatcher;
+    std::atomic<bool> m_exit;
+    std::shared_ptr<TransientI2C> m_i2c;
+
+    std::filesystem::path m_hwmonDir;
+    std::string m_psuName;
+
+    std::shared_ptr<velia::ietf_hardware::sysfs::HWMon> m_hwmon;
+    std::vector<std::function<velia::ietf_hardware::DataTree()>> m_properties;
+
+    void createPower();
+};
+}

src/ietf-hardware/IETFHardware.h
@@ -102,7 +102,12 @@ public:
 };
 
 enum class SensorType {
-   Temperature
+   Temperature,
+   Current,
+   VoltageDC,
+   VoltageAC,
+   Power
+
 };
 
 /** @brief Manages a single value from sysfs, data is provided by a sysfs::HWMon object. */

commit 7632360810bd807c3820f138d6bd28a22a014aa5

hardware: fix voltage sensors reporting
As per linux sysfs documentation[1], the voltages reported by sysfs files
are in millivolts, not microvolts.

Thanks to Martin for discovering the bug.

[1]: https://github.com/torvalds/linux/blob/32b9a19a1966fb3124d7ddd60ebd08688be2f3fa/Documentation/ABI/testing/sysfs-class-hwmon#L70

Fixes: 97e5ea16
Change-Id: I1475388eb88d69a13b81b6f042c43f0b439d0732

src/ietf-hardware/IETFHardware.cpp
@@ -186,14 +186,14 @@ template <> const DataTree sysfsStaticData<SensorType::Power> = {
 template <> const DataTree sysfsStaticData<SensorType::VoltageAC> = {
     {"class", "iana-hardware:sensor"},
     {"sensor-data/value-type", "volts-AC"},
-    {"sensor-data/value-scale", "micro"},
+    {"sensor-data/value-scale", "milli"},
     {"sensor-data/value-precision", "0"},
     {"sensor-data/oper-status", "ok"}
 };
 template <> const DataTree sysfsStaticData<SensorType::VoltageDC> = {
     {"class", "iana-hardware:sensor"},
     {"sensor-data/value-type", "volts-DC"},
-    {"sensor-data/value-scale", "micro"},
+    {"sensor-data/value-scale", "milli"},
     {"sensor-data/value-precision", "0"},
     {"sensor-data/oper-status", "ok"}
 };

tests/hardware_ietf-hardware.cpp
@@ -173,14 +173,14 @@ TEST_CASE("HardwareState")
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-in']/sensor-data/oper-status", "ok"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-in']/sensor-data/value", "220000"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-in']/sensor-data/value-precision", "0"},
-            {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-in']/sensor-data/value-scale", "micro"},
+            {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-in']/sensor-data/value-scale", "milli"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-in']/sensor-data/value-type", "volts-AC"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-out']/class", "iana-hardware:sensor"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-out']/parent", "ne:ctrl"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-out']/sensor-data/oper-status", "ok"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-out']/sensor-data/value", "12000"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-out']/sensor-data/value-precision", "0"},
-            {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-out']/sensor-data/value-scale", "micro"},
+            {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-out']/sensor-data/value-scale", "milli"},
             {"/ietf-hardware:hardware/component[name='ne:ctrl:voltage-out']/sensor-data/value-type", "volts-DC"},
 
             {"/ietf-hardware:hardware/component[name='ne:ctrl:current']/class", "iana-hardware:sensor"},
@@ -360,7 +360,7 @@ TEST_CASE("HardwareState")
                 {"[name='ne:ctrl:voltage-in']/sensor-data/oper-status", "ok"},
                 {"[name='ne:ctrl:voltage-in']/sensor-data/value", "220000"},
                 {"[name='ne:ctrl:voltage-in']/sensor-data/value-precision", "0"},
-                {"[name='ne:ctrl:voltage-in']/sensor-data/value-scale", "micro"},
+                {"[name='ne:ctrl:voltage-in']/sensor-data/value-scale", "milli"},
                 {"[name='ne:ctrl:voltage-in']/sensor-data/value-type", "volts-AC"},
                 {"[name='ne:ctrl:voltage-out']", ""},
                 {"[name='ne:ctrl:voltage-out']/name", "ne:ctrl:voltage-out"},
@@ -370,7 +370,7 @@ TEST_CASE("HardwareState")
                 {"[name='ne:ctrl:voltage-out']/sensor-data/oper-status", "ok"},
                 {"[name='ne:ctrl:voltage-out']/sensor-data/value", "12000"},
                 {"[name='ne:ctrl:voltage-out']/sensor-data/value-precision", "0"},
-                {"[name='ne:ctrl:voltage-out']/sensor-data/value-scale", "micro"},
+                {"[name='ne:ctrl:voltage-out']/sensor-data/value-scale", "milli"},
                 {"[name='ne:ctrl:voltage-out']/sensor-data/value-type", "volts-DC"},
 
                 {"[name='ne:ctrl:current']", ""},
@@ -574,21 +574,21 @@ TEST_CASE("FspYhPsu")
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-12V']/sensor-data/oper-status", "ok"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-12V']/sensor-data/value", "0"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-12V']/sensor-data/value-precision", "0"},
-                {"/ietf-hardware:hardware/component[name='ne:psu:voltage-12V']/sensor-data/value-scale", "micro"},
+                {"/ietf-hardware:hardware/component[name='ne:psu:voltage-12V']/sensor-data/value-scale", "milli"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-12V']/sensor-data/value-type", "volts-DC"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-5Vsb']/class", "iana-hardware:sensor"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-5Vsb']/parent", "ne:psu"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-5Vsb']/sensor-data/oper-status", "ok"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-5Vsb']/sensor-data/value", "0"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-5Vsb']/sensor-data/value-precision", "0"},
-                {"/ietf-hardware:hardware/component[name='ne:psu:voltage-5Vsb']/sensor-data/value-scale", "micro"},
+                {"/ietf-hardware:hardware/component[name='ne:psu:voltage-5Vsb']/sensor-data/value-scale", "milli"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-5Vsb']/sensor-data/value-type", "volts-DC"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-in']/class", "iana-hardware:sensor"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-in']/parent", "ne:psu"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-in']/sensor-data/oper-status", "ok"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-in']/sensor-data/value", "0"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-in']/sensor-data/value-precision", "0"},
-                {"/ietf-hardware:hardware/component[name='ne:psu:voltage-in']/sensor-data/value-scale", "micro"},
+                {"/ietf-hardware:hardware/component[name='ne:psu:voltage-in']/sensor-data/value-scale", "milli"},
                 {"/ietf-hardware:hardware/component[name='ne:psu:voltage-in']/sensor-data/value-type", "volts-AC"},
             };
             break;
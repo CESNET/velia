6a2334bda49ff32f987b6dcf8fb22666e2c7f16e
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-07-12 13:57:54 +0200

    health: Report systemd unit states with alarms
    We have been developing sysrepo-ietf-alarms project which implements
    important parts of the ietf-alarms YANG model.
    Let's use ietf-alarms for reporting states of systemd units. If the unit
    is not healthy, we issue or clear alarm (by calling the RPC located at
    /sysrepo-ietf-alarms:create-or-update-alarm).
    
    Also, we used to had the ability to ignore some of the systemd units
    because they were unhealthy but it was okay for us. We will no longer
    ignore units on this level. ietf-alarms will do that for us by letting
    consumers to filter alarms.
    
    One of the next commits will rewrite the LED manager to use alarms.
    The original health manager will be scraped after that.
    
    Change-Id: I08eb07f3d02836b77ca2f43b2cca49469264c97a

---
+++ src/health/alarms/SystemdUnits.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020 - 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+#pragma once
+
+#include <memory>
+#include <sdbus-c++/sdbus-c++.h>
+#include <set>
+#include <sysrepo-cpp/Session.hpp>
+#include "utils/log-fwd.h"
+
+namespace velia::health {
+
+/** @brief Watches for systemd units state via DBus and reports their state changes via ietf-alarms */
+class SystemdUnits {
+public:
+    SystemdUnits(sysrepo::Session session, sdbus::IConnection& connection);
+    SystemdUnits(sysrepo::Session session, sdbus::IConnection& connection, const std::string& busname, const std::string& managerObjectPath, const std::string& managerIface, const std::string& unitIface);
+    ~SystemdUnits();
+
+private:
+    velia::Log m_log;
+
+    sysrepo::Session m_srSession;
+
+    std::string m_busName;
+    std::string m_unitIface;
+    std::unique_ptr<sdbus::IProxy> m_proxyManager;
+
+    /** List of registered unit watchers */
+    std::map<sdbus::ObjectPath, std::unique_ptr<sdbus::IProxy>> m_proxyUnits;
+
+    /** Current unit state. */
+    std::map<std::string, std::pair<std::string, std::string>> m_unitState;
+
+    void registerSystemdUnit(sdbus::IConnection& connection, const std::string& unitName, const sdbus::ObjectPath& unitObjectPath);
+    void onUnitStateChange(const std::string& name, const std::string& activeState, const std::string& nSubState);
+};
+
+}

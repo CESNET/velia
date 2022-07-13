e1f0abdec57bdf84c01a3a3aa1c0ffaa355cb751
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-07-13 19:31:13 +0200

    health: control LEDs via sysrepo and ietf-alarms
    This patch introduces a sysrepo subscription that listens for changes
    in ietf-alarms' operational datastore and whenever the alarm count
    changes it triggers a callback. We write this code with the intention
    to use it for controlling LED colour but it is generic so one can plug
    another stuff that somehow reacts to the number of alarms.
    
    For now we only react on changes in the alarms list. We consider only
    uncleared alarms. We set the output state to OK or ERROR based on the
    count of alarms (if 0 alarms then OK else ERROR) and we let the output
    signal to handle the state change somehow (e.g. light up different LED).
    
    Change-Id: I5958975f246f96762389aae27b1974d5eea4093d

---
+++ src/health/outputs/AlarmsOutputs.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 - 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+#pragma once
+
+#include <boost/signals2/signal.hpp>
+#include <optional>
+#include <sysrepo-cpp/Session.hpp>
+#include <sysrepo-cpp/Subscription.hpp>
+#include <vector>
+#include "health/State.h"
+#include "utils/log-fwd.h"
+
+namespace velia::health {
+
+/** @brief Sysrepo subscription listening for changes in alarms from ietf-alarms model. */
+class AlarmsOutputs {
+public:
+    AlarmsOutputs(sysrepo::Session session, const std::vector<std::function<void(State)>>& outputHandlers);
+    ~AlarmsOutputs();
+
+private:
+    ::boost::signals2::signal<void(State)> m_outputSignal;
+    velia::Log m_log;
+    sysrepo::Session m_srSession;
+    std::optional<sysrepo::Subscription> m_srSubscription;
+};
+
+}

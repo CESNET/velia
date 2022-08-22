d9e32952eaddcef1b199d95a096a824fe3fdb6f0
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-08-22 17:48:58 +0200

    health: populate alarms' alarm-inventory container
    RFC for alarms says that we should populate
    ietf-alarms:alarms/alarm-inventory with data about the alarms. We don't
    have many alarms in velia yet, so this is quite straightforward.
    
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/sysrepo-ietf-alarms/+/5890
    Change-Id: Iae1c237cfc0cc54eff835a6c83962598e5bf96ac

--- src/health/alarms/alarms.h
+++ src/health/alarms/alarms.h
@@ -10,4 +10,5 @@
 
 namespace velia::health {
 void createOrUpdateAlarm(sysrepo::Session session, const std::string& alarmId, const std::optional<std::string>& alarmQualifierType, const std::string& alarmResource, const std::string& severity, const std::string& alarmText);
+void createOrUpdateAlarmInventoryEntry(sysrepo::Session session, const std::string& alarmId, const std::optional<std::string>& alarmQualifierType, const std::vector<std::string>& resource, bool willClear, const std::vector<std::string>& severities, const std::string& description);
 }

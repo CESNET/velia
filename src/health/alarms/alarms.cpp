375ee3e1fea129da5dbca1ab9ee4f6236274bb61
Tomáš Pecka <tomas.pecka@cesnet.cz>
2022-08-22 17:48:58 +0200

    health: populate alarms' alarm-inventory container
    RFC for alarms says that we should populate
    ietf-alarms:alarms/alarm-inventory with data about the alarms. We don't
    have many alarms in velia yet, so this is quite straightforward.
    
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/sysrepo-ietf-alarms/+/5890
    Change-Id: Iae1c237cfc0cc54eff835a6c83962598e5bf96ac

--- src/health/alarms/alarms.cpp
+++ src/health/alarms/alarms.cpp
@@ -4,12 +4,23 @@
  * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
  *
  */
+#include <sysrepo-cpp/Enum.hpp>
 #include "alarms.h"
+#include "utils/UniqueResource.h"
+#include "utils/libyang.h"
 
 using namespace std::string_literals;
 
 namespace {
+const auto alarmInventory = "/ietf-alarms:alarms/alarm-inventory"s;
 const auto alarmRpc = "/sysrepo-ietf-alarms:create-or-update-alarm";
+
+std::optional<libyang::DataNode> getNodeByPath(auto session, const std::string& path) {
+	if (auto data = session.getData(path)) {
+		return data->findPath(path);
+	}
+	return std::nullopt;
+}
 }
 
 namespace velia::health {
@@ -24,5 +35,50 @@ void createOrUpdateAlarm(sysrepo::Session session, const std::string& alarmId, c
     inputNode.newPath(alarmRpc + "/alarm-text"s, text);
 
     session.sendRPC(inputNode);
+}
+
+void createOrUpdateAlarmInventoryEntry(sysrepo::Session session, const std::string& alarmId, const std::optional<std::string>& alarmTypeQualifier, const std::vector<std::string>& resource, bool willClear, const std::vector<std::string>& severities, const std::string& description)
+{
+    sysrepo::Datastore originalDs;
+    auto x = make_unique_resource(
+        [&]() {
+            originalDs = session.activeDatastore();
+            session.switchDatastore(sysrepo::Datastore::Operational);
+        },
+        [&]() {
+            session.switchDatastore(originalDs);
+        });
+
+    const auto prefix = alarmInventory + "/alarm-type[alarm-type-id='" + alarmId + "'][alarm-type-qualifier='" + alarmTypeQualifier.value_or("") + "']";
+	const auto existingInventoryEntry = getNodeByPath(session, prefix);
+
+	// fetch existing entries from leaf-lists resource and severity-level so we don't add duplicate values into them
+	std::set<std::string> existingResources;
+	std::set<std::string> existingSeverities;
+	if (existingInventoryEntry) {
+		for (const auto& r : existingInventoryEntry->findXPath("resource")) {
+			existingResources.insert(r.asTerm().valueStr().data());
+		}
+		for (const auto& s : existingInventoryEntry->findXPath("severity-level")) {
+			existingSeverities.insert(s.asTerm().valueStr().data());
+		}
+	}
+
+    session.setItem(prefix + "/will-clear", willClear ? "true" : "false");
+    session.setItem(prefix + "/description", description);
+
+    for (const auto& r : resource) {
+        if (!existingResources.contains(r)) {
+            session.setItem(prefix + "/resource", r.c_str());
+        }
+    }
+
+    for (const auto& s : severities) {
+        if (!existingSeverities.contains(s)) {
+            session.setItem(prefix + "/severity-level", s.c_str());
+        }
+    }
+
+    session.applyChanges();
 }
 }

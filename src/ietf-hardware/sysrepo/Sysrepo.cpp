44181d02c4e262c8379b5fbdb99135aff0f16473
Tomáš Pecka <tomas.pecka@cesnet.cz>
2023-04-13 15:56:48 +0200

    hardware: push ietf-hardware data into sysrepo periodically
    We want to raise alarms in future, so prepare for that by reading the
    sensors every now and then. It also makes sense to  switch from providing
    ops callbacks for ietf-hardware data to pushing ops data periodically.
    
    Change-Id: If602ab64be674e766ef5d2b88e0a8f68b9cdb6d5

--- src/ietf-hardware/sysrepo/Sysrepo.cpp
+++ src/ietf-hardware/sysrepo/Sysrepo.cpp
@@ -1,40 +1,97 @@
 /*
- * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ * Copyright (C) 2020-2023 CESNET, https://photonics.cesnet.cz/
  *
  * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
  *
  */
 
+#include <boost/algorithm/string/predicate.hpp>
+#include <sysrepo-cpp/Connection.hpp>
 #include "Sysrepo.h"
 #include "utils/log.h"
 #include "utils/sysrepo.h"
 
-namespace velia::ietf_hardware::sysrepo {
-
 namespace {
 
-const auto IETF_HARDWARE_MODULE_NAME = "ietf-hardware"s;
-const auto IETF_HARDWARE_MODULE_PREFIX = "/"s + IETF_HARDWARE_MODULE_NAME + ":hardware/*"s;
+/** @brief Extracts component path prefix from an XPath under /ietf-hardware/component node
+ *
+ * Example input:  /ietf-hardware:hardware/component[name='ne:psu:child']/oper-state/disabled
+ * Example output: /ietf-hardware:hardware/component[name='ne:psu:child']
+ */
+std::string extractComponentPrefix(const std::string& componentXPath)
+{
+    static const std::string prefix = "/ietf-hardware:hardware/component[name=";
 
+    if (!boost::algorithm::starts_with(componentXPath, prefix)) {
+        throw std::logic_error("Invalid XPath '"s + componentXPath + "'");
+    }
+
+    char quotes;
+    if (!componentXPath.empty() && componentXPath[prefix.size()] == '\'') {
+        quotes = '\'';
+    } else if (!componentXPath.empty() && componentXPath[prefix.size()] == '\"') {
+        quotes = '\"';
+    } else {
+        throw std::logic_error("Invalid XPath '"s + componentXPath + "'");
+    }
+
+    auto nextQuote = componentXPath.find_first_of(quotes, prefix.size() + 1);
+    if (nextQuote == std::string::npos) {
+        throw std::logic_error("Invalid XPath '"s + componentXPath + "'");
+    }
+
+    return componentXPath.substr(0, nextQuote + 2 /* quote itself, closing bracket */);
+}
 }
 
-/** @brief The constructor expects the HardwareState instance which will provide the actual hardware state data */
-Sysrepo::Sysrepo(::sysrepo::Session session, std::shared_ptr<IETFHardware> hwState)
-    : m_hwState(std::move(hwState))
-    , m_srSubscribe()
+namespace velia::ietf_hardware::sysrepo {
+
+/** @brief The constructor expects the HardwareState instance which will provide the actual hardware state data and the poll interval */
+Sysrepo::Sysrepo(::sysrepo::Session session, std::shared_ptr<IETFHardware> hwState, std::chrono::microseconds pollInterval)
+    : m_log(spdlog::get("hardware"))
+    , m_pollInterval(std::move(pollInterval))
+    , m_session(std::move(session))
+    , m_hwState(std::move(hwState))
+    , m_quit(false)
+    , m_pollThread([&]() {
+        m_log->trace("Poll thread started");
+        auto conn = m_session.getConnection();
+
+        DataTree prevValues;
+
+        while (!m_quit) {
+            m_log->trace("IetfHardware poll");
+
+            auto hwStateValues = m_hwState->process();
+            std::set<std::string> deletedComponents;
+
+            /* Some data readers can stop returning data in some cases (e.g. ejected PSU).
+             * Prune tree components that were removed before updating to avoid having not current data from previous invocations.
+             */
+            for (const auto& [k, v] : prevValues) {
+                if (!hwStateValues.contains(k)) {
+                    deletedComponents.emplace(extractComponentPrefix(k));
+                }
+            }
+
+            for (const auto& component : deletedComponents) {
+                conn.discardOperationalChanges(component);
+            }
+
+            utils::valuesPush(hwStateValues, {}, m_session, ::sysrepo::Datastore::Operational);
+
+            prevValues = std::move(hwStateValues);
+            std::this_thread::sleep_for(m_pollInterval);
+        }
+        m_log->trace("Poll thread stopped");
+    })
 {
-    ::sysrepo::OperGetCb cb = [this](::sysrepo::Session session, auto, auto, auto, auto, auto, auto& parent) {
-        auto hwStateValues = m_hwState->process();
-        utils::valuesToYang(hwStateValues, {}, session, parent);
+}
 
-        spdlog::get("hardware")->trace("Pushing to sysrepo (JSON): {}", *parent->printStr(::libyang::DataFormat::JSON, libyang::PrintFlags::WithSiblings));
-        return ::sysrepo::ErrorCode::Ok;
-    };
-
-    m_srSubscribe = session.onOperGet(
-        IETF_HARDWARE_MODULE_NAME,
-        cb,
-        IETF_HARDWARE_MODULE_PREFIX,
-        ::sysrepo::SubscribeOptions::Passive | ::sysrepo::SubscribeOptions::OperMerge);
+Sysrepo::~Sysrepo()
+{
+    m_log->trace("Requesting poll thread stop");
+    m_quit = true;
+    m_pollThread.join();
 }
 }
--- tests/sysrepo_ietf-hardware.cpp
+++ tests/sysrepo_ietf-hardware.cpp
@@ -1,4 +1,7 @@
 #include "trompeloeil_doctest.h"
+#include <iterator>
+#include <sysrepo-cpp/Enum.hpp>
+#include <trompeloeil.hpp>
 #include "ietf-hardware/IETFHardware.h"
 #include "ietf-hardware/sysrepo/Sysrepo.h"
 #include "mock/ietf_hardware.h"
@@ -8,6 +11,10 @@
 
 using namespace std::literals;
 
+struct ModuleChangeTester {
+    MAKE_CONST_MOCK1(components, void(const std::set<std::string>&));
+};
+
 TEST_CASE("IETF Hardware with sysrepo")
 {
     TEST_SYSREPO_INIT_LOGS;
@@ -15,89 +22,268 @@ TEST_CASE("IETF Hardware with sysrepo")
     TEST_SYSREPO_INIT_CLIENT;
     static const auto modulePrefix = "/ietf-hardware:hardware"s;
 
+    client.switchDatastore(sysrepo::Datastore::Operational);
+
+    ModuleChangeTester modChangeTester;
+
     trompeloeil::sequence seq1;
-    auto ietfHardware = std::make_shared<velia::ietf_hardware::IETFHardware>();
+
+    auto directLeafNodeQuery = [&](const std::string& xpath) {
+        auto val = client.getData(xpath);
+        REQUIRE(val);
+        return std::string{val->findPath(xpath)->asTerm().valueStr()};
+    };
 
     auto sysfsTempCpu = std::make_shared<FakeHWMon>();
     auto sysfsPower = std::make_shared<FakeHWMon>();
-
-    REQUIRE_CALL(*sysfsTempCpu, attribute("temp1_input")).RETURN(41800);
-    REQUIRE_CALL(*sysfsPower, attribute("power1_input")).RETURN(14000000);
 
     using velia::ietf_hardware::data_reader::SensorType;
     using velia::ietf_hardware::data_reader::StaticData;
     using velia::ietf_hardware::data_reader::SysfsValue;
 
+    std::atomic<bool> psuActive; // this needs to be destroyed after ietfHardware to avoid dangling reference (we are passing it as a ref to PsuDataReader)
+    std::atomic<int64_t> cpuTempValue;
+    std::atomic<int64_t> powerValue;
+
     // register components into hw state
+    auto ietfHardware = std::make_shared<velia::ietf_hardware::IETFHardware>();
     ietfHardware->registerDataReader(StaticData("ne", std::nullopt, {{"class", "iana-hardware:chassis"}, {"mfg-name", "CESNET"s}}));
     ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:temperature-cpu", "ne", sysfsTempCpu, 1));
     ietfHardware->registerDataReader(SysfsValue<SensorType::Power>("ne:power", "ne", sysfsPower, 1));
 
-    auto ietfHardwareSysrepo = std::make_shared<velia::ietf_hardware::sysrepo::Sysrepo>(srSess, ietfHardware);
+    /* Some data readers (like our PSU reader, see the FspYhPsu test) may set oper-state to enabled/disabled depending on whether the device is present and Some
+     * data might not even be pushed (e.g. the child sensors).
+     * Since we push data into sysrepo we have to erase old data (that should no longer be present) from the sysrepo operational DS.
+     * We test such situation via the following data reader which returns data only when psuActive is set to true.
+     */
+    struct PsuDataReader {
+        const std::atomic<bool>& active;
 
-    SECTION("test last-change")
-    {
-        // at least check that there is some timestamp
-        REQUIRE(dataFromSysrepo(client, modulePrefix, sysrepo::Datastore::Operational).count("/last-change") > 0);
-    }
+        velia::ietf_hardware::DataTree operator()()
+        {
+            velia::ietf_hardware::DataTree res = {
+                {"/ietf-hardware:hardware/component[name='ne:psu']/class", "iana-hardware:power-supply"},
+                {"/ietf-hardware:hardware/component[name='ne:psu']/parent", "ne"},
+                {"/ietf-hardware:hardware/component[name='ne:psu']/state/oper-state", "disabled"},
+            };
 
-    SECTION("test components")
-    {
-        std::map<std::string, std::string> expected = {
-            {"[name='ne']", ""},
-            {"[name='ne']/name", "ne"},
-            {"[name='ne']/class", "iana-hardware:chassis"},
-            {"[name='ne']/mfg-name", "CESNET"},
-            {"[name='ne']/state", ""},
-            {"[name='ne']/state/oper-state", "enabled"},
+            if (active) {
+                res["/ietf-hardware:hardware/component[name='ne:psu']/state/oper-state"] = "enabled";
+                res["/ietf-hardware:hardware/component[name='ne:psu:child']/class"] = "iana-hardware:sensor";
+                res["/ietf-hardware:hardware/component[name='ne:psu:child']/parent"] = "ne:psu";
+                res["/ietf-hardware:hardware/component[name='ne:psu:child']/state/oper-state"] = "enabled";
+            }
 
-            {"[name='ne:temperature-cpu']", ""},
-            {"[name='ne:temperature-cpu']/name", "ne:temperature-cpu"},
-            {"[name='ne:temperature-cpu']/class", "iana-hardware:sensor"},
-            {"[name='ne:temperature-cpu']/parent", "ne"},
-            {"[name='ne:temperature-cpu']/state", ""},
-            {"[name='ne:temperature-cpu']/state/oper-state", "enabled"},
-            {"[name='ne:temperature-cpu']/sensor-data", ""},
-            {"[name='ne:temperature-cpu']/sensor-data/oper-status", "ok"},
-            {"[name='ne:temperature-cpu']/sensor-data/value", "41800"},
-            {"[name='ne:temperature-cpu']/sensor-data/value-precision", "0"},
-            {"[name='ne:temperature-cpu']/sensor-data/value-scale", "milli"},
-            {"[name='ne:temperature-cpu']/sensor-data/value-type", "celsius"},
+            return res;
+        }
+    };
+    ietfHardware->registerDataReader(PsuDataReader{psuActive});
 
-            {"[name='ne:power']", ""},
-            {"[name='ne:power']/name", "ne:power"},
-            {"[name='ne:power']/class", "iana-hardware:sensor"},
-            {"[name='ne:power']/parent", "ne"},
-            {"[name='ne:power']/state", ""},
-            {"[name='ne:power']/state/oper-state", "enabled"},
-            {"[name='ne:power']/sensor-data", ""},
-            {"[name='ne:power']/sensor-data/oper-status", "ok"},
-            {"[name='ne:power']/sensor-data/value", "14000000"},
-            {"[name='ne:power']/sensor-data/value-precision", "0"},
-            {"[name='ne:power']/sensor-data/value-scale", "micro"},
-            {"[name='ne:power']/sensor-data/value-type", "watts"},
-        };
+    /* Ensure that there are sane data after each sysrepo change callback (all the component subtrees are expected). */
+    auto changeSub = client.onModuleChange(
+        "ietf-hardware",
+        [&](sysrepo::Session session, auto, auto, auto, auto, auto) {
+            std::set<std::string> componentXPaths;
 
-        REQUIRE(dataFromSysrepo(client, modulePrefix + "/component", sysrepo::Datastore::Operational) == expected);
+            if (auto data = session.getData("/ietf-hardware:hardware/component")) {
+                for (const auto& e : data->findXPath("/ietf-hardware:hardware/component")) {
+                    componentXPaths.emplace(e.path());
+                }
+            }
 
-        // data changes
-        REQUIRE_CALL(*sysfsTempCpu, attribute("temp1_input")).RETURN(222);
-        REQUIRE_CALL(*sysfsPower, attribute("power1_input")).RETURN(333);
+            modChangeTester.components(componentXPaths);
+            return sysrepo::ErrorCode::Ok;
+        },
+        "/ietf-hardware:hardware/component",
+        0,
+        sysrepo::SubscribeOptions::DoneOnly);
 
-        expected["[name='ne:temperature-cpu']/sensor-data/value"] = "222";
-        expected["[name='ne:power']/sensor-data/value"] = "333";
+    // first batch of values
+    cpuTempValue = 41800;
+    powerValue = 14000000;
+    psuActive = true;
+    REQUIRE_CALL(*sysfsTempCpu, attribute("temp1_input")).LR_RETURN(cpuTempValue).TIMES(AT_LEAST(1));
+    REQUIRE_CALL(*sysfsPower, attribute("power1_input")).LR_RETURN(powerValue).TIMES(AT_LEAST(1));
+    REQUIRE_CALL(modChangeTester, components(std::set<std::string>{
+                                      "/ietf-hardware:hardware/component[name='ne']",
+                                      "/ietf-hardware:hardware/component[name='ne:power']",
+                                      "/ietf-hardware:hardware/component[name='ne:temperature-cpu']",
+                                      "/ietf-hardware:hardware/component[name='ne:psu']",
+                                      "/ietf-hardware:hardware/component[name='ne:psu:child']",
+                                  })).TIMES(1 /* addition of new components does not trigger any delete */).IN_SEQUENCE(seq1);
 
-        REQUIRE(dataFromSysrepo(client, modulePrefix + "/component", sysrepo::Datastore::Operational) == expected);
-    }
+    auto ietfHardwareSysrepo = std::make_shared<velia::ietf_hardware::sysrepo::Sysrepo>(srSess, ietfHardware, 150ms);
+    std::this_thread::sleep_for(400ms); // let's wait until the bg polling thread is spawned; 400 ms is probably enough to spawn the thread and poll 2 or 3 times
 
-    SECTION("test leafnode query")
-    {
-        const auto xpath = modulePrefix + "/component[name='ne:power']/class";
-        client.switchDatastore(sysrepo::Datastore::Operational);
-        auto val = client.getData(xpath);
-        client.switchDatastore(sysrepo::Datastore::Running);
-        REQUIRE(val);
-        REQUIRE(val->findPath(xpath)->asTerm().valueStr() == "iana-hardware:sensor"s);
-    }
+    std::string lastChange = directLeafNodeQuery(modulePrefix + "/last-change");
+    REQUIRE(directLeafNodeQuery(modulePrefix + "/component[name='ne:power']/class") == "iana-hardware:sensor"s);
+    REQUIRE(directLeafNodeQuery(modulePrefix + "/component[name='ne:power']/sensor-data/value") == std::to_string(powerValue));
+    REQUIRE(dataFromSysrepo(client, modulePrefix + "/component", sysrepo::Datastore::Operational) == std::map<std::string, std::string>{
+                {"[name='ne']", ""},
+                {"[name='ne']/name", "ne"},
+                {"[name='ne']/class", "iana-hardware:chassis"},
+                {"[name='ne']/mfg-name", "CESNET"},
+                {"[name='ne']/state", ""},
+                {"[name='ne']/state/oper-state", "enabled"},
+
+                {"[name='ne:psu']", ""},
+                {"[name='ne:psu']/name", "ne:psu"},
+                {"[name='ne:psu']/class", "iana-hardware:power-supply"},
+                {"[name='ne:psu']/parent", "ne"},
+                {"[name='ne:psu']/state", ""},
+                {"[name='ne:psu']/state/oper-state", "enabled"},
+                {"[name='ne:psu:child']", ""},
+                {"[name='ne:psu:child']/name", "ne:psu:child"},
+                {"[name='ne:psu:child']/class", "iana-hardware:sensor"},
+                {"[name='ne:psu:child']/parent", "ne:psu"},
+                {"[name='ne:psu:child']/state", ""},
+                {"[name='ne:psu:child']/state/oper-state", "enabled"},
+                {"[name='ne:psu:child']/sensor-data", ""},
+
+                {"[name='ne:temperature-cpu']", ""},
+                {"[name='ne:temperature-cpu']/name", "ne:temperature-cpu"},
+                {"[name='ne:temperature-cpu']/class", "iana-hardware:sensor"},
+                {"[name='ne:temperature-cpu']/parent", "ne"},
+                {"[name='ne:temperature-cpu']/sensor-data", ""},
+                {"[name='ne:temperature-cpu']/sensor-data/oper-status", "ok"},
+                {"[name='ne:temperature-cpu']/sensor-data/value", "41800"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-precision", "0"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-scale", "milli"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-type", "celsius"},
+                {"[name='ne:temperature-cpu']/state", ""},
+                {"[name='ne:temperature-cpu']/state/oper-state", "enabled"},
+
+                {"[name='ne:power']", ""},
+                {"[name='ne:power']/name", "ne:power"},
+                {"[name='ne:power']/class", "iana-hardware:sensor"},
+                {"[name='ne:power']/parent", "ne"},
+                {"[name='ne:power']/sensor-data", ""},
+                {"[name='ne:power']/sensor-data/oper-status", "ok"},
+                {"[name='ne:power']/sensor-data/value", "14000000"},
+                {"[name='ne:power']/sensor-data/value-precision", "0"},
+                {"[name='ne:power']/sensor-data/value-scale", "micro"},
+                {"[name='ne:power']/sensor-data/value-type", "watts"},
+                {"[name='ne:power']/state", ""},
+                {"[name='ne:power']/state/oper-state", "enabled"},
+            });
+
+    // second batch of values, sensor data changed, PSU ejected
+    REQUIRE_CALL(modChangeTester, components(std::set<std::string>{
+                                      "/ietf-hardware:hardware/component[name='ne']",
+                                      "/ietf-hardware:hardware/component[name='ne:power']",
+                                      "/ietf-hardware:hardware/component[name='ne:temperature-cpu']",
+                                      "/ietf-hardware:hardware/component[name='ne:psu']",
+                                  })).TIMES(2 /* one discard call (ne:psu:child), one push with changes to other components */).IN_SEQUENCE(seq1);
+    REQUIRE_CALL(*sysfsTempCpu, attribute("temp1_input")).LR_RETURN(cpuTempValue).TIMES(AT_LEAST(1));
+    REQUIRE_CALL(*sysfsPower, attribute("power1_input")).LR_RETURN(powerValue).TIMES(AT_LEAST(1));
+    cpuTempValue = 222;
+    powerValue = 11222333;
+    psuActive = false;
+
+    std::this_thread::sleep_for(2000ms); // longer sleep here: last-change does not report milliseconds so this should increase last-change timestamp at least by one second
+    REQUIRE(directLeafNodeQuery(modulePrefix + "/last-change") > lastChange); // check that last-change leaf has timestamp that is greater than the previous one
+    REQUIRE(directLeafNodeQuery(modulePrefix + "/component[name='ne:power']/class") == "iana-hardware:sensor"s);
+    REQUIRE(directLeafNodeQuery(modulePrefix + "/component[name='ne:power']/sensor-data/value") == std::to_string(powerValue));
+    REQUIRE(dataFromSysrepo(client, modulePrefix + "/component", sysrepo::Datastore::Operational) == std::map<std::string, std::string>{
+                {"[name='ne']", ""},
+                {"[name='ne']/name", "ne"},
+                {"[name='ne']/class", "iana-hardware:chassis"},
+                {"[name='ne']/mfg-name", "CESNET"},
+                {"[name='ne']/state", ""},
+                {"[name='ne']/state/oper-state", "enabled"},
+
+                {"[name='ne:psu']", ""},
+                {"[name='ne:psu']/name", "ne:psu"},
+                {"[name='ne:psu']/class", "iana-hardware:power-supply"},
+                {"[name='ne:psu']/parent", "ne"},
+                {"[name='ne:psu']/state", ""},
+                {"[name='ne:psu']/state/oper-state", "disabled"},
+
+                {"[name='ne:temperature-cpu']", ""},
+                {"[name='ne:temperature-cpu']/name", "ne:temperature-cpu"},
+                {"[name='ne:temperature-cpu']/class", "iana-hardware:sensor"},
+                {"[name='ne:temperature-cpu']/parent", "ne"},
+                {"[name='ne:temperature-cpu']/sensor-data", ""},
+                {"[name='ne:temperature-cpu']/sensor-data/oper-status", "ok"},
+                {"[name='ne:temperature-cpu']/sensor-data/value", "222"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-precision", "0"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-scale", "milli"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-type", "celsius"},
+                {"[name='ne:temperature-cpu']/state", ""},
+                {"[name='ne:temperature-cpu']/state/oper-state", "enabled"},
+
+                {"[name='ne:power']", ""},
+                {"[name='ne:power']/name", "ne:power"},
+                {"[name='ne:power']/class", "iana-hardware:sensor"},
+                {"[name='ne:power']/parent", "ne"},
+                {"[name='ne:power']/sensor-data", ""},
+                {"[name='ne:power']/sensor-data/oper-status", "ok"},
+                {"[name='ne:power']/sensor-data/value", "11222333"},
+                {"[name='ne:power']/sensor-data/value-precision", "0"},
+                {"[name='ne:power']/sensor-data/value-scale", "micro"},
+                {"[name='ne:power']/sensor-data/value-type", "watts"},
+                {"[name='ne:power']/state", ""},
+                {"[name='ne:power']/state/oper-state", "enabled"},
+            });
+
+    // third batch of changes, wild PSU appears
+    REQUIRE_CALL(modChangeTester, components(std::set<std::string>{
+                                      "/ietf-hardware:hardware/component[name='ne']",
+                                      "/ietf-hardware:hardware/component[name='ne:power']",
+                                      "/ietf-hardware:hardware/component[name='ne:temperature-cpu']",
+                                      "/ietf-hardware:hardware/component[name='ne:psu']",
+                                      "/ietf-hardware:hardware/component[name='ne:psu:child']",
+                                  })).TIMES(1 /* addition of a new component does not trigger any delete */).IN_SEQUENCE(seq1);
+    psuActive = true;
+
+    std::this_thread::sleep_for(400ms);
+    REQUIRE(dataFromSysrepo(client, modulePrefix + "/component", sysrepo::Datastore::Operational) == std::map<std::string, std::string>{
+                {"[name='ne']", ""},
+                {"[name='ne']/name", "ne"},
+                {"[name='ne']/class", "iana-hardware:chassis"},
+                {"[name='ne']/mfg-name", "CESNET"},
+                {"[name='ne']/state", ""},
+                {"[name='ne']/state/oper-state", "enabled"},
+
+                {"[name='ne:psu']", ""},
+                {"[name='ne:psu']/name", "ne:psu"},
+                {"[name='ne:psu']/class", "iana-hardware:power-supply"},
+                {"[name='ne:psu']/parent", "ne"},
+                {"[name='ne:psu']/state", ""},
+                {"[name='ne:psu']/state/oper-state", "enabled"},
+                {"[name='ne:psu:child']", ""},
+                {"[name='ne:psu:child']/name", "ne:psu:child"},
+                {"[name='ne:psu:child']/class", "iana-hardware:sensor"},
+                {"[name='ne:psu:child']/parent", "ne:psu"},
+                {"[name='ne:psu:child']/state", ""},
+                {"[name='ne:psu:child']/state/oper-state", "enabled"},
+                {"[name='ne:psu:child']/sensor-data", ""},
+
+                {"[name='ne:temperature-cpu']", ""},
+                {"[name='ne:temperature-cpu']/name", "ne:temperature-cpu"},
+                {"[name='ne:temperature-cpu']/class", "iana-hardware:sensor"},
+                {"[name='ne:temperature-cpu']/parent", "ne"},
+                {"[name='ne:temperature-cpu']/sensor-data", ""},
+                {"[name='ne:temperature-cpu']/sensor-data/oper-status", "ok"},
+                {"[name='ne:temperature-cpu']/sensor-data/value", "222"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-precision", "0"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-scale", "milli"},
+                {"[name='ne:temperature-cpu']/sensor-data/value-type", "celsius"},
+                {"[name='ne:temperature-cpu']/state", ""},
+                {"[name='ne:temperature-cpu']/state/oper-state", "enabled"},
+
+                {"[name='ne:power']", ""},
+                {"[name='ne:power']/name", "ne:power"},
+                {"[name='ne:power']/class", "iana-hardware:sensor"},
+                {"[name='ne:power']/parent", "ne"},
+                {"[name='ne:power']/sensor-data", ""},
+                {"[name='ne:power']/sensor-data/oper-status", "ok"},
+                {"[name='ne:power']/sensor-data/value", "11222333"},
+                {"[name='ne:power']/sensor-data/value-precision", "0"},
+                {"[name='ne:power']/sensor-data/value-scale", "micro"},
+                {"[name='ne:power']/sensor-data/value-type", "watts"},
+                {"[name='ne:power']/state", ""},
+                {"[name='ne:power']/state/oper-state", "enabled"},
+            });
 }
 

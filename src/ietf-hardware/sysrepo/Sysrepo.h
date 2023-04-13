053374307c8abf2bd8b7e6f3157f0bcb3ce6ddc8
Tomáš Pecka <tomas.pecka@cesnet.cz>
2023-04-13 15:56:48 +0200

    hardware: push ietf-hardware data into sysrepo periodically
    We want to raise alarms in future, so prepare for that by reading the
    sensors every now and then. It also makes sense to  switch from
    providing ops callbacks for ietf-hardware data to pushing ops data
    periodically.
    
    Change-Id: If602ab64be674e766ef5d2b88e0a8f68b9cdb6d5

--- src/ietf-hardware/sysrepo/Sysrepo.h
+++ src/ietf-hardware/sysrepo/Sysrepo.h
@@ -1,32 +1,37 @@
 /*
- * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ * Copyright (C) 2020-2023 CESNET, https://photonics.cesnet.cz/
  *
  * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
  *
  */
 #pragma once
 
+#include <chrono>
 #include <memory>
-#include <optional>
 #include <sysrepo-cpp/Session.hpp>
+#include <thread>
 #include "ietf-hardware/IETFHardware.h"
+#include "utils/log-fwd.h"
 
 namespace velia::ietf_hardware::sysrepo {
 
 /** @class Sysrepo
  *  A callback class for operational data in Sysrepo. This class expects a shared_pointer<HardwareState> instance.
- *  When sysrepo callbacks for the data in the subtree this callback is registered for, it asks HardwareState instace
- *  for the data it should return back to Sysrepo.
- *  OpsCallback then creates the YANG tree structure from the data returned by HardwareState and returns it.
+ *  It asks HardwareState instance for the hardware state data every @p pollInterval interval and it pushes them into Sysrepo.
  *
  *  @see velia::ietf_hardware::IETFHardware
  */
 class Sysrepo {
 public:
-    Sysrepo(::sysrepo::Session session, std::shared_ptr<IETFHardware> driver);
+    Sysrepo(::sysrepo::Session session, std::shared_ptr<IETFHardware> driver, std::chrono::microseconds pollInterval);
+    ~Sysrepo();
 
 private:
+    velia::Log m_log;
+    std::chrono::microseconds m_pollInterval;
+    ::sysrepo::Session m_session;
     std::shared_ptr<IETFHardware> m_hwState;
-    std::optional<::sysrepo::Subscription> m_srSubscribe;
+    std::atomic<bool> m_quit;
+    std::thread m_pollThread;
 };
 }

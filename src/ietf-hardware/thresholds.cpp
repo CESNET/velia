6405cfcebb7dac67a6216100d4bc607bf9555ebb
Tomáš Pecka <tomas.pecka@cesnet.cz>
2023-05-04 20:18:18 +0200

    hardware: move threshold watcher's State op<< into velia
    This change is done in order to print state changes to a logger in one
    of the future commits.
    We need to include fmt/ostream.h so spdlog can pick up the operator<<
    definition and use it in the logger.
    
    Change-Id: I804e1fc045fd5382638a3f2a2f15171232062657

---
+++ src/ietf-hardware/thresholds.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#include <ostream>
+#include <stdexcept>
+#include "ietf-hardware/thresholds_fwd.h"
+
+namespace velia::ietf_hardware {
+
+std::ostream& operator<<(std::ostream& os, const State state)
+{
+    switch (state) {
+    case State::Initial:
+        os << "Unknown";
+        break;
+    case State::Disabled:
+        os << "Disabled";
+        break;
+    case State::CriticalLow:
+        os << "CriticalLow";
+        break;
+    case State::WarningLow:
+        os << "WarningLow";
+        break;
+    case State::Normal:
+        os << "Normal";
+        break;
+    case State::WarningHigh:
+        os << "WarningHigh";
+        break;
+    case State::CriticalHigh:
+        os << "CriticalHigh";
+        break;
+    }
+    return os;
+}
+}

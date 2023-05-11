CESNET/velia

commit 6a2334bda49ff32f987b6dcf8fb22666e2c7f16e

health: Report systemd unit states with alarms
We have been developing sysrepo-ietf-alarms project which implements
important parts of the ietf-alarms YANG model.
Let's use ietf-alarms for reporting states of systemd units. If the unit
is not healthy, we issue or clear alarm (by calling the RPC located at
/sysrepo-ietf-alarms:create-or-update-alarm).

Also, we used to had the ability to ignore some of the systemd units
because they were unhealthy but it was okay for us. We will no longer
ignore units on this level. ietf-alarms will do that for us by letting
consumers to filter alarms.

One of the next commits will rewrite the LED manager to use alarms.
The original health manager will be scraped after that.

Change-Id: I08eb07f3d02836b77ca2f43b2cca49469264c97a

CMakeLists.txt
@@ -73,6 +73,8 @@ set(YANG_SRCS
     yang/ietf-ipv6-unicast-routing@2018-03-13.yang
     yang/czechlight-lldp@2020-11-04.yang
     yang/iana-afn-safi@2013-07-04.yang
+    yang/ietf-alarms@2019-09-11.yang
+    yang/velia-alarms@2022-07-12.yang
     )
 
 # temporary until our CI switches to fmt>=9
@@ -107,6 +109,10 @@ target_link_libraries(velia-utils PUBLIC spdlog::spdlog PRIVATE PkgConfig::SYSTE
 
 # - health
 add_library(velia-health STATIC
+    src/health/alarms/alarms.cpp
+    src/health/alarms/alarms.h
+    src/health/alarms/SystemdUnits.cpp
+    src/health/alarms/SystemdUnits.h
     src/health/inputs/AbstractInput.cpp
     src/health/inputs/DbusSemaphoreInput.cpp
     src/health/inputs/DbusSystemdInput.cpp
@@ -122,6 +128,8 @@ target_link_libraries(velia-health
         Boost::boost
         SDBusCpp::sdbus-c++
         ${STD_FILESYSTEM_LIBRARY}
+    PRIVATE
+        PkgConfig::LIBYANG
     )
 
 # - ietf-hardware
@@ -493,6 +501,13 @@ if(BUILD_TESTING)
     )
 
     velia_test(NAME sysrepo-firewall LIBRARIES velia-firewall FIXTURE fixture_sysrepo-firewall)
+
+    set(fixture_sysrepo-alarms
+        YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-alarms@2019-09-11.yang
+        YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/velia-alarms@2022-07-12.yang
+        YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/sysrepo-ietf-alarms@2022-02-17.yang
+    )
+	velia_test(NAME health_systemd-units LIBRARIES velia-health DbusTesting FIXTURE fixture_sysrepo-alarms)
 endif()
 
 if(WITH_DOCS)

tests/health_systemd-units.cpp
@@ -1,0 +1,123 @@
+/*
+ * Copyright (C) 2020 - 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#include "trompeloeil_doctest.h"
+#include <sysrepo-cpp/Connection.hpp>
+#include <sysrepo-cpp/Enum.hpp>
+#include <sysrepo-cpp/Subscription.hpp>
+#include <thread>
+#include "dbus-helpers/dbus_systemd_server.h"
+#include "health/alarms/SystemdUnits.h"
+#include "mock/health.h"
+#include "test_log_setup.h"
+#include "test_sysrepo_helpers.h"
+#include "utils/log-init.h"
+#include "utils/log.h"
+
+using namespace std::chrono_literals;
+
+// clang-format off
+#define EXPECT_ALARM_RPC(RESOURCE, SEVERITY, TEXT) REQUIRE_CALL(fakeAlarmServer, rpcCalled(alarmRPC, std::map<std::string, std::string>{ \
+            {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-text", TEXT},                                                            \
+            {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-type-id", "velia-alarms:systemd-unit-failure"},                          \
+            {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-type-qualifier", ""},                                                    \
+            {"/sysrepo-ietf-alarms:create-or-update-alarm/resource", RESOURCE},                                                          \
+            {"/sysrepo-ietf-alarms:create-or-update-alarm/severity", SEVERITY}                                                           \
+        })).IN_SEQUENCE(seq1);
+// clang-format on
+
+const auto alarmRPC = "/sysrepo-ietf-alarms:create-or-update-alarm";
+
+class FakeAlarmServerSysrepo {
+public:
+    FakeAlarmServerSysrepo();
+    MAKE_CONST_MOCK2(rpcCalled, void(std::string_view, const std::map<std::string, std::string>&));
+
+private:
+    sysrepo::Session m_srSess;
+    std::optional<sysrepo::Subscription> m_srSub;
+};
+
+
+FakeAlarmServerSysrepo::FakeAlarmServerSysrepo()
+    : m_srSess(sysrepo::Connection{}.sessionStart())
+{
+    m_srSub = m_srSess.onRPCAction(alarmRPC, [&](auto, auto, std::string_view path, const libyang::DataNode input, auto, auto, auto) {
+        std::map<std::string, std::string> in;
+
+        for (auto n : input.childrenDfs()) {
+            if (n.isTerm()) {
+                in[n.path()] = n.asTerm().valueStr();
+            }
+        }
+
+        rpcCalled(path, in);
+        return sysrepo::ErrorCode::Ok;
+    });
+}
+
+TEST_CASE("systemd unit state monitoring (alarms)")
+{
+    TEST_INIT_LOGS;
+    TEST_SYSREPO_INIT;
+    trompeloeil::sequence seq1;
+
+    // Create and setup separate connections for both client and server to simulate real-world server-client architecture.
+    // Also this doesn't work one a single dbus connection.
+    auto clientConnection = sdbus::createSessionBusConnection();
+    auto serverConnection = sdbus::createSessionBusConnection();
+    clientConnection->enterEventLoopAsync();
+    serverConnection->enterEventLoopAsync();
+
+    auto server = DbusSystemdServer(*serverConnection);
+    FakeAlarmServerSysrepo fakeAlarmServer;
+
+    EXPECT_ALARM_RPC("unit1.service", "cleared", "systemd unit state: (active, running)");
+    server.createUnit(*serverConnection, "unit1.service", "/org/freedesktop/systemd1/unit/unit1", "active", "running");
+    EXPECT_ALARM_RPC("unit2.service", "critical", "systemd unit state: (activating, auto-restart)");
+    server.createUnit(*serverConnection, "unit2.service", "/org/freedesktop/systemd1/unit/unit2", "activating", "auto-restart");
+    EXPECT_ALARM_RPC("unit3.service", "critical", "systemd unit state: (failed, failed)");
+    server.createUnit(*serverConnection, "unit3.service", "/org/freedesktop/systemd1/unit/unit3", "failed", "failed");
+
+    auto systemdAlarms = std::make_shared<velia::health::SystemdUnits>(srSess, *clientConnection, serverConnection->getUniqueName(), "/org/freedesktop/systemd1", "org.freedesktop.systemd1.Manager", "org.freedesktop.systemd1.Unit");
+
+    EXPECT_ALARM_RPC("unit2.service", "cleared", "systemd unit state: (active, running)");
+    EXPECT_ALARM_RPC("unit3.service", "cleared", "systemd unit state: (active, running)");
+    EXPECT_ALARM_RPC("unit4.service", "critical", "systemd unit state: (failed, failed)");
+    EXPECT_ALARM_RPC("unit3.service", "critical", "systemd unit state: (activating, auto-restart)");
+    EXPECT_ALARM_RPC("unit3.service", "cleared", "systemd unit state: (active, running)");
+    EXPECT_ALARM_RPC("unit3.service", "critical", "systemd unit state: (failed, failed)");
+    EXPECT_ALARM_RPC("unit3.service", "critical", "systemd unit state: (activating, auto-restart)");
+    EXPECT_ALARM_RPC("unit3.service", "cleared", "systemd unit state: (active, running)");
+    EXPECT_ALARM_RPC("unit4.service", "cleared", "systemd unit state: (active, running)");
+
+    std::thread systemdSimulator([&] {
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit2", "active", "running");
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit3", "active", "running");
+
+        // In case we obtain a notifications that unit changed state from (X,Y) to (X,Y), do not trigger any events.
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit3", "active", "running");
+
+        // add new unit with failed/failed, DbusSystemdInput should receive UnitNew signal and monitor this unit too
+        server.createUnit(*serverConnection, "unit4.service", "/org/freedesktop/systemd1/unit/unit4", "failed", "failed");
+
+        // Sleep for a while; the rest of the code might be too fast and we need to be sure that we pick up event for (failed, failed) before the state of unit4 is changed in the DBus server
+        std::this_thread::sleep_for(25ms);
+
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit3", "activating", "auto-restart");
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit3", "active", "running");
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit3", "failed", "failed");
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit3", "activating", "auto-restart");
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit3", "active", "running");
+
+        server.changeUnitState("/org/freedesktop/systemd1/unit/unit4", "active", "running");
+    });
+
+
+    systemdSimulator.join();
+    waitForCompletionAndBitMore(seq1);
+}

src/health/alarms/SystemdUnits.cpp
@@ -1,0 +1,120 @@
+/*
+ * Copyright (C) 2020 - 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+#include "SystemdUnits.h"
+#include "alarms.h"
+#include "utils/log.h"
+#include "utils/sysrepo.h"
+
+namespace {
+const auto ALARM_ID = "velia-alarms:systemd-unit-failure";
+const auto ALARM_SEVERITY = "critical";
+}
+
+namespace velia::health {
+
+/** @brief Construct the systemd unit watcher for arbitrary dbus object. Mainly for tests. */
+SystemdUnits::SystemdUnits(sysrepo::Session session, sdbus::IConnection& connection, const std::string& busname, const std::string& managerObjectPath, const std::string& managerIface, const std::string& unitIface)
+    : m_log(spdlog::get("health"))
+    , m_srSession(std::move(session))
+    , m_busName(busname)
+    , m_unitIface(unitIface)
+    , m_proxyManager(sdbus::createProxy(connection, m_busName, managerObjectPath))
+{
+    utils::ensureModuleImplemented(m_srSession, "sysrepo-ietf-alarms", "2022-02-17");
+    utils::ensureModuleImplemented(m_srSession, "velia-alarms", "2022-07-12");
+
+    // Subscribe to systemd events. Systemd may not generate signals unless explicitly called
+    m_proxyManager->callMethod("Subscribe").onInterface(managerIface).withArguments().dontExpectReply();
+
+    // Register to a signal introducing new unit
+    m_proxyManager->uponSignal("UnitNew").onInterface(managerIface).call([&](const std::string& unitName, const sdbus::ObjectPath& unitObjectPath) {
+        if (m_proxyUnits.find(unitObjectPath) == m_proxyUnits.end()) {
+            registerSystemdUnit(connection, unitName, unitObjectPath);
+        }
+    });
+    m_proxyManager->finishRegistration();
+
+    /* Track all current units. Method ListUnits() -> a(ssssssouso) returns a DBus struct type with information
+     * about the unit (see https://www.freedesktop.org/wiki/Software/systemd/dbus/#Manager-ListUnits).
+     * In our code we need only the first (index 0, the unit name) field and seventh (index 6, unit object path) field.
+     */
+    std::vector<sdbus::Struct<std::string, std::string, std::string, std::string, std::string, std::string, sdbus::ObjectPath, uint32_t, std::string, sdbus::ObjectPath>> units;
+    m_proxyManager->callMethod("ListUnits").onInterface(managerIface).storeResultsTo(units);
+    for (const auto& unit : units) {
+        const auto& unitName = unit.get<0>();
+        const auto& unitObjectPath = unit.get<6>();
+
+        registerSystemdUnit(connection, unitName, unitObjectPath);
+    }
+}
+
+/** @brief Construct the systemd watcher for well-known systemd paths. */
+SystemdUnits::SystemdUnits(sysrepo::Session session, sdbus::IConnection& connection)
+    : SystemdUnits(session, connection, "org.freedesktop.systemd1", "/org/freedesktop/systemd1", "org.freedesktop.systemd1.Manager", "org.freedesktop.systemd1.Unit")
+{
+}
+
+/** @brief Registers a systemd unit by its unit name and unit dbus objectpath. */
+void SystemdUnits::registerSystemdUnit(sdbus::IConnection& connection, const std::string& unitName, const sdbus::ObjectPath& unitObjectPath)
+{
+    auto proxyUnit = sdbus::createProxy(connection, m_busName, unitObjectPath);
+    proxyUnit->uponSignal("PropertiesChanged").onInterface("org.freedesktop.DBus.Properties").call([&, unitName](const std::string& iface, const std::map<std::string, sdbus::Variant>& changed, [[maybe_unused]] const std::vector<std::string>& invalidated) {
+        if (iface != m_unitIface) {
+            return;
+        }
+
+        std::string newActiveState, newSubState;
+        if (auto it = changed.find("ActiveState"); it != changed.end()) {
+            newActiveState = it->second.get<std::string>();
+        }
+        if (auto it = changed.find("SubState"); it != changed.end()) {
+            newSubState = it->second.get<std::string>();
+        }
+
+        onUnitStateChange(unitName, newActiveState, newSubState);
+    });
+    proxyUnit->finishRegistration();
+    m_log->trace("Registered systemd unit watcher for '{}'", unitName);
+
+    // Query the current state of this unit
+    std::string newActiveState = proxyUnit->getProperty("ActiveState").onInterface(m_unitIface);
+    std::string newSubState = proxyUnit->getProperty("SubState").onInterface(m_unitIface);
+    onUnitStateChange(unitName, newActiveState, newSubState);
+
+    m_proxyUnits.emplace(std::make_pair(unitObjectPath, std::move(proxyUnit)));
+}
+
+/** @brief Callback for unit state change */
+void SystemdUnits::onUnitStateChange(const std::string& name, const std::string& activeState, const std::string& subState)
+{
+    auto systemdState = std::make_pair(activeState, subState);
+
+    auto lastState = m_unitState.find(name);
+    if (lastState == m_unitState.end()) {
+        lastState = m_unitState.insert(std::make_pair(name, systemdState)).first;
+    } else if (lastState->second == systemdState) {
+        // We were notified about a state change into the same state. No need to fire any events, everything is still the same.
+        m_log->trace("Systemd unit '{}' changed state but it is the same state as before ({}, {})", name, systemdState.first, systemdState.second);
+        return;
+    }
+
+    std::string alarmSeverity;
+    if (activeState == "failed" || (activeState == "activating" && subState == "auto-restart")) {
+        alarmSeverity = ALARM_SEVERITY;
+    } else {
+        alarmSeverity = "cleared";
+    }
+
+    m_log->debug("Systemd unit '{}' changed state ({} {})", name, activeState, subState);
+    lastState->second = systemdState;
+
+    createOrUpdateAlarm(m_srSession, ALARM_ID, std::nullopt, name, alarmSeverity, "systemd unit state: (" + activeState + ", " + subState + ")");
+}
+
+SystemdUnits::~SystemdUnits() = default;
+
+}

src/health/alarms/SystemdUnits.h
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2020 - 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+#pragma once
+
+#include <memory>
+#include <sdbus-c++/sdbus-c++.h>
+#include <set>
+#include <sysrepo-cpp/Session.hpp>
+#include "utils/log-fwd.h"
+
+namespace velia::health {
+
+/** @brief Watches for systemd units state via DBus and reports their state changes via ietf-alarms */
+class SystemdUnits {
+public:
+    SystemdUnits(sysrepo::Session session, sdbus::IConnection& connection);
+    SystemdUnits(sysrepo::Session session, sdbus::IConnection& connection, const std::string& busname, const std::string& managerObjectPath, const std::string& managerIface, const std::string& unitIface);
+    ~SystemdUnits();
+
+private:
+    velia::Log m_log;
+
+    sysrepo::Session m_srSession;
+
+    std::string m_busName;
+    std::string m_unitIface;
+    std::unique_ptr<sdbus::IProxy> m_proxyManager;
+
+    /** List of registered unit watchers */
+    std::map<sdbus::ObjectPath, std::unique_ptr<sdbus::IProxy>> m_proxyUnits;
+
+    /** Current unit state. */
+    std::map<std::string, std::pair<std::string, std::string>> m_unitState;
+
+    void registerSystemdUnit(sdbus::IConnection& connection, const std::string& unitName, const sdbus::ObjectPath& unitObjectPath);
+    void onUnitStateChange(const std::string& name, const std::string& activeState, const std::string& nSubState);
+};
+
+}

src/health/alarms/alarms.cpp
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+#include "alarms.h"
+
+using namespace std::string_literals;
+
+namespace {
+const auto alarmRpc = "/sysrepo-ietf-alarms:create-or-update-alarm";
+}
+
+namespace velia::health {
+void createOrUpdateAlarm(sysrepo::Session session, const std::string& alarmId, const std::optional<std::string>& alarmTypeQualifier, const std::string& resource, const std::string& severity, const std::string& text)
+{
+    auto inputNode = session.getContext().newPath(alarmRpc, std::nullopt);
+
+    inputNode.newPath(alarmRpc + "/resource"s, resource);
+    inputNode.newPath(alarmRpc + "/alarm-type-id"s, alarmId);
+    inputNode.newPath(alarmRpc + "/alarm-type-qualifier"s, alarmTypeQualifier.value_or(""));
+    inputNode.newPath(alarmRpc + "/severity"s, severity);
+    inputNode.newPath(alarmRpc + "/alarm-text"s, text);
+
+    session.sendRPC(inputNode);
+}
+}

src/health/alarms/alarms.h
@@ -1,0 +1,13 @@
+/*
+ * Copyright (C) 2022 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+#include <optional>
+#include <string>
+#include <sysrepo-cpp/Session.hpp>
+
+namespace velia::health {
+void createOrUpdateAlarm(sysrepo::Session session, const std::string& alarmId, const std::optional<std::string>& alarmQualifierType, const std::string& alarmResource, const std::string& severity, const std::string& alarmText);
+}

tests/yang/sysrepo-ietf-alarms@2022-02-17.yang
@@ -1,0 +1,50 @@
+module sysrepo-ietf-alarms {
+    yang-version 1.1;
+    namespace "urn:sysrepo:ietf-alarms";
+    prefix sr-al;
+
+    import ietf-alarms {
+        prefix al;
+        revision-date 2019-09-11;
+    }
+
+    revision 2022-02-17 {
+        description
+            "Initial revision.";
+    }
+
+    rpc create-or-update-alarm {
+        input {
+            leaf resource {
+                type al:resource;
+                mandatory true;
+            }
+
+            leaf alarm-type-id {
+                type al:alarm-type-id;
+                mandatory true;
+            }
+
+            leaf alarm-type-qualifier {
+                type al:alarm-type-qualifier;
+                default "";
+            }
+
+            leaf severity {
+                type al:severity-with-clear;
+                mandatory true;
+                description
+                    "Current severity or clearance state of the alarm.";
+            }
+
+            leaf alarm-text {
+                type al:alarm-text;
+                mandatory true;
+                description
+                    "The last reported alarm text.  This text should contain
+                    information for an operator to be able to understand the
+                    problem and how to resolve it.";
+            }
+        }
+    }
+}

commit 1be2e4f8b83303e95918ddb44427634fdbc8bf7f

health: move SystemdUnits into health directory
Last commit left SystemdUnits.cpp/h alone in directory called alarms.
That not make much sense, SystemdUnits is one of the main processing
classes of velia-health and it only creates alarms, it is not managing
them.

Fixes: 6a2334bd ("health: Report systemd unit states with alarms")
Change-Id: I9865fb869e0ac53650651df3174910620ab94435

CMakeLists.txt
@@ -111,13 +111,13 @@ target_link_libraries(velia-utils PUBLIC spdlog::spdlog PRIVATE PkgConfig::SYSTE
 
 # - health
 add_library(velia-health STATIC
-    src/health/alarms/SystemdUnits.cpp
-    src/health/alarms/SystemdUnits.h
     src/health/outputs/AlarmsOutputs.cpp
     src/health/outputs/AlarmsOutputs.h
     src/health/outputs/LedSysfsDriver.cpp
     src/health/outputs/callables.cpp
     src/health/State.cpp
+    src/health/SystemdUnits.cpp
+    src/health/SystemdUnits.h
     )
 target_link_libraries(velia-health
     PUBLIC

src/main-health.cpp
@@ -7,7 +7,7 @@
 #include <sysrepo-cpp/Connection.hpp>
 #include "VELIA_VERSION.h"
 #include "health/Factory.h"
-#include "health/alarms/SystemdUnits.h"
+#include "health/SystemdUnits.h"
 #include "health/outputs/callables.h"
 #include "main.h"
 #include "utils/exceptions.h"

tests/health_systemd-units.cpp
@@ -11,7 +11,7 @@
 #include <sysrepo-cpp/Subscription.hpp>
 #include <thread>
 #include "dbus-helpers/dbus_systemd_server.h"
-#include "health/alarms/SystemdUnits.h"
+#include "health/SystemdUnits.h"
 #include "pretty_printers.h"
 #include "test_log_setup.h"
 #include "test_sysrepo_helpers.h"

tests/sysrepo_ietf-alarms.cpp
@@ -3,7 +3,7 @@
 #include <trompeloeil.hpp>
 #include "dbus-helpers/dbus_rauc_server.h"
 #include "health/State.h"
-#include "health/alarms/SystemdUnits.h"
+#include "health/SystemdUnits.h"
 #include "health/outputs/AlarmsOutputs.h"
 #include "pretty_printers.h"
 #include "test_log_setup.h"
CESNET/velia

commit a46f6fa1f565cf1627a217f368250c369100b061

refactor: IETFInterfaces::onRouteUpdate never deletes anything
On a quick glance I don't know if this is the correct behavior -- but
it's what the code has been doing already, there's just nothing which
does anything with the deleted routes. Don't use an empty variable for
that; I'll need this in a later refactoring.

Change-Id: I91c9abdece04ca35007a3f481f927ea1f1f7b33b

src/system/IETFInterfaces.cpp
@@ -299,7 +299,6 @@ void IETFInterfaces::onRouteUpdate(rtnl_route*, int)
      */
 
     std::vector<utils::YANGPair> values;
-    std::vector<std::string> deletePaths;
 
     auto routes = m_rtnetlink->getRoutes();
     auto links = m_rtnetlink->getLinks();
@@ -405,6 +404,6 @@ void IETFInterfaces::onRouteUpdate(rtnl_route*, int)
     }
 
     std::lock_guard<std::mutex> lock(m_mtx);
-    utils::valuesPush(values, deletePaths, m_srSession, sysrepo::Datastore::Operational);
+    utils::valuesPush(values, {}, m_srSession, sysrepo::Datastore::Operational);
 }
 }

commit bea97b7503027ba79ce233464326ccf339956114

system: fix onRouteUpdate not discarding anything
Fixes: a46f6fa1f565cf1627a217f368250c369100b061
Change-Id: I2cb33299f4fe090922e8f5eb352061581e11adac

src/system/IETFInterfaces.cpp
@@ -404,6 +404,6 @@ void IETFInterfaces::onRouteUpdate(rtnl_route*, int)
     }
 
     std::lock_guard<std::mutex> lock(m_mtx);
-    utils::valuesPush(values, {}, {}, m_srSession, sysrepo::Datastore::Operational);
+    utils::valuesPush(values, {}, {"/ietf-routing:routing/ribs/rib"}, m_srSession, sysrepo::Datastore::Operational);
 }
 }

tests/sysrepo_interfaces-libnl.cpp
@@ -268,20 +268,32 @@ TEST_CASE("Test ietf-interfaces and ietf-routing")
         {
             auto routeIdx = findRouteIndex("198.51.100.0/24");
             REQUIRE(routeIdx > 0);
+            REQUIRE(data["/routes/route["s + std::to_string(routeIdx) + "]/ietf-ipv4-unicast-routing:destination-prefix"] == "198.51.100.0/24");
             REQUIRE(data["/routes/route["s + std::to_string(routeIdx) + "]/next-hop/outgoing-interface"] == IFACE);
             REQUIRE(data["/routes/route["s + std::to_string(routeIdx) + "]/source-protocol"] == "static");
         }
         {
             auto routeIdx = findRouteIndex("192.0.2.0/24");
             REQUIRE(routeIdx > 0);
+            REQUIRE(data["/routes/route["s + std::to_string(routeIdx) + "]/ietf-ipv4-unicast-routing:destination-prefix"] == "192.0.2.0/24");
             REQUIRE(data["/routes/route["s + std::to_string(routeIdx) + "]/next-hop/outgoing-interface"] == IFACE);
             REQUIRE(data["/routes/route["s + std::to_string(routeIdx) + "]/source-protocol"] == "direct");
+        }
+        {
+            auto routeIdx = findRouteIndex("192.5.5.5/24");
+            REQUIRE(routeIdx == 0);
         }
 
         data = dataFromSysrepo(client, "/ietf-routing:routing/ribs/rib[name='ipv6-master']", sysrepo::Datastore::Operational);
         REQUIRE(data["/name"] == "ipv6-master");
 
         iproute2_exec_and_wait(WAIT, "route", "del", "198.51.100.0/24");
+
+        {
+            auto routeIdx = findRouteIndex("198.51.100.0/24");
+            REQUIRE(routeIdx == 0);
+        }
+
         iproute2_exec_and_wait(WAIT, "link", "set", IFACE, "down");
     }
 
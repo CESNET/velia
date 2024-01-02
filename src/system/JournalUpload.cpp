1173922c013f6aaa3048ad0ce2efb4fa5fcbe765
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-01-02 17:00:10 +0100

    system: allow to configure remote journal upload
    We add a new capability that allows user to configure
    systemd-journal-upload service.
    When the /czechlight-system:syslog/journal-upload is present then its
    child leaf "url" is used as a destination for journal upload.
    
    Change-Id: I0f77fc39af337fb4b350758e5c59117e3ea78a85

---
+++ src/system/JournalUpload.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#include <filesystem>
+#include <sysrepo-cpp/Enum.hpp>
+#include "JournalUpload.h"
+#include "utils/io.h"
+#include "utils/log.h"
+
+using namespace std::string_literals;
+
+static const auto UPLOAD_URL_LEAF = "/czechlight-system:syslog/journal-upload/url";
+
+namespace {
+sysrepo::ErrorCode configureJournalUpload(velia::Log log, sysrepo::Session session, const std::filesystem::path& envFile, const velia::system::JournalUpload::RestartCb& restartCb)
+{
+    std::optional<std::string> url;
+
+    if (auto data = session.getData(UPLOAD_URL_LEAF)) {
+        if (auto node = data->findPath(UPLOAD_URL_LEAF)) {
+            url = node->asTerm().valueStr();
+        }
+    }
+
+    std::optional<std::string> oldContent;
+    std::optional<std::string> newContent;
+
+    try {
+        oldContent = velia::utils::readFileToString(envFile);
+    } catch (const std::invalid_argument&) {
+        // if the file does not exist, oldContent will still be nullopt
+    }
+
+    if (url) {
+        newContent = "DESTINATION=" + *url + "\n";
+    }
+
+    if (oldContent != newContent) {
+        if (newContent) {
+            std::ofstream ofs(envFile);
+            ofs << *newContent;
+
+            newContent->pop_back(); // remove the \n
+            log->debug("systemd-journal-upload.service environment file set to {}", *newContent);
+        } else {
+            std::filesystem::remove(envFile);
+            log->debug("systemd-journal-upload.service environment file removed");
+        }
+
+        restartCb(log);
+    }
+
+    return sysrepo::ErrorCode::Ok;
+}
+
+sysrepo::Subscription journalUploadSubscription(velia::Log log, sysrepo::Session session, const std::filesystem::path& envFile, const velia::system::JournalUpload::RestartCb& restartCb)
+{
+    auto sub = session.onModuleChange(
+        "czechlight-system",
+        [log, envFile, restartCb](auto session, auto, auto, auto, auto, auto) {
+            return configureJournalUpload(log, session, envFile, restartCb);
+        },
+        std::nullopt,
+        0,
+        sysrepo::SubscribeOptions::DoneOnly | sysrepo::SubscribeOptions::Enabled);
+
+    /*
+     * In case someone removes the presence container between sysrepo loads the data and this module startup we won't get a change (Deleted) from sysrepo and the file won't be written.
+     * Therefore, first register the callback and than call the configure manually.
+     * The configure function does not restart the service unless the configuration file content changes so this should not trigger the unit restart.
+     */
+    configureJournalUpload(log, session, envFile, restartCb);
+
+    return sub;
+}
+
+}
+
+namespace velia::system {
+
+JournalUpload::JournalUpload(sysrepo::Session session, const std::filesystem::path& envFile, const RestartCb& restartCb)
+    : m_log(spdlog::get("system"))
+    , m_srSub(journalUploadSubscription(m_log, session, envFile, restartCb))
+{
+}
+
+}
---
+++ src/system/JournalUpload.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#pragma once
+
+#include <sysrepo-cpp/Session.hpp>
+#include "LLDP.h"
+#include "utils/log-fwd.h"
+
+namespace velia::system {
+
+class JournalUpload {
+public:
+    using RestartCb = std::function<void(velia::Log)>;
+
+    JournalUpload(sysrepo::Session session, const std::filesystem::path& envFile, const RestartCb& restartCb);
+
+private:
+    velia::Log m_log;
+    sysrepo::Subscription m_srSub;
+};
+
+}

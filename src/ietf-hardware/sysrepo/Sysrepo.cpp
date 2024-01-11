CESNET/velia

commit 1b3c1732fc364c15132fc5f334919db508e9c8ad

hardware: invoke missing sensor alarms
Enable reporting alarm concerning a missing sensor.

Change-Id: I6fc0a1043623f5d97921c820b1e7c6807f1f96a4

src/ietf-hardware/sysrepo/Sysrepo.cpp
@@ -14,7 +14,11 @@
 
 namespace {
 
+const auto ALARM_CLEARED = "cleared";
 const auto ALARM_SENSOR_MISSING = "velia-alarms:sensor-missing-alarm";
+const auto ALARM_MISSING = "velia-alarms:sensor-missing-alarm";
+const auto ALARM_MISSING_SEVERITY = "warning";
+const auto ALARM_MISSING_DESCRIPTION = "Sensor value not reported. Maybe the sensor was unplugged?";
 const auto ALARM_THRESHOLD_CROSSING_LOW = "velia-alarms:sensor-low-value-alarm";
 const auto ALARM_THRESHOLD_CROSSING_HIGH = "velia-alarms:sensor-high-value-alarm";
 
@@ -33,6 +37,11 @@ std::string extractComponentPrefix(const std::string& componentXPath)
     }
 
     throw std::logic_error("Invalid xPath provided ('" + componentXPath + "')");
+}
+
+void logAlarm(velia::Log logger, const std::string_view sensor, const std::string_view alarm, const std::string_view severity)
+{
+    logger->info("Alarm {}: {} for {}", alarm, severity, sensor);
 }
 }
 
@@ -57,6 +66,7 @@ Sysrepo::Sysrepo(::sysrepo::Session session, std::shared_ptr<IETFHardware> hwSta
         auto conn = m_session.getConnection();
 
         DataTree prevValues;
+        std::map<std::string, State> thresholdsStates;
 
         while (!m_quit) {
             m_log->trace("IetfHardware poll");
@@ -78,6 +88,31 @@ Sysrepo::Sysrepo(::sysrepo::Session session, std::shared_ptr<IETFHardware> hwSta
             std::copy(deletedComponents.begin(), deletedComponents.end(), std::back_inserter(discards));
 
             utils::valuesPush(hwStateValues, {}, discards, m_session, ::sysrepo::Datastore::Operational);
+
+            for (const auto& [sensorXPath, state] : thresholds) {
+                // missing prevState can be considered as Normal
+                const State prevState = [&, sensorXPath = sensorXPath] {
+                    if (auto it = thresholdsStates.find(sensorXPath); it != thresholdsStates.end()) {
+                        return it->second;
+                    }
+                    return State::Normal;
+                }();
+                const auto componentXPath = extractComponentPrefix(sensorXPath);
+
+                if (state == State::NoValue) {
+                    logAlarm(m_log, componentXPath, ALARM_MISSING, ALARM_MISSING_SEVERITY);
+                    utils::createOrUpdateAlarm(m_session, ALARM_MISSING, std::nullopt, componentXPath, ALARM_MISSING_SEVERITY, ALARM_MISSING_DESCRIPTION);
+                } else if (prevState == State::NoValue) {
+                    logAlarm(m_log, componentXPath, ALARM_MISSING, ALARM_CLEARED);
+                    /* The alarm message is same for both setting and clearing the alarm. RFC8632 says that it is
+                     * "The string used to inform operators about the alarm. This MUST contain enough information for an operator to be able to understand the problem and how to resolve it.",
+                     * i.e., from my POV it does not make sense to say something like "cleared" when clearing the alarm as this would not be beneficial for the operator to understand what happened.
+                     */
+                    utils::createOrUpdateAlarm(m_session, ALARM_MISSING, std::nullopt, componentXPath, ALARM_CLEARED, ALARM_MISSING_DESCRIPTION);
+                }
+
+                thresholdsStates[sensorXPath] = state;
+            }
 
             prevValues = std::move(hwStateValues);
             std::this_thread::sleep_for(m_pollInterval);

tests/sysrepo_ietf-hardware.cpp
@@ -54,6 +54,10 @@ struct DatastoreChange {
     MAKE_CONST_MOCK1(change, void(const std::map<std::string, std::variant<std::string, Deleted>>&));
 };
 
+struct AlarmEvent {
+    MAKE_CONST_MOCK1(event, void(const std::map<std::string, std::string>&));
+};
+
 #define REQUIRE_ALARM_INVENTORY_ADD_ALARM(ALARM_TYPE, IETF_HARDWARE_RESOURCE)                                                                                                                                                                 \
     REQUIRE_CALL(dsChangeAlarmInventory, change(std::map<std::string, std::variant<std::string, Deleted>>{                                                                                                                                    \
                                              {"/ietf-alarms:alarms/alarm-inventory/alarm-type[alarm-type-id='" ALARM_TYPE "'][alarm-type-qualifier='']", "(list instance)"},                                                                  \
@@ -86,6 +90,16 @@ void processDsChanges(sysrepo::Session session, DatastoreChange& dsChange, const
     dsChange.change(changes);
 }
 
+#define REQUIRE_ALARM_RPC(ALARM_TYPE_ID, IETF_HARDWARE_RESOURCE_KEY, SEVERITY, TEXT)                                                                                    \
+    REQUIRE_CALL(alarmEvents, event(std::map<std::string, std::string>{                                                                                                 \
+                                  {"/sysrepo-ietf-alarms:create-or-update-alarm", "(unprintable)"},                                                                     \
+                                  {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-text", TEXT},                                                                     \
+                                  {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-type-id", ALARM_TYPE_ID},                                                         \
+                                  {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-type-qualifier", ""},                                                             \
+                                  {"/sysrepo-ietf-alarms:create-or-update-alarm/resource", "/ietf-hardware:hardware/component[name='" IETF_HARDWARE_RESOURCE_KEY "']"}, \
+                                  {"/sysrepo-ietf-alarms:create-or-update-alarm/severity", SEVERITY},                                                                   \
+                              }))
+
 TEST_CASE("IETF Hardware with sysrepo")
 {
     TEST_SYSREPO_INIT_LOGS;
@@ -99,8 +113,21 @@ TEST_CASE("IETF Hardware with sysrepo")
 
     DatastoreChange dsChangeHardware;
     DatastoreChange dsChangeAlarmInventory;
+    AlarmEvent alarmEvents;
 
     trompeloeil::sequence seq1;
+
+    auto alarmsRPC = alarmsClient.onRPCAction("/sysrepo-ietf-alarms:create-or-update-alarm", [&](auto, auto, auto, const libyang::DataNode input, auto, auto, auto) {
+        std::map<std::string, std::string> inputData;
+
+        for (const auto& node : input.childrenDfs()) {
+            inputData.emplace(node.path(), nodeAsString(node));
+        }
+
+        alarmEvents.event(inputData);
+
+        return sysrepo::ErrorCode::Ok;
+    });
 
     auto directLeafNodeQuery = [&](const std::string& xpath) {
         auto val = client.getData(xpath);
@@ -158,7 +185,13 @@ TEST_CASE("IETF Hardware with sysrepo")
 
         velia::ietf_hardware::ThresholdsBySensorPath thresholds() const
         {
-            return {{"/ietf-hardware:hardware/component[name='ne:psu:child']/sensor-data/value", {}}};
+            using namespace velia::ietf_hardware;
+            return {{"/ietf-hardware:hardware/component[name='ne:psu:child']/sensor-data/value", Thresholds<int64_t>{
+                                                                                                     .criticalLow = std::nullopt,
+                                                                                                     .warningLow = OneThreshold<int64_t>{10000, 2000},
+                                                                                                     .warningHigh = OneThreshold<int64_t>{15000, 2000},
+                                                                                                     .criticalHigh = std::nullopt,
+                                                                                                 }}};
         }
     };
     ietfHardware->registerDataReader(PsuDataReader{psuActive});
@@ -285,6 +318,7 @@ TEST_CASE("IETF Hardware with sysrepo")
                                        {"/ietf-hardware:hardware/component[name='ne:temperature-cpu']/sensor-data/value", "222"},
                                    }))
         .IN_SEQUENCE(seq1);
+    REQUIRE_ALARM_RPC("velia-alarms:sensor-missing-alarm", "ne:psu:child", "warning", "Sensor value not reported. Maybe the sensor was unplugged?").IN_SEQUENCE(seq1);
     REQUIRE_CALL(*sysfsTempCpu, attribute("temp1_input")).LR_RETURN(cpuTempValue).TIMES(AT_LEAST(1));
     REQUIRE_CALL(*sysfsPower, attribute("power1_input")).LR_RETURN(powerValue).TIMES(AT_LEAST(1));
     cpuTempValue = 222;
@@ -309,6 +343,8 @@ TEST_CASE("IETF Hardware with sysrepo")
                                        {"/ietf-hardware:hardware/component[name='ne:psu:child']/state/oper-state", "enabled"},
                                    }))
         .IN_SEQUENCE(seq1);
+    REQUIRE_ALARM_RPC("velia-alarms:sensor-missing-alarm", "ne:psu:child", "cleared", "Sensor value not reported. Maybe the sensor was unplugged?").IN_SEQUENCE(seq1);
+
     psuActive = true;
 
     waitForCompletionAndBitMore(seq1);

commit 2ec3748047bb82e38a513d0a62542eec2b2d3f8e

alarms: remove redundant definition
These two constants were just different names for a single alarm type.

Fixes: 1b3c1732 (hardware: invoke missing sensor alarms)
Change-Id: I081d3bb09661ab1cee189bb806046800eec5da98

src/ietf-hardware/sysrepo/Sysrepo.cpp
@@ -17,7 +17,6 @@ namespace {
 
 const auto ALARM_CLEARED = "cleared";
 const auto ALARM_SENSOR_MISSING = "velia-alarms:sensor-missing-alarm";
-const auto ALARM_MISSING = "velia-alarms:sensor-missing-alarm";
 const auto ALARM_MISSING_SEVERITY = "warning";
 const auto ALARM_MISSING_DESCRIPTION = "Sensor value not reported. Maybe the sensor was unplugged?";
 const auto ALARM_THRESHOLD_CROSSING_LOW = "velia-alarms:sensor-low-value-alarm";
@@ -155,15 +154,15 @@ Sysrepo::Sysrepo(::sysrepo::Session session, std::shared_ptr<IETFHardware> hwSta
                 const auto componentXPath = extractComponentPrefix(sensorXPath);
 
                 if (state == State::NoValue) {
-                    logAlarm(m_log, componentXPath, ALARM_MISSING, ALARM_MISSING_SEVERITY);
-                    utils::createOrUpdateAlarm(m_session, ALARM_MISSING, std::nullopt, componentXPath, ALARM_MISSING_SEVERITY, ALARM_MISSING_DESCRIPTION);
+                    logAlarm(m_log, componentXPath, ALARM_SENSOR_MISSING, ALARM_MISSING_SEVERITY);
+                    utils::createOrUpdateAlarm(m_session, ALARM_SENSOR_MISSING, std::nullopt, componentXPath, ALARM_MISSING_SEVERITY, ALARM_MISSING_DESCRIPTION);
                 } else if (prevState == State::NoValue) {
-                    logAlarm(m_log, componentXPath, ALARM_MISSING, ALARM_CLEARED);
+                    logAlarm(m_log, componentXPath, ALARM_SENSOR_MISSING, ALARM_CLEARED);
                     /* The alarm message is same for both setting and clearing the alarm. RFC8632 says that it is
                      * "The string used to inform operators about the alarm. This MUST contain enough information for an operator to be able to understand the problem and how to resolve it.",
                      * i.e., from my POV it does not make sense to say something like "cleared" when clearing the alarm as this would not be beneficial for the operator to understand what happened.
                      */
-                    utils::createOrUpdateAlarm(m_session, ALARM_MISSING, std::nullopt, componentXPath, ALARM_CLEARED, ALARM_MISSING_DESCRIPTION);
+                    utils::createOrUpdateAlarm(m_session, ALARM_SENSOR_MISSING, std::nullopt, componentXPath, ALARM_CLEARED, ALARM_MISSING_DESCRIPTION);
                 }
 
                 /*
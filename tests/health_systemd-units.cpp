b80a7e368403759b1c4ea794217672d371f07548
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-01-24 15:04:14 +0100

    tests: use alarm expectations in systemd-unit tests
    I will be rewriting the class that is tested by this code soon. So far,
    we only check how the datastore looks after performing some operations.
    However, I would like to be sure that I see all the changes made to a
    datastore and verify that all RPCs are indeed called and the
    alarm-inventory is filled with the correct data.
    
    Change-Id: Iffcea3208c299e926295a1913503ede1feb51bd4

--- tests/health_systemd-units.cpp
+++ tests/health_systemd-units.cpp
@@ -14,23 +14,15 @@
 #include "health/SystemdUnits.h"
 #include "pretty_printers.h"
 #include "test_log_setup.h"
+#include "tests/sysrepo-helpers/alarms.h"
 #include "tests/sysrepo-helpers/common.h"
-#include "tests/sysrepo-helpers/rpc.h"
 #include "utils/log-init.h"
 #include "utils/log.h"
 
 using namespace std::chrono_literals;
 
-// clang-format off
-#define REQUIRE_ALARM_RPC(RESOURCE, SEVERITY, TEXT) REQUIRE_RPC_CALL(alarmRPC, (Values{                          \
-            {"/sysrepo-ietf-alarms:create-or-update-alarm", "(unprintable)"},                                   \
-            {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-text", TEXT},                                   \
-            {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-type-id", "velia-alarms:systemd-unit-failure"}, \
-            {"/sysrepo-ietf-alarms:create-or-update-alarm/alarm-type-qualifier", ""},                           \
-            {"/sysrepo-ietf-alarms:create-or-update-alarm/resource", RESOURCE},                                 \
-            {"/sysrepo-ietf-alarms:create-or-update-alarm/severity", SEVERITY}                                  \
-        })).IN_SEQUENCE(seq1);
-// clang-format on
+#define REQUIRE_NEW_ALARM_INVENTORY_UNIT(UNIT) REQUIRE_NEW_ALARM_INVENTORY_RESOURCE(alarmsWatcher, "velia-alarms:systemd-unit-failure", "", UNIT)
+#define REQUIRE_ALARM_RPC(UNIT, SEVERITY, TEXT) REQUIRE_NEW_ALARM(alarmsWatcher, "velia-alarms:systemd-unit-failure", "", UNIT, SEVERITY, TEXT)
 
 TEST_CASE("systemd unit state monitoring (alarms)")
 {
@@ -47,32 +39,29 @@ TEST_CASE("systemd unit state monitoring (alarms)")
     serverConnection->enterEventLoopAsync();
 
     auto server = DbusSystemdServer(*serverConnection);
-    RPCWatcher alarmRPC(client, "/sysrepo-ietf-alarms:create-or-update-alarm");
 
+    client.switchDatastore(sysrepo::Datastore::Operational);
+    AlarmWatcher alarmsWatcher(client);
+
+    REQUIRE_NEW_ALARM_INVENTORY_ENTRY(alarmsWatcher, "velia-alarms:systemd-unit-failure", "", (std::set<std::string>{}), (std::set<std::string>{"critical"}), true, "The systemd service is considered in failed state.");
+
+    REQUIRE_NEW_ALARM_INVENTORY_UNIT("unit1.service");
     REQUIRE_ALARM_RPC("unit1.service", "cleared", "systemd unit state: (active, running)");
     server.createUnit(*serverConnection, "unit1.service", "/org/freedesktop/systemd1/unit/unit1", "active", "running");
+
+    REQUIRE_NEW_ALARM_INVENTORY_UNIT("unit2.service");
     REQUIRE_ALARM_RPC("unit2.service", "critical", "systemd unit state: (activating, auto-restart)");
     server.createUnit(*serverConnection, "unit2.service", "/org/freedesktop/systemd1/unit/unit2", "activating", "auto-restart");
+
+    REQUIRE_NEW_ALARM_INVENTORY_UNIT("unit3.service");
     REQUIRE_ALARM_RPC("unit3.service", "critical", "systemd unit state: (failed, failed)");
     server.createUnit(*serverConnection, "unit3.service", "/org/freedesktop/systemd1/unit/unit3", "failed", "failed");
 
     auto systemdAlarms = std::make_shared<velia::health::SystemdUnits>(srSess, *clientConnection, serverConnection->getUniqueName(), "/org/freedesktop/systemd1", "org.freedesktop.systemd1.Manager", "org.freedesktop.systemd1.Unit");
 
-    REQUIRE(dataFromSysrepo(srSess, "/ietf-alarms:alarms/alarm-inventory", sysrepo::Datastore::Operational) == std::map<std::string, std::string>{
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']", ""},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/alarm-type-id", "velia-alarms:systemd-unit-failure"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/alarm-type-qualifier", ""},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/resource[1]", "unit1.service"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/resource[2]", "unit2.service"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/resource[3]", "unit3.service"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/will-clear", "true"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/severity-level[1]", "critical"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/description", "The systemd service is considered in failed state."}
-            });
-    // clang-format on
-
     REQUIRE_ALARM_RPC("unit2.service", "cleared", "systemd unit state: (active, running)");
     REQUIRE_ALARM_RPC("unit3.service", "cleared", "systemd unit state: (active, running)");
+    REQUIRE_NEW_ALARM_INVENTORY_UNIT("unit4.service");
     REQUIRE_ALARM_RPC("unit4.service", "critical", "systemd unit state: (failed, failed)");
     REQUIRE_ALARM_RPC("unit3.service", "critical", "systemd unit state: (activating, auto-restart)");
     REQUIRE_ALARM_RPC("unit3.service", "cleared", "systemd unit state: (active, running)");
@@ -105,19 +94,4 @@ TEST_CASE("systemd unit state monitoring (alarms)")
 
     systemdSimulator.join();
     waitForCompletionAndBitMore(seq1);
-
-    // clang-format off
-    REQUIRE(dataFromSysrepo(srSess, "/ietf-alarms:alarms/alarm-inventory", sysrepo::Datastore::Operational) == std::map<std::string, std::string>{
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']", ""},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/alarm-type-id", "velia-alarms:systemd-unit-failure"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/alarm-type-qualifier", ""},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/resource[1]", "unit1.service"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/resource[2]", "unit2.service"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/resource[3]", "unit3.service"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/resource[4]", "unit4.service"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/will-clear", "true"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/severity-level[1]", "critical"},
-            {"/alarm-type[alarm-type-id='velia-alarms:systemd-unit-failure'][alarm-type-qualifier='']/description", "The systemd service is considered in failed state."}
-            });
-    // clang-format on
 }

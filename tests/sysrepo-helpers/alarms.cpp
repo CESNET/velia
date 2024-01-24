085264d976bc7413ed4db76ffb9f426d9141490f
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-01-24 15:12:44 +0100

    tests: allow AlarmInventory to be reusable
    Change-Id: I914d6ee6d941ec942288df3093a1f95ffbe30591

---
+++ tests/sysrepo-helpers/alarms.cpp
@@ -0,0 +1,14 @@
+#include "alarms.h"
+
+void AlarmInventory::add(const std::string& alarmTypeId, const std::string& alarmTypeQualifier, const std::string& resource)
+{
+    inventory[{alarmTypeId, alarmTypeQualifier}].push_back(resource);
+}
+
+bool AlarmInventory::contains(const std::string& alarmTypeId, const std::string& alarmTypeQualifier, const std::string& resource) const
+{
+    if (auto it = inventory.find({alarmTypeId, alarmTypeQualifier}); it != inventory.end()) {
+        return std::find(it->second.begin(), it->second.end(), resource) != it->second.end();
+    }
+    return false;
+}

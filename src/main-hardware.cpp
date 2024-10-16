CESNET/velia

commit aae9fe3379e51df7ba5edc5795151cac1efd7668

veliad-hardware: sysrepo daemon managing hardware
This patch factors out the Sysrepo implementation of ietf-hardware
model. We agreed to split velia into smaller parts and this is a logical
individual unit.
The original velia will implement only the health tracking.

In the future, veliad-hardware might start notifying the original veliad
daemon when something goes wrong in the hardware. This will probably be
done using some kind of IPC, like D-Bus.

Change-Id: Ib831b1fd97d8ec87a711321461a3d6a73b63bcf7

src/main-hardware.cpp
@@ -1,18 +1,15 @@
 #include <docopt.h>
-#include <future>
-#include <sdbus-c++/sdbus-c++.h>
 #include <spdlog/sinks/ansicolor_sink.h>
 #include <spdlog/spdlog.h>
 #include <sysrepo-cpp/Session.hpp>
 #include "VELIA_VERSION.h"
-#include "health/Factory.h"
-#include "health/inputs/DbusSystemdInput.h"
-#include "health/manager/StateManager.h"
-#include "health/outputs/callables.h"
-#include "main.h"
+#include "ietf-hardware/Factory.h"
+#include "ietf-hardware/IETFHardware.h"
+#include "ietf-hardware/sysrepo/Sysrepo.h"
 #include "utils/exceptions.h"
 #include "utils/journal.h"
 #include "utils/log-init.h"
+#include "utils/waitUntilSignalled.h"
 
 /** @short Extract log level from a CLI option */
 spdlog::level::level_enum parseLogLevel(const std::string& name, const docopt::value& option)
@@ -32,30 +29,27 @@ spdlog::level::level_enum parseLogLevel(const std::string& name, const docopt::v
 }
 
 static const char usage[] =
-    R"(Monitor system health status.
+    R"(Hardware monitoring via Sysrepo.
 
 Usage:
-  veliad
+  veliad-hardware
     [--appliance=<Model>]
     [--log-level=<Level>]
-    [--health-log-level=<Level>]
     [--sysrepo-log-level=<Level>]
-    [--systemd-ignore-unit=<Unit>]...
-  veliad (-h | --help)
-  veliad --version
+    [--hardware-log-level=<Level>]
+  veliad-hardware (-h | --help)
+  veliad-hardware --version
 
 Options:
   -h --help                         Show this screen.
   --version                         Show version.
+  --appliance=<Model>               Initialize IETF Hardware and outputs for specific appliance.
   --log-level=<N>                   Log level for everything [default: 3]
                                     (0 -> critical, 1 -> error, 2 -> warning, 3 -> info,
                                     4 -> debug, 5 -> trace)
-  --health-log-level=<N>            Log level for the health monitoring [default: 3]
   --sysrepo-log-level=<N>           Log level for the sysrepo library [default: 3]
-  --systemd-ignore-unit=<Unit>      Ignore state of systemd's unit in systemd state tracker. Can be specified multiple times.
+  --hardware-log-level=<N>          Log level for the hardware drivers [default: 3]
 )";
-
-DBUS_EVENTLOOP_INIT
 
 int main(int argc, char* argv[])
 {
@@ -66,7 +60,7 @@ int main(int argc, char* argv[])
         loggingSink = std::make_shared<spdlog::sinks::ansicolor_stderr_sink_mt>();
     }
 
-    auto args = docopt::docopt(usage, {argv + 1, argv + argc}, true, "veliad " VELIA_VERSION, true);
+    auto args = docopt::docopt(usage, {argv + 1, argv + argc}, true, "veliad-system " VELIA_VERSION, true);
 
     velia::utils::initLogs(loggingSink);
     spdlog::set_level(spdlog::level::info);
@@ -74,36 +68,26 @@ int main(int argc, char* argv[])
     try {
         spdlog::set_level(parseLogLevel("Generic", args["--log-level"]));
         spdlog::get("sysrepo")->set_level(parseLogLevel("Sysrepo library", args["--sysrepo-log-level"]));
+        spdlog::get("hardware")->set_level(parseLogLevel("Hardware loggers", args["--hardware-log-level"]));
 
         spdlog::get("main")->debug("Opening Sysrepo connection");
         auto srConn = std::make_shared<sysrepo::Connection>();
         auto srSess = std::make_shared<sysrepo::Session>(srConn);
         auto srSubscription = std::make_shared<sysrepo::Subscribe>(srSess);
 
-        DBUS_EVENTLOOP_START
-
-        // health
-        auto manager = std::make_shared<velia::health::StateManager>();
-
-        // output configuration
-        spdlog::get("main")->debug("Initializing LED drivers");
+        // initialize ietf-hardware
+        spdlog::get("main")->debug("Initializing IETFHardware module");
+        std::shared_ptr<velia::ietf_hardware::IETFHardware> ietfHardware;
         if (const auto& appliance = args["--appliance"]) {
-            manager->m_outputSignal.connect(velia::health::createOutput(appliance.asString()));
+            ietfHardware = velia::ietf_hardware::create(appliance.asString());
+        } else {
+            ietfHardware = std::make_shared<velia::ietf_hardware::IETFHardware>();
         }
 
-        spdlog::get("main")->debug("All outputs initialized.");
+        spdlog::get("main")->debug("Initializing Sysrepo ietf-hardware callback");
+        auto sysrepoIETFHardware = velia::ietf_hardware::sysrepo::Sysrepo(srSubscription, ietfHardware);
 
-        // input configuration
-        std::set<std::string> ignoredUnits(args["--systemd-ignore-unit"].asStringList().begin(), args["--systemd-ignore-unit"].asStringList().end());
-        spdlog::get("main")->debug("Starting DBus systemd watcher");
-        if (!ignoredUnits.empty()) {
-            spdlog::get("main")->debug("Systemd input will ignore changes of the following units: {}", args["--systemd-ignore-unit"]);
-        }
-        auto inputSystemdDbus = std::make_shared<velia::health::DbusSystemdInput>(manager, ignoredUnits, *g_dbusConnection);
-
-        spdlog::get("main")->debug("All inputs initialized.");
-
-        DBUS_EVENTLOOP_END;
+        waitUntilSignaled();
 
         return 0;
     } catch (std::exception& e) {

CMakeLists.txt
@@ -160,18 +160,14 @@ target_link_libraries(velia-firewall
 # - daemons
 add_executable(veliad
     src/main.cpp
-    src/ietf-hardware/Factory.cpp
-    src/ietf-hardware/Factory.h
     src/health/Factory.cpp
     src/health/Factory.h
     )
 target_link_libraries(veliad
-        PUBLIC
+    PUBLIC
         velia-health
-        velia-ietf-hardware
-        velia-ietf-hardware-sysrepo
         docopt
-        )
+    )
 add_dependencies(veliad target-VELIA_VERSION)
 target_include_directories(veliad PUBLIC ${CMAKE_BINARY_DIR})
 
@@ -198,6 +194,20 @@ target_link_libraries(veliad-firewall
     )
 add_dependencies(veliad-firewall target-VELIA_VERSION)
 target_include_directories(veliad-firewall PUBLIC ${CMAKE_BINARY_DIR})
+
+add_executable(veliad-hardware
+    src/main-hardware.cpp
+    src/ietf-hardware/Factory.cpp
+    src/ietf-hardware/Factory.h
+    )
+target_link_libraries(veliad-hardware
+    PUBLIC
+        velia-ietf-hardware
+        velia-ietf-hardware-sysrepo
+        docopt
+    )
+add_dependencies(veliad-hardware target-VELIA_VERSION)
+target_include_directories(veliad-hardware PUBLIC ${CMAKE_BINARY_DIR})
 
 # Testing
 include(CTest)
@@ -390,3 +400,4 @@ set(YANG_DIR ${CMAKE_INSTALL_PREFIX}/share/velia/yang)
 install(FILES ${YANG_SRCS} DESTINATION ${YANG_DIR})
 install(TARGETS veliad RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/)
 install(TARGETS veliad-system RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/)
+install(TARGETS veliad-hardware RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/)

commit 2b54d68162000e4fd7658ee4808966e57b27312a

hardware: fix daemon name
Fixes: aae9fe33 veliad-hardware: sysrepo daemon managing hardware
Change-Id: If5961b95b5c7b4f557202252c6a6c75731b33b32

src/main-hardware.cpp
@@ -46,7 +46,7 @@ int main(int argc, char* argv[])
         loggingSink = std::make_shared<spdlog::sinks::ansicolor_stderr_sink_mt>();
     }
 
-    auto args = docopt::docopt(usage, {argv + 1, argv + argc}, true, "veliad-system " VELIA_VERSION, true);
+    auto args = docopt::docopt(usage, {argv + 1, argv + argc}, true, "veliad-hardware " VELIA_VERSION, true);
 
     velia::utils::initLogs(loggingSink);
     velia::utils::initLogsSysrepo();
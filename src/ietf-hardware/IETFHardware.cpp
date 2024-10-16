CESNET/velia

commit c98da3845a61c46f2d4f8ac2a70b4ac6b9203d22

hw: read static serial numbers from EEPROMs
On the most recent HW revisions of Clearfog (both the SoM and the
carrier board), there should be some EEPROMs that are factory-populated
with ONIE-compatible TLV data. The EEPROMs also happen to be these
semi-magic models which embed some unique data at the end of the memory
space (and within a HW-protected, read-only range).

On some of these boards, the EEPROMs are not populated at all (which is
expected on pre-1.3 "base" carrier and on some older SoM). The majority
of our HW actually has this old revision, so we cannot really hard-fail
with an error message. This is easily detectable by a missing "eeprom"
file in sysfs (but the dir entry for the I2C slave is always present, as
long as the Device Tree is not borked).

The other source of breakage is that these EEPROMs are documented to
contain some ONIE-compatible TLV data, but I have yet to see an EEPROM
with *any* data burned in; the ones which we have are usually empty.
However, in future we *will* probably get our hands on a ClearFog Base
or a SoM with these TLV data built in, and on these models, we *should*
show serial numbers from these EEPROMs which will, hopefully, match the
printed barcode that's surely found somewhere on the PCBs.

In the meanwhile, though, we can leverage that unique UID/EUI-48 code
that's available at the end of the EEPROM space, and in fact, there's no
harm in showing both on HW which has both the magic EEPROM and some
actual TLV data. So I felt that the cleanest solution for this is to put
a new node just for the EEPROM into ietf-hardware, and put the S/N from
the unique part of each EEPROM in there. This required some extra
handling of the `oper-state` leaf so as not to confuse the downstream
consumers on legacy boards with no populated EEPROM.

Compared to all that mess, the EEPROMs which we're putting on our PCBs
are also magic, but in a different way: these EEPROM chips occupy a pair

src/ietf-hardware/IETFHardware.cpp
@@ -5,7 +5,9 @@
  *
  */
 
+#include <boost/algorithm/hex.hpp>
 #include <chrono>
+#include <filesystem>
 #include <libyang-cpp/Time.hpp>
 #include <utility>
 #include "IETFHardware.h"
@@ -24,7 +26,7 @@ std::string xpathForComponent(const std::string& componentName)
 }
 
 /** @brief Prefix all properties from values DataTree with a component name (calculated from @p componentName) and push them into the DataTree */
-void addComponent(velia::ietf_hardware::DataTree& res, const std::string& componentName, const std::optional<std::string>& parent, const velia::ietf_hardware::DataTree& values)
+void addComponent(velia::ietf_hardware::DataTree& res, const std::string& componentName, const std::optional<std::string>& parent, const velia::ietf_hardware::DataTree& values, const std::string& operState = "enabled")
 {
     auto componentPrefix = xpathForComponent(componentName);
 
@@ -35,7 +37,7 @@ void addComponent(velia::ietf_hardware::DataTree& res, const std::string& compon
         res[componentPrefix + k] = v;
     }
 
-    res[componentPrefix + "state/oper-state"] = "enabled";
+    res[componentPrefix + "state/oper-state"] = operState;
 }
 
 void writeSensorValue(velia::ietf_hardware::DataTree& res, const std::string& componentName, const std::string& value, const std::string& operStatus)
@@ -353,5 +355,68 @@ SensorPollData EMMC::operator()() const
 
     return {data, ThresholdsBySensorPath{{xpathForComponent(m_componentName + ":lifetime") + "sensor-data/value", m_thresholds}}, {}};
 }
+
+EepromWithUid::EepromWithUid(std::string componentName, std::optional<std::string> parent, const std::string& sysfsPrefix, const uint8_t bus, const uint8_t address, const uint32_t totalSize, const uint32_t offset, const uint32_t length)
+    : DataReader(std::move(componentName), std::move(parent))
+{
+    DataTree tree{
+        {"class", "iana-hardware:module"},
+    };
+
+    if (auto sn = hexEEPROM(sysfsPrefix, bus, address, totalSize, offset, length)) {
+        tree["serial-num"] = *sn;
+    }
+
+    addComponent(m_staticData,
+                 m_componentName,
+                 m_parent,
+                 tree,
+                 tree.count("serial-num") ? "enabled" : "disabled");
+}
+
+SensorPollData EepromWithUid::operator()() const { return {m_staticData, {}, {}}; }
+}
+
+std::optional<std::string> hexEEPROM(const std::string& sysfsPrefix,
+                                     const uint8_t bus,
+                                     const uint8_t address,
+                                     const uint32_t totalSize,
+                                     const uint32_t offset,
+                                     const uint32_t length)
+{
+    namespace fs = std::filesystem;
+    auto log = spdlog::get("hardware");
+
+    if (offset + length > totalSize) {
+        throw std::logic_error{"EEPROM: region out of range"};
+    }
+
+    auto dirname = fs::path{fmt::format("{}/bus/i2c/devices/{}-{:04x}", sysfsPrefix, bus, address)};
+    if (!fs::is_directory(dirname)) {
+        // this is a hard error because the device is expected to always exist, even when it fails to probe
+        throw std::runtime_error{fmt::format("EEPROM: no I2C device defined at bus {} address 0x{:02x}", bus, address)};
+    }
+    auto filename = dirname / "eeprom";
+    try {
+        // any errors are "soft errors": older clearfog boards don't have these EEPROMs populated at all
+        if (!fs::is_regular_file(filename)) {
+            throw std::runtime_error{"sysfs entry missing"};
+        }
+        std::ifstream stream;
+        stream.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+        stream.open(filename, std::ios_base::in | std::ios_base::binary);
+        std::vector<uint8_t> buf(std::istreambuf_iterator<char>{stream}, {});
+        if (buf.size() != totalSize) {
+            throw std::runtime_error{fmt::format("expected {} bytes of data, got {}", totalSize, buf.size())};
+        }
+        std::string res;
+        res.reserve(length * 2 /* two hex characters per byte */);
+        boost::algorithm::hex(buf.begin() + offset, buf.end(), std::back_inserter(res));
+        log->trace("I2C EEPROM at bus {} address {:#02x}: UID/EUI {}", bus, address, res);
+        return res;
+    } catch (std::exception& e) {
+        log->error("EEPROM: cannot read from {}: {}", filename.string(), e.what());
+    }
+    return std::nullopt;
 }
 }

tests/hardware_eeprom.cpp
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#include "trompeloeil_doctest.h"
+#include <filesystem>
+#include "ietf-hardware/IETFHardware.h"
+#include "pretty_printers.h"
+#include "test_log_setup.h"
+#include "tests/configure.cmake.h"
+
+using namespace std::literals;
+
+TEST_CASE("EEPROM with UID/EID")
+{
+    using namespace velia::ietf_hardware;
+    using namespace data_reader;
+    TEST_INIT_LOGS;
+    const auto sysfs = CMAKE_CURRENT_SOURCE_DIR + "/tests/sysfs/"s;
+
+    REQUIRE(*hexEEPROM(sysfs, 1, 0x5c, 16, 0, 16) == "1E70C61C941000628C2EA000A000000C");
+
+    auto working = EepromWithUid("x:eeprom", "x", sysfs, 0, 0x52, 256, 256 - 6, 6);
+    REQUIRE(working().data == DataTree{
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/class", "iana-hardware:module"},
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/parent", "x"},
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/serial-num", "294100B13DA3"},
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/state/oper-state", "enabled"},
+            });
+
+    auto missing = EepromWithUid("x:eeprom", "x", sysfs, 0, 0x53, 256, 256 - 6, 6);
+    REQUIRE(missing().data == DataTree{
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/class", "iana-hardware:module"},
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/parent", "x"},
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/state/oper-state", "disabled"},
+            });
+
+    auto corrupted = EepromWithUid("x:eeprom", "x", sysfs, 0, 0x53, 16, 2, 6);
+    REQUIRE(corrupted().data == DataTree{
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/class", "iana-hardware:module"},
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/parent", "x"},
+                {"/ietf-hardware:hardware/component[name='x:eeprom']/state/oper-state", "disabled"},
+            });
+
+    REQUIRE_THROWS_WITH(EepromWithUid("x:eeprom", "x", sysfs, 0, 0x20, 256, 256 - 6, 6),
+                        "EEPROM: no I2C device defined at bus 0 address 0x20");
+
+    REQUIRE_THROWS_WITH(hexEEPROM(sysfs, 0, 0, 10, 5, 6),
+                        "EEPROM: region out of range");
+}

CMakeLists.txt
@@ -404,6 +404,7 @@ if(BUILD_TESTING)
     velia_test(NAME system_lldp LIBRARIES velia-system DbusTesting)
 
     velia_test(NAME hardware_thresholds LIBRARIES velia-ietf-hardware)
+    velia_test(NAME hardware_eeprom LIBRARIES velia-ietf-hardware)
     velia_test(NAME hardware_emmc LIBRARIES velia-ietf-hardware FsTestUtils)
     velia_test(NAME hardware_hwmon LIBRARIES velia-ietf-hardware FsTestUtils)
     velia_test(NAME hardware_fspyh LIBRARIES velia-ietf-hardware FsTestUtils)

src/ietf-hardware/Factory.cpp
@@ -7,6 +7,7 @@
 
 namespace velia::ietf_hardware {
 using velia::ietf_hardware::data_reader::EMMC;
+using velia::ietf_hardware::data_reader::EepromWithUid;
 using velia::ietf_hardware::data_reader::Fans;
 using velia::ietf_hardware::data_reader::SensorType;
 using velia::ietf_hardware::data_reader::StaticData;
@@ -69,11 +70,28 @@ std::shared_ptr<IETFHardware> create(const std::string& applianceName)
         auto emmc = std::make_shared<velia::ietf_hardware::sysfs::EMMC>("/sys/block/mmcblk0/device/");
 
         /* FIXME:
-         * Publish more properties for ne element. We have an EEPROM at the PCB for storing serial numbers (etc.), but it's so far unused. We could also use U-Boot env variables
+         * - handle dynamic hot plug of the ne:fans, read (and re-read) its EEPROM for the S/N
          */
         ietfHardware->registerDataReader(StaticData("ne", std::nullopt, {{"class", "iana-hardware:chassis"}}));
 
-        ietfHardware->registerDataReader(StaticData("ne:ctrl", "ne", {{"class", "iana-hardware:module"}}));
+        ietfHardware->registerDataReader(StaticData{"ne:ctrl",
+                                                    "ne",
+                                                    {
+                                                        {"class", "iana-hardware:module"},
+                                                        {"serial-num", *hexEEPROM("/sys", 1, 0x5b, 16, 0, 16)},
+                                                    }});
+        try {
+            auto eeprom = hexEEPROM("/sys", 1, 0x5a, 16, 0, 16);
+            ietfHardware->registerDataReader(StaticData{"ne:voa-sw",
+                                                        "ne",
+                                                        {
+                                                            {"class", "iana-hardware:module"},
+                                                            {"serial-num", *eeprom},
+                                                        }});
+        } catch (std::runtime_error& e) {
+            // this EEPROM is only present on regular inline amplifiers and on ROADM line/degree boxes
+            // -> silently ignore any failures
+        }
         ietfHardware->registerDataReader(Fans("ne:fans",
                                               "ne",
                                               fans,
@@ -84,6 +102,20 @@ std::shared_ptr<IETFHardware> create(const std::string& applianceName)
                                                   .warningHigh = std::nullopt,
                                                   .criticalHigh = std::nullopt,
                                               }));
+        ietfHardware->registerDataReader(StaticData{"ne:ctrl:som",
+                                                    "ne:ctrl",
+                                                    {
+                                                        {"class", "iana-hardware:module"},
+                                                        {"model-name", "ClearFog A388 SOM"},
+                                                    }});
+        ietfHardware->registerDataReader(EepromWithUid{"ne:ctrl:som:eeprom", "ne:ctrl:som", "/sys", 0, 0x53, 256, 256 - 6, 6});
+        ietfHardware->registerDataReader(StaticData{"ne:ctrl:carrier",
+                                                    "ne:ctrl",
+                                                    {
+                                                        {"class", "iana-hardware:module"},
+                                                        {"model-name", "ClearFog Base"},
+                                                    }});
+        ietfHardware->registerDataReader(EepromWithUid{"ne:ctrl:carrier:eeprom", "ne:ctrl:carrier", "/sys", 0, 0x52, 256, 256 - 6, 6});
         ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:ctrl:temperature-front", "ne:ctrl", tempMainBoard, 1));
         ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:ctrl:temperature-cpu", "ne:ctrl", tempCpu, 1));
         ietfHardware->registerDataReader(SysfsValue<SensorType::Temperature>("ne:ctrl:temperature-rear", "ne:ctrl", tempFans, 1));

src/ietf-hardware/IETFHardware.h
@@ -90,6 +90,11 @@ private:
 };
 
 /**
+ * @brief Read a range of bytes from an EEPROM in hex
+ */
+std::optional<std::string> hexEEPROM(const std::string& sysfsPrefix, const uint8_t bus, const uint8_t address, const uint32_t totalSize, const uint32_t offset, const uint32_t length);
+
+/**
  * This namespace contains several predefined data readers for IETFHardware.
  * They are implemented as functors and fulfill the required interface -- std::function<DataTree()>
  *
@@ -166,6 +171,12 @@ private:
 
 public:
     EMMC(std::string propertyPrefix, std::optional<std::string> parent, std::shared_ptr<sysfs::EMMC> emmc, Thresholds<int64_t> thresholds = {});
+    SensorPollData operator()() const;
+};
+
+/** brief Static data and a serial number read from the trailing part of the EEPROM */
+struct EepromWithUid : private DataReader {
+    EepromWithUid(std::string componentName, std::optional<std::string> parent, const std::string& sysfsPrefix, const uint8_t bus, const uint8_t address, const uint32_t totalSize, const uint32_t offset, const uint32_t length);
     SensorPollData operator()() const;
 };
 

commit 59fbe2278fadad49f7e6028bb990625549433106

EEPROM: respect the expected size of the UID
Fixes: c98da384 hw: read static serial numbers from EEPROMs
Change-Id: I4628d833f0ceeefde740ff2173f4e739b911749d

src/ietf-hardware/IETFHardware.cpp
@@ -441,7 +441,7 @@ std::optional<std::string> hexEEPROM(const std::string& sysfsPrefix,
         }
         std::string res;
         res.reserve(length * 2 /* two hex characters per byte */);
-        boost::algorithm::hex(buf.begin() + offset, buf.end(), std::back_inserter(res));
+        boost::algorithm::hex(buf.begin() + offset, buf.begin() + offset + length, std::back_inserter(res));
         log->trace("I2C EEPROM at bus {} address {:#02x}: UID/EUI {}", bus, address, res);
         return res;
     } catch (std::exception& e) {

tests/hardware_eeprom.cpp
@@ -24,6 +24,7 @@ TEST_CASE("EEPROM with UID/EID")
     TEST_INIT_LOGS;
 
     REQUIRE(*hexEEPROM(sysfsPrefix, 1, 0x5c, 16, 0, 16) == "1E70C61C941000628C2EA000A000000C");
+    REQUIRE(*hexEEPROM(sysfsPrefix, 1, 0x5c, 16, 0, 15) == "1E70C61C941000628C2EA000A00000");
 
     auto working = EepromWithUid("x:eeprom", "x", sysfsPrefix, 0, 0x52, 256, 256 - 6, 6);
     REQUIRE(working().data == DataTree{
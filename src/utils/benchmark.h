433780673d86e7698515b494d9c234bf16eaee4a
Jan Kundrát <jan.kundrat@cesnet.cz>
2024-10-17 17:39:24 +0200

    performance: trace how much time is spent in sysrepo interactions
    I did not want to mess with passing a custom logger, so we now have a
    default one (the "main" one, obviously). I'm very much tempted to get
    rid of all the custom logging, TBH, but that's for some other time.
    
    Change-Id: I7d0a4d79e4777f77544ad0f39af8ce58de2c31a6

---
+++ src/utils/benchmark.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+*/
+
+#pragma once
+
+#include <chrono>
+#include <string_view>
+#include <source_location>
+
+namespace velia::utils {
+/** @short Log profiling information about how much time was spent in a given block */
+class MeasureTime {
+    std::chrono::time_point<std::chrono::steady_clock> start;
+    std::string what;
+public:
+    MeasureTime(const std::source_location location = std::source_location::current());
+    MeasureTime(const std::string_view message);
+    ~MeasureTime();
+};
+}
+#define WITH_TIME_MEASUREMENT ::velia::utils::MeasureTime _benchmark_ ## __LINE__

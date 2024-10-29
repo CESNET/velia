b7a44e82b863eaad25aca0b25ae2e4d735a10810
Jan Kundrát <jan.kundrat@cesnet.cz>
2024-10-29 16:06:06 +0100

    error handling in sysrepo has changed
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/7969
    Change-Id: Id028806ed49114cba4c55e2874bcf3fc98308bdc

--- tests/sysrepo_system-firmware.cpp
+++ tests/sysrepo_system-firmware.cpp
@@ -249,11 +249,10 @@ TEST_CASE("Firmware in czechlight-system, RPC")
             SECTION("Invoking second installation throws")
             {
                 REQUIRE_THROWS_WITH_AS(client.sendRPC(rpcInput),
-                        "Couldn't send RPC: SR_ERR_CALLBACK_FAILED\n"
+                        "Couldn't send RPC: SR_ERR_OPERATION_FAILED\n"
                         // FIXME: why is this present twice? Looks like a libyang-v2.2/sysrepo change that I do not understand
                         " Already processing a different method (SR_ERR_OPERATION_FAILED)\n"
                         " Already processing a different method (SR_ERR_OPERATION_FAILED)\n"
-                        " User callback failed. (SR_ERR_CALLBACK_FAILED)\n"
                         " NETCONF: application: operation-failed: Already processing a different method",
                         sysrepo::ErrorWithCode);
             }

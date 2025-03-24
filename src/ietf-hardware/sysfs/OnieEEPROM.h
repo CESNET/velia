8dfd0905393cdaa058d65cb8d546973e73a78488
Tomáš Pecka <tomas.pecka@cesnet.cz>
2025-03-24 15:56:33 +0100

    hardware: reader for ONIE EEPROMs
    We have some new ClearFog boards that have EEPROMs with data that
    follows the ONIE format [1]. Let's read the stuff from them!
    
    [1] https://opencomputeproject.github.io/onie/design-spec/hw_requirements.html#board-eeprom-information-format
    
    Change-Id: I0dc61ba5f4af25d0f0e691bffd940b6f5b62e5c4

---
+++ src/ietf-hardware/sysfs/OnieEEPROM.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2025 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+#pragma once
+#include <cstdint>
+#include <filesystem>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace velia::ietf_hardware::sysfs {
+
+struct TLV {
+    enum class Type {
+        ProductName = 0x21,
+        PartNumber = 0x22,
+        SerialNumber = 0x23,
+        ManufactureDate = 0x25,
+        DeviceVersion = 0x26,
+        Vendor = 0x2d,
+        VendorExtension = 0xfd,
+    };
+
+    Type type;
+    std::variant<std::string, uint8_t, std::vector<uint8_t>> value;
+
+    bool operator==(const TLV&) const = default;
+};
+
+using TlvInfo = std::vector<TLV>;
+
+TlvInfo onieEeprom(const std::filesystem::path& eepromPath);
+TlvInfo onieEeprom(const std::filesystem::path& sysfsPrefix, const uint8_t bus, const uint8_t address);
+}

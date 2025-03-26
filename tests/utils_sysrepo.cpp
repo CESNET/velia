CESNET/velia

commit 98ad18de51c44385e878bafe09ac429890267803

HardwareState: Add Sysrepo operational data callback
Implement Sysrepo operational data callback class.

Depends-on: https://cesnet-gerrit-czechlight/c/CzechLight/br2-external/+/3279
Depends-on: https://gerrit.cesnet.cz/c/CzechLight/br2-external/+/3279
Change-Id: I6987555dfc3541f6433e1892c62f7c4638aef4dd

CMakeLists.txt
@@ -39,6 +39,9 @@ if(${SYSTEMD_VERSION} VERSION_EQUAL 245)
     message(FATAL_ERROR "Systemd 245 has a bug affecting DBus method calls using sdbus-c++. Please see https://github.com/Kistler-Group/sdbus-cpp/issues/106.")
 endif()
 
+pkg_check_modules(SYSREPO REQUIRED sysrepo-cpp>=1.4.79 IMPORTED_TARGET sysrepo)
+pkg_check_modules(LIBYANG REQUIRED libyang-cpp>=1.0.190 IMPORTED_TARGET libyang)
+
 include(GNUInstallDirs)
 
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/src/)
@@ -114,6 +117,19 @@ target_link_libraries(velia-ietf-hardware
     )
 target_include_directories(velia-ietf-hardware PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/src/velia-hardware)
 
+add_library(velia-ietf-hardware-sysrepo STATIC
+    src/ietf-hardware/sysrepo/Logging.cpp
+    src/ietf-hardware/sysrepo/Logging.h
+    src/ietf-hardware/sysrepo/Sysrepo.cpp
+    src/ietf-hardware/sysrepo/Sysrepo.h
+    )
+target_link_libraries(velia-ietf-hardware-sysrepo
+    PUBLIC
+        velia-utils
+        PkgConfig::SYSREPO
+        PkgConfig::LIBYANG
+    )
+target_include_directories(velia-ietf-hardware PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/src/velia-hardware)
 
 # Testing
 include(CTest)
@@ -170,6 +186,28 @@ if(BUILD_TESTING)
         endif()
     endfunction()
 
+    find_program(SYSREPOCTL sysrepoctl)
+    find_program(SYSREPOCFG sysrepocfg)
+
+    set(sysrepo_previous_fixture_name UNDEFINED)
+    function(sysrepo_fixture_env name)
+        set(test_name_init sysrepo:prep:${name})
+        set(test_name_cleanup sysrepo:clean:${name})
+        set(fixture_name sysrepo:env:${name})
+
+        add_test(NAME ${test_name_init} COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/tests/sysrepoctl-manage-module.sh ${SYSREPOCTL} ${SYSREPOCFG} prepare ${ARGN})
+        set_tests_properties(${test_name_init} PROPERTIES FIXTURES_SETUP ${fixture_name} RESOURCE_LOCK sysrepo)
+        add_test(NAME ${test_name_cleanup} COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/tests/sysrepoctl-manage-module.sh ${SYSREPOCTL} ${SYSREPOCFG} uninstall ${ARGN})
+        set_tests_properties(${test_name_cleanup} PROPERTIES FIXTURES_CLEANUP ${fixture_name} RESOURCE_LOCK sysrepo)
+
+        if (sysrepo_previous_fixture_name)
+            set_tests_properties(${test_name_init} PROPERTIES DEPENDS sysrepo:clean:${sysrepo_previous_fixture_name})
+        endif ()
+
+        set(sysrepo_previous_fixture_name ${name} PARENT_SCOPE)
+    endfunction()
+
+
     velia_test(health_state-manager velia-health)
     velia_test(health_input-semaphore velia-health DbusTesting)
     velia_test(health_input-systemd velia-health DbusTesting)
@@ -177,7 +215,13 @@ if(BUILD_TESTING)
 
     velia_test(hardware_emmc velia-ietf-hardware FsTestUtils)
     velia_test(hardware_hwmon velia-ietf-hardware FsTestUtils)
-    velia_test(hardware_ietf-hardware velia-ietf-hardware)
+    velia_test(hardware_ietf-hardware velia-ietf-hardware velia-ietf-hardware-sysrepo)
+    sysrepo_fixture_env(sysrepo YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/iana-hardware@2018-03-13.yang YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-hardware-state@2018-03-13.yang FEATURE hardware-sensor)
+    set_tests_properties(
+            test-hardware_ietf-hardware
+            PROPERTIES FIXTURES_REQUIRED sysrepo:env:sysrepo
+            RESOURCE_LOCK sysrepo
+    )
 
     find_program(YANGLINT_PATH yanglint)
     if (NOT YANGLINT_PATH)

ci/build.sh
@@ -43,6 +43,7 @@ if [[ $ZUUL_JOB_NAME =~ .*-tsan ]]; then
     export CFLAGS="-fsanitize=thread ${CFLAGS}"
     export CXXFLAGS="-fsanitize=thread ${CXXFLAGS}"
     export LDFLAGS="-fsanitize=thread ${LDFLAGS}"
+    export TSAN_OPTIONS="suppressions=$HOME/target/tsan.supp:detect_deadlocks=0"
 fi
 
 if [[ $ZUUL_JOB_NAME =~ .*-cover.* ]]; then

src/ietf-hardware/sysrepo/Logging.cpp
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+extern "C" {
+#include <sysrepo.h>
+}
+
+#include "Logging.h"
+#include "utils/log.h"
+
+extern "C" {
+/** @short Propagate sysrepo events to spdlog */
+static void spdlog_sr_log_cb(sr_log_level_t level, const char* message)
+{
+    // Thread safety note: this is, as far as I know, thread safe:
+    // - the static initialization itself is OK
+    // - all loggers which we instantiate are thread-safe
+    // - std::shared_ptr::operator-> is const, and all const members of that class are documented to be thread-safe
+    static auto log = spdlog::get("sysrepo");
+    assert(log);
+    switch (level) {
+    case SR_LL_NONE:
+    case SR_LL_ERR:
+        log->error(message);
+        break;
+    case SR_LL_WRN:
+        log->warn(message);
+        break;
+    case SR_LL_INF:
+        log->info(message);
+        break;
+    case SR_LL_DBG:
+        log->debug(message);
+        break;
+    }
+}
+}
+
+namespace velia::ietf_hardware::sysrepo {
+
+/** @short Setup sysrepo log forwarding
+You must call cla::utils::initLogs prior to this function.
+*/
+void initLogs()
+{
+    sr_log_set_cb(spdlog_sr_log_cb);
+}
+}

src/ietf-hardware/sysrepo/Logging.h
@@ -1,0 +1,15 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+#pragma once
+
+/** @file
+  * @short sysrepo-spdlog bridge
+*/
+namespace velia::ietf_hardware::sysrepo {
+
+void initLogs();
+}

src/ietf-hardware/sysrepo/Sysrepo.cpp
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+
+#include "Sysrepo.h"
+#include "utils/log.h"
+
+namespace velia::ietf_hardware::sysrepo {
+
+namespace {
+
+const auto IETF_HARDWARE_MODULE_NAME = "ietf-hardware-state"s;
+const auto IETF_HARDWARE_MODULE_PREFIX = "/"s + IETF_HARDWARE_MODULE_NAME + ":hardware"s;
+
+void valuesToYang(const std::map<std::string, std::string>& values, std::shared_ptr<::sysrepo::Session> session, std::shared_ptr<libyang::Data_Node>& parent)
+{
+    for (const auto& [propertyName, value] : values) {
+        spdlog::get("main")->trace("propertyName: {}, value: {}", propertyName, value.c_str());
+
+        if (!parent) {
+            parent = std::make_shared<libyang::Data_Node>(
+                session->get_context(),
+                propertyName.c_str(),
+                value.c_str(),
+                LYD_ANYDATA_CONSTSTRING,
+                LYD_PATH_OPT_OUTPUT);
+        } else {
+            parent->new_path(
+                session->get_context(),
+                propertyName.c_str(),
+                value.c_str(),
+                LYD_ANYDATA_CONSTSTRING,
+                LYD_PATH_OPT_OUTPUT);
+        }
+    }
+}
+}
+
+/** @brief The constructor expects the HardwareState instance which will provide the actual hardware state data */
+Sysrepo::Sysrepo(std::shared_ptr<::sysrepo::Subscribe> srSubscribe, std::shared_ptr<IETFHardware> hwState)
+    : m_hwState(std::move(hwState))
+    , m_srSubscribe(std::move(srSubscribe))
+    , m_srLastRequestId(0)
+{
+    m_srSubscribe->oper_get_items_subscribe(
+        IETF_HARDWARE_MODULE_NAME.c_str(),
+        [this](std::shared_ptr<::sysrepo::Session> session, const char* module_name, const char* xpath, const char* request_xpath, uint32_t request_id, std::shared_ptr<libyang::Data_Node>& parent) {
+            spdlog::get("main")->debug("operational data callback: XPath {} req {} orig-XPath {}", xpath, request_id, request_xpath);
+
+            // when asking for something in the subtree of THIS request
+            if (m_srLastRequestId == request_id) {
+                spdlog::trace(" ops data request already handled");
+                return SR_ERR_OK;
+            }
+
+            m_srLastRequestId = request_id;
+
+            auto ctx = session->get_context();
+            auto mod = ctx->get_module(module_name);
+
+            auto hwStateValues = m_hwState->process();
+            valuesToYang(hwStateValues, session, parent);
+
+            spdlog::get("main")->trace("Pushing to sysrepo (JSON): {}", parent->print_mem(LYD_FORMAT::LYD_JSON, 0));
+
+            return SR_ERR_OK;
+        },
+        IETF_HARDWARE_MODULE_PREFIX.c_str(),
+        SR_SUBSCR_PASSIVE | SR_SUBSCR_OPER_MERGE | SR_SUBSCR_CTX_REUSE);
+}
+}

src/ietf-hardware/sysrepo/Sysrepo.h
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+#pragma once
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <optional>
+#include <sysrepo-cpp/Session.hpp>
+#include "ietf-hardware/IETFHardware.h"
+
+namespace velia::ietf_hardware::sysrepo {
+
+/** @class Sysrepo
+ *  A callback class for operational data in Sysrepo. This class expects a shared_pointer<HardwareState> instance.
+ *  When sysrepo callbacks for the data in the subtree this callback is registered for, it asks HardwareState instace
+ *  for the data it should return back to Sysrepo.
+ *  OpsCallback then creates the YANG tree structure from the data returned by HardwareState and returns it.
+ *
+ *  @see velia::ietf_hardware::IETFHardware
+ */
+class Sysrepo {
+public:
+    Sysrepo(std::shared_ptr<::sysrepo::Subscribe> srSubscribe, std::shared_ptr<IETFHardware> driver);
+
+private:
+    std::shared_ptr<IETFHardware> m_hwState;
+    std::shared_ptr<::sysrepo::Subscribe> m_srSubscribe;
+    uint64_t m_srLastRequestId;
+};
+}

src/utils/log-init.cpp
@@ -18,7 +18,7 @@ Creates and registers all required loggers and connect them to the provided sink
 */
 void initLogs(std::shared_ptr<spdlog::sinks::sink> sink)
 {
-    for (const auto& name : std::vector<std::string> {"main", "hardware"}) {
+    for (const auto& name : std::vector<std::string> {"main", "hardware", "sysrepo"}) {
         spdlog::register_logger(std::make_shared<spdlog::logger>(name, sink));
     }
 }

tests/hardware_ietf-hardware.cpp
@@ -1,9 +1,11 @@
 #include "trompeloeil_doctest.h"
 #include <iterator>
 #include "ietf-hardware/IETFHardware.h"
+#include "ietf-hardware/sysrepo/Sysrepo.h"
 #include "mock/ietf_hardware.h"
 #include "pretty_printers.h"
 #include "test_log_setup.h"
+#include "test_sysrepo_helpers.h"
 #include "tests/configure.cmake.h"
 
 using namespace std::literals;
@@ -11,7 +13,6 @@ using namespace std::literals;
 TEST_CASE("HardwareState")
 {
     TEST_INIT_LOGS;
-
     static const auto modulePrefix = "/ietf-hardware-state:hardware"s;
 
     trompeloeil::sequence seq1;
@@ -162,5 +163,164 @@ TEST_CASE("HardwareState")
         auto result = hwState->process();
         result.erase(modulePrefix + "/last-change");
         REQUIRE(result == expected);
+    }
+
+    SECTION("Test HardwareState from sysrepo's view")
+    {
+        TEST_SYSREPO_INIT_LOGS;
+
+        auto srConn = std::make_shared<sysrepo::Connection>();
+        auto srSess = std::make_shared<sysrepo::Session>(srConn);
+        auto srSubs = std::make_shared<sysrepo::Subscribe>(srSess);
+        auto ietfHardwareSysrepo = std::make_shared<velia::ietf_hardware::sysrepo::Sysrepo>(srSubs, hwState);
+
+        SECTION("test last-change")
+        {
+            // at least check that there is some timestamp
+            srSess->session_switch_ds(SR_DS_OPERATIONAL);
+            REQUIRE(dataFromSysrepo(srSess, modulePrefix).count("/last-change") > 0);
+            srSess->session_switch_ds(SR_DS_RUNNING);
+        }
+
+        SECTION("test components")
+        {
+            std::map<std::string, std::string> expected = {
+                {"[name='ne']/name", "ne"},
+                {"[name='ne']/class", "iana-hardware:chassis"},
+                {"[name='ne']/mfg-name", "CESNET"},
+                {"[name='ne']/sensor-data", ""},
+
+                {"[name='ne:fans']/class", "iana-hardware:module"},
+                {"[name='ne:fans']/name", "ne:fans"},
+                {"[name='ne:fans']/parent", "ne"},
+                {"[name='ne:fans']/sensor-data", ""},
+                {"[name='ne:fans:fan1']/class", "iana-hardware:fan"},
+                {"[name='ne:fans:fan1']/name", "ne:fans:fan1"},
+                {"[name='ne:fans:fan1']/parent", "ne:fans"},
+                {"[name='ne:fans:fan1']/sensor-data", ""},
+                {"[name='ne:fans:fan1:rpm']/class", "iana-hardware:sensor"},
+                {"[name='ne:fans:fan1:rpm']/name", "ne:fans:fan1:rpm"},
+                {"[name='ne:fans:fan1:rpm']/parent", "ne:fans:fan1"},
+                {"[name='ne:fans:fan1:rpm']/sensor-data", ""},
+                {"[name='ne:fans:fan1:rpm']/sensor-data/oper-status", "ok"},
+                {"[name='ne:fans:fan1:rpm']/sensor-data/value", "253"},
+                {"[name='ne:fans:fan1:rpm']/sensor-data/value-precision", "0"},
+                {"[name='ne:fans:fan1:rpm']/sensor-data/value-scale", "units"},
+                {"[name='ne:fans:fan1:rpm']/sensor-data/value-type", "rpm"},
+                {"[name='ne:fans:fan2']/class", "iana-hardware:fan"},
+                {"[name='ne:fans:fan2']/name", "ne:fans:fan2"},
+                {"[name='ne:fans:fan2']/parent", "ne:fans"},
+                {"[name='ne:fans:fan2']/sensor-data", ""},
+                {"[name='ne:fans:fan2:rpm']/class", "iana-hardware:sensor"},
+                {"[name='ne:fans:fan2:rpm']/name", "ne:fans:fan2:rpm"},
+                {"[name='ne:fans:fan2:rpm']/parent", "ne:fans:fan2"},
+                {"[name='ne:fans:fan2:rpm']/sensor-data", ""},
+                {"[name='ne:fans:fan2:rpm']/sensor-data/oper-status", "ok"},
+                {"[name='ne:fans:fan2:rpm']/sensor-data/value", "0"},
+                {"[name='ne:fans:fan2:rpm']/sensor-data/value-precision", "0"},
+                {"[name='ne:fans:fan2:rpm']/sensor-data/value-scale", "units"},
+                {"[name='ne:fans:fan2:rpm']/sensor-data/value-type", "rpm"},
+                {"[name='ne:fans:fan3']/class", "iana-hardware:fan"},
+                {"[name='ne:fans:fan3']/name", "ne:fans:fan3"},
+                {"[name='ne:fans:fan3']/parent", "ne:fans"},
+                {"[name='ne:fans:fan3']/sensor-data", ""},
+                {"[name='ne:fans:fan3:rpm']/class", "iana-hardware:sensor"},
+                {"[name='ne:fans:fan3:rpm']/name", "ne:fans:fan3:rpm"},
+                {"[name='ne:fans:fan3:rpm']/parent", "ne:fans:fan3"},
+                {"[name='ne:fans:fan3:rpm']/sensor-data", ""},
+                {"[name='ne:fans:fan3:rpm']/sensor-data/oper-status", "ok"},
+                {"[name='ne:fans:fan3:rpm']/sensor-data/value", "1280"},
+                {"[name='ne:fans:fan3:rpm']/sensor-data/value-precision", "0"},
+                {"[name='ne:fans:fan3:rpm']/sensor-data/value-scale", "units"},
+                {"[name='ne:fans:fan3:rpm']/sensor-data/value-type", "rpm"},
+                {"[name='ne:fans:fan4']/class", "iana-hardware:fan"},
+                {"[name='ne:fans:fan4']/name", "ne:fans:fan4"},
+                {"[name='ne:fans:fan4']/parent", "ne:fans"},
+                {"[name='ne:fans:fan4']/sensor-data", ""},
+                {"[name='ne:fans:fan4:rpm']/class", "iana-hardware:sensor"},
+                {"[name='ne:fans:fan4:rpm']/name", "ne:fans:fan4:rpm"},
+                {"[name='ne:fans:fan4:rpm']/parent", "ne:fans:fan4"},
+                {"[name='ne:fans:fan4:rpm']/sensor-data", ""},
+                {"[name='ne:fans:fan4:rpm']/sensor-data/oper-status", "ok"},
+                {"[name='ne:fans:fan4:rpm']/sensor-data/value", "666"},
+                {"[name='ne:fans:fan4:rpm']/sensor-data/value-precision", "0"},
+                {"[name='ne:fans:fan4:rpm']/sensor-data/value-scale", "units"},
+                {"[name='ne:fans:fan4:rpm']/sensor-data/value-type", "rpm"},
+
+                {"[name='ne:ctrl']/name", "ne:ctrl"},
+                {"[name='ne:ctrl']/parent", "ne"},
+                {"[name='ne:ctrl']/class", "iana-hardware:module"},
+                {"[name='ne:ctrl']/sensor-data", ""},
+
+                {"[name='ne:ctrl:temperature-cpu']/name", "ne:ctrl:temperature-cpu"},
+                {"[name='ne:ctrl:temperature-cpu']/class", "iana-hardware:sensor"},
+                {"[name='ne:ctrl:temperature-cpu']/parent", "ne:ctrl"},
+                {"[name='ne:ctrl:temperature-cpu']/sensor-data", ""},
+                {"[name='ne:ctrl:temperature-cpu']/sensor-data/oper-status", "ok"},
+                {"[name='ne:ctrl:temperature-cpu']/sensor-data/value", "41800"},
+                {"[name='ne:ctrl:temperature-cpu']/sensor-data/value-precision", "0"},
+                {"[name='ne:ctrl:temperature-cpu']/sensor-data/value-scale", "milli"},
+                {"[name='ne:ctrl:temperature-cpu']/sensor-data/value-type", "celsius"},
+                {"[name='ne:ctrl:temperature-front']/name", "ne:ctrl:temperature-front"},
+                {"[name='ne:ctrl:temperature-front']/class", "iana-hardware:sensor"},
+                {"[name='ne:ctrl:temperature-front']/parent", "ne:ctrl"},
+                {"[name='ne:ctrl:temperature-front']/sensor-data", ""},
+                {"[name='ne:ctrl:temperature-front']/sensor-data/oper-status", "ok"},
+                {"[name='ne:ctrl:temperature-front']/sensor-data/value", "30800"},
+                {"[name='ne:ctrl:temperature-front']/sensor-data/value-precision", "0"},
+                {"[name='ne:ctrl:temperature-front']/sensor-data/value-scale", "milli"},
+                {"[name='ne:ctrl:temperature-front']/sensor-data/value-type", "celsius"},
+                {"[name='ne:ctrl:temperature-internal-0']/name", "ne:ctrl:temperature-internal-0"},
+                {"[name='ne:ctrl:temperature-internal-0']/class", "iana-hardware:sensor"},
+                {"[name='ne:ctrl:temperature-internal-0']/parent", "ne:ctrl"},
+                {"[name='ne:ctrl:temperature-internal-0']/sensor-data", ""},
+                {"[name='ne:ctrl:temperature-internal-0']/sensor-data/oper-status", "ok"},
+                {"[name='ne:ctrl:temperature-internal-0']/sensor-data/value", "39000"},
+                {"[name='ne:ctrl:temperature-internal-0']/sensor-data/value-precision", "0"},
+                {"[name='ne:ctrl:temperature-internal-0']/sensor-data/value-scale", "milli"},
+                {"[name='ne:ctrl:temperature-internal-0']/sensor-data/value-type", "celsius"},
+                {"[name='ne:ctrl:temperature-internal-1']/name", "ne:ctrl:temperature-internal-1"},
+                {"[name='ne:ctrl:temperature-internal-1']/class", "iana-hardware:sensor"},
+                {"[name='ne:ctrl:temperature-internal-1']/parent", "ne:ctrl"},
+                {"[name='ne:ctrl:temperature-internal-1']/sensor-data", ""},
+                {"[name='ne:ctrl:temperature-internal-1']/sensor-data/oper-status", "ok"},
+                {"[name='ne:ctrl:temperature-internal-1']/sensor-data/value", "36000"},
+                {"[name='ne:ctrl:temperature-internal-1']/sensor-data/value-precision", "0"},
+                {"[name='ne:ctrl:temperature-internal-1']/sensor-data/value-scale", "milli"},
+                {"[name='ne:ctrl:temperature-internal-1']/sensor-data/value-type", "celsius"},
+
+                {"[name='ne:ctrl:emmc']/name", "ne:ctrl:emmc"},
+                {"[name='ne:ctrl:emmc']/parent", "ne:ctrl"},
+                {"[name='ne:ctrl:emmc']/class", "iana-hardware:module"},
+                {"[name='ne:ctrl:emmc']/serial-num", "0x00a8808d"},
+                {"[name='ne:ctrl:emmc']/mfg-date", "2017-02-01T00:00:00Z"},
+                {"[name='ne:ctrl:emmc']/model-name", "8GME4R"},
+                {"[name='ne:ctrl:emmc']/sensor-data", ""},
+                {"[name='ne:ctrl:emmc:lifetime']/name", "ne:ctrl:emmc:lifetime"},
+                {"[name='ne:ctrl:emmc:lifetime']/class", "iana-hardware:sensor"},
+                {"[name='ne:ctrl:emmc:lifetime']/parent", "ne:ctrl:emmc"},
+                {"[name='ne:ctrl:emmc:lifetime']/sensor-data", ""},
+                {"[name='ne:ctrl:emmc:lifetime']/sensor-data/oper-status", "ok"},
+                {"[name='ne:ctrl:emmc:lifetime']/sensor-data/value", "40"},
+                {"[name='ne:ctrl:emmc:lifetime']/sensor-data/value-precision", "0"},
+                {"[name='ne:ctrl:emmc:lifetime']/sensor-data/value-scale", "units"},
+                {"[name='ne:ctrl:emmc:lifetime']/sensor-data/value-type", "other"},
+                {"[name='ne:ctrl:emmc:lifetime']/sensor-data/units-display", "percent"},
+            };
+
+            srSess->session_switch_ds(SR_DS_OPERATIONAL);
+            REQUIRE(dataFromSysrepo(srSess, modulePrefix + "/component") == expected);
+            srSess->session_switch_ds(SR_DS_RUNNING);
+        }
+
+        SECTION("test leafnode query")
+        {
+            const auto xpath = modulePrefix + "/component[name='ne:ctrl:emmc:lifetime']/class";
+            srSess->session_switch_ds(SR_DS_OPERATIONAL);
+            auto val = srSess->get_item(xpath.c_str());
+            srSess->session_switch_ds(SR_DS_RUNNING);
+            REQUIRE(!!val);
+            REQUIRE(val->data()->get_identityref() == "iana-hardware:sensor"s);
+        }
     }
 }

commit 2b7187405740fe61e8b37e0efab8c3173d398a26

utils: Fix valuesToYang creating wrong edits
We have seen on real devices that the /ietf-interfaces:interfaces
container in operation datastore was not present.

After some debugging we have discovered that sysrepo Session::editBatch
does not consider all the trees in the edit, but only those following
the passed edit tree (and it is actually properly documented[1]).

Our utils::valuesToYang function was creating a forest of trees, and the
handle DataNode was not necessarily the first sibling. This caused the
edit not to push the data we wanted.

This did not appear before, because (I think) we were always pushing
edit containing only one tree (i.e. one container of one module). But
after recent changes in sysrepo, the edit contains all the changes to
the operational DS done by this session, so now it is easy to have more
than one tree in the edit.
It is fascinating that the bug was there since, well, always.

[1] https://github.com/sysrepo/sysrepo/blob/ef93a1253cc97f13671759f6e7790cbf729a5ae9/src/sysrepo.h#L1023

Fixes: 98ad18de ("HardwareState: Add Sysrepo operational data callback")
Change-Id: I0106751f25c3983b92dc6b5a05ee48369c3bea2a

CMakeLists.txt
@@ -450,6 +450,17 @@ if(BUILD_TESTING)
             --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/czechlight-network@2021-02-22.yang)
     velia_test(NAME sysrepo_interfaces-systemd-networkd LIBRARIES velia-system FIXTURE fixture_sysrepo-czechlight-network)
 
+    set(fixture_sysrepo-utils
+            --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-system@2014-08-06.yang
+            --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/iana-if-type@2017-01-19.yang
+            --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-interfaces@2018-02-20.yang
+            --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-ip@2018-02-22.yang
+            --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-routing@2018-03-13.yang
+            --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-ipv4-unicast-routing@2018-03-13.yang
+            --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-ipv6-unicast-routing@2018-03-13.yang
+            --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/czechlight-network@2021-02-22.yang)
+    velia_test(NAME utils_sysrepo LIBRARIES velia-utils SysrepoTesting FIXTURE fixture_sysrepo-utils)
+
     find_program(UNSHARE_EXECUTABLE unshare REQUIRED)
     find_program(MOUNT_EXECUTABLE mount REQUIRED)
     pkg_get_variable(SR_GROUP sysrepo SR_GROUP)

src/utils/sysrepo.cpp
@@ -101,6 +101,13 @@ void valuesToYang(const YANGData& values, const std::vector<std::string>& foreig
             log->trace("Cannot remove {} from stored ops edit: not found", xpath);
         }
     }
+
+    /* We could have added a new node to the parent which is a sibling to the left. This could be a problem because
+       libyang's printing the tree with WithSiblings flag does not print the left siblings, and so does sysrepo's editBatch.
+    */
+    if (parent) {
+        parent = parent->firstSibling();
+    }
 }
 
 /** @brief Update the operational DS

tests/utils_sysrepo.cpp
@@ -1,0 +1,58 @@
+#include "trompeloeil_doctest.h"
+#include "system_vars.h"
+#include "tests/pretty_printers.h"
+#include "tests/test_log_setup.h"
+#include "utils/sysrepo.h"
+
+using namespace std::literals;
+
+TEST_CASE("sysrepo utils")
+{
+    TEST_INIT_LOGS;
+    TEST_SYSREPO_INIT;
+    srSess.sendRPC(srSess.getContext().newPath("/ietf-factory-default:factory-reset"));
+
+    SECTION("Values to yang return edit's first sibling")
+    {
+        srSess.switchDatastore(sysrepo::Datastore::Operational);
+
+        srSess.setItem("/ietf-system:system-state/platform/os-name", "GNU/Linux");
+        srSess.applyChanges();
+
+        auto edit = srSess.operationalChanges();
+        REQUIRE(edit);
+        REQUIRE(*edit->printStr(libyang::DataFormat::JSON, libyang::PrintFlags::WithSiblings) == R"({
+  "ietf-system:system-state": {
+    "@": {
+      "ietf-origin:origin": "ietf-origin:unknown"
+    },
+    "platform": {
+      "os-name": "GNU/Linux"
+    }
+  }
+}
+)"s);
+
+        velia::utils::valuesToYang({{"/ietf-interfaces:interfaces/interface[name='eth0']/name", "eth0"}}, {}, {}, srSess, edit);
+
+        REQUIRE(edit);
+        REQUIRE(*edit->printStr(libyang::DataFormat::JSON, libyang::PrintFlags::WithSiblings) == R"({
+  "ietf-interfaces:interfaces": {
+    "interface": [
+      {
+        "name": "eth0"
+      }
+    ]
+  },
+  "ietf-system:system-state": {
+    "@": {
+      "ietf-origin:origin": "ietf-origin:unknown"
+    },
+    "platform": {
+      "os-name": "GNU/Linux"
+    }
+  }
+}
+)"s);
+    }
+}
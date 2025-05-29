7c094f1e27d5e48fe806279266c2d4a162cb2a53
Jan Kundrát <jan.kundrat@cesnet.cz>
2025-05-29 18:24:12 +0200

    refactor: reusable network initialization
    ...so that we can test what we use on real devices.
    
    Change-Id: Ibe2687947b76a5abf9e7389e6621edc30621f49c

---
+++ src/network/Factory.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <filesystem>
+#include <sysrepo-cpp/Session.hpp>
+#include "network/IETFInterfaces.h"
+#include "network/IETFInterfacesConfig.h"
+#include "network/LLDP.h"
+#include "network/LLDPSysrepo.h"
+
+namespace velia::network {
+struct Services {
+    IETFInterfaces opsData;
+    IETFInterfacesConfig startupConfig, runtimeConfig;
+    LLDPSysrepo lldp;
+};
+
+Services create(
+    sysrepo::Session startupSess,
+    const std::filesystem::path& persistentNetworkDirectory,
+    sysrepo::Session runningSess,
+    const std::filesystem::path& runtimeNetworkDirectory,
+    const std::vector<std::string>& managedLinks,
+    IETFInterfacesConfig::reload_cb_t runningNetworkReloadCB,
+    LLDPDataProvider::data_callback_t lldpCallback,
+    LLDPDataProvider::LocalData lldpLocalData)
+{
+    std::filesystem::create_directories(runtimeNetworkDirectory);
+    std::filesystem::create_directories(persistentNetworkDirectory);
+    return {
+        .opsData = velia::network::IETFInterfaces{runningSess},
+        .startupConfig = IETFInterfacesConfig{startupSess, persistentNetworkDirectory, managedLinks, [](const auto&) {}},
+        .runtimeConfig = IETFInterfacesConfig{runningSess, runtimeNetworkDirectory, managedLinks, std::move(runningNetworkReloadCB)},
+        .lldp = LLDPSysrepo{runningSess,
+                            std::make_shared<velia::network::LLDPDataProvider>(std::move(lldpCallback), std::move(lldpLocalData))},
+    };
+}
+}

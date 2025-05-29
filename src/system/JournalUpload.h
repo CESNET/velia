CESNET/velia

commit e566fd422f0e05e026036240189b89dd5266968e

system: allow to configure remote journal upload
We add a new capability that allows user to configure
systemd-journal-upload service.

When the /czechlight-system:journal-upload is present then its
child leafs form a url that is used as a destination for journal
upload.

Velia writes into two environment files containing the DESTINATION=<url>
variable where <url> is the value for --url option of
systemd-journal-upload [1]. One file for both running and startup
datastores.
The systemd-journal-upload unit file should read both of these
environment files (first the startup, second the running) and also it
should not start unless at least one of these files exists.

We need both of these files. systemd-journal-upload can start quite
early during the system startup phase, definitely before velia or any
sysrepo startup->running copying. In case velia fails to start for some
reason we want these logs to be uploaded as well to know what prevented
velia from start. Therefore we can't wait for velia to setup the env
file and start the systemd-journal-upload service.

[1] https://www.freedesktop.org/software/systemd/man/252/systemd-journal-upload.service.html

Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/6766
Change-Id: I0f77fc39af337fb4b350758e5c59117e3ea78a85

src/system/JournalUpload.h
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#pragma once
+
+#include <sysrepo-cpp/Session.hpp>
+#include "LLDP.h"
+#include "utils/log-fwd.h"
+
+namespace velia::system {
+
+class JournalUpload {
+public:
+    using RestartCb = std::function<void(velia::Log)>;
+
+    JournalUpload(sysrepo::Session session, const std::filesystem::path& envFile, const RestartCb& restartCb);
+
+private:
+    velia::Log m_log;
+    sysrepo::Subscription m_srSub;
+};
+
+}

CMakeLists.txt
@@ -231,6 +231,8 @@ add_library(velia-system STATIC
         src/system/IETFInterfaces.h
         src/system/IETFInterfacesConfig.cpp
         src/system/IETFInterfacesConfig.h
+        src/system/JournalUpload.cpp
+        src/system/JournalUpload.h
         src/system/LED.cpp
         src/system/LED.h
         src/system/LLDP.cpp
@@ -442,6 +444,7 @@ if(BUILD_TESTING)
     velia_test(NAME sysrepo_system-firmware LIBRARIES velia-system DbusTesting SysrepoTesting FIXTURE fixture_sysrepo-czechlight-system RESOURCE_LOCK dbus-rauc)
     velia_test(NAME sysrepo_authentication LIBRARIES velia-system FsTestUtils FIXTURE fixture_sysrepo-czechlight-system)
     velia_test(NAME sysrepo_system-leds LIBRARIES velia-system FsTestUtils FIXTURE fixture_sysrepo-czechlight-system)
+    velia_test(NAME sysrepo_system-journalupload LIBRARIES velia-system FIXTURE fixture_sysrepo-czechlight-system)
 
     set(fixture_sysrepo-czechlight-network
             YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/iana-if-type@2017-01-19.yang

src/main-system.cpp
@@ -1,4 +1,5 @@
 #include <docopt.h>
+#include <sdbus-c++/IProxy.h>
 #include <spdlog/sinks/ansicolor_sink.h>
 #include <spdlog/spdlog.h>
 #include <sysrepo-cpp/Session.hpp>
@@ -10,6 +11,7 @@
 #include "system/IETFInterfaces.h"
 #include "system/IETFInterfacesConfig.h"
 #include "system/IETFSystem.h"
+#include "system/JournalUpload.h"
 #include "system/LED.h"
 #include "system/LLDP.h"
 #include "system/LLDPCallback.h"
@@ -18,6 +20,8 @@
 #include "utils/journal.h"
 #include "utils/log-init.h"
 #include "utils/sysrepo.h"
+
+using namespace std::literals;
 
 static const char usage[] =
     R"(Sysrepo-powered system management.
@@ -66,6 +70,13 @@ int main(int argc, char* argv[])
         auto srSess = srConn.sessionStart();
 
         DBUS_EVENTLOOP_START
+
+        auto journalUploadStartup = velia::system::JournalUpload(srConn.sessionStart(sysrepo::Datastore::Startup), "/cfg/journald-remote", [](auto) {});
+        auto journalUploadRunning = velia::system::JournalUpload(srConn.sessionStart(sysrepo::Datastore::Running), "/run/journald-remote", [dbusConn = g_dbusConnection.get()](auto log) {
+            log->debug("Restarting systemd-journal-upload.service");
+            auto sdManager = sdbus::createProxy(*dbusConn, "org.freedesktop.systemd1", "/org/freedesktop/systemd1");
+            sdManager->callMethod("RestartUnit").onInterface("org.freedesktop.systemd1.Manager").withArguments("systemd-journal-upload.service"s, "replace"s);
+        });
 
         // initialize ietf-system
         auto sysrepoIETFSystem = velia::system::IETFSystem(srSess, "/etc/os-release", *g_dbusConnection, "org.freedesktop.resolve1");

src/system/JournalUpload.cpp
@@ -1,0 +1,112 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#include <filesystem>
+#include <regex>
+#include <sysrepo-cpp/Enum.hpp>
+#include "JournalUpload.h"
+#include "utils/io.h"
+#include "utils/log.h"
+
+using namespace std::string_literals;
+
+static const auto UPLOAD_URL_CONTAINER = "/czechlight-system:journal-upload";
+
+namespace {
+
+std::optional<std::string> extractURL(sysrepo::Session session)
+{
+    auto data = session.getData(UPLOAD_URL_CONTAINER);
+    if (!data) {
+        return std::nullopt;
+    }
+
+    std::string url;
+
+    auto hostNode = data->findPath(UPLOAD_URL_CONTAINER + "/host"s);
+    auto hostValue = std::string{hostNode->asTerm().valueStr()};
+    auto isIpv6 = hostNode->asTerm().valueType().internalPluginId().find("ipv6") != std::string::npos;
+
+    url += data->findPath(UPLOAD_URL_CONTAINER + "/protocol"s)->asTerm().valueStr();
+    url += "://";
+
+    if (isIpv6) {
+        url += "[" + hostValue + "]";
+    } else {
+        url += hostValue;
+    }
+
+    url += ":";
+    url += std::string(data->findPath(UPLOAD_URL_CONTAINER + "/port"s)->asTerm().valueStr());
+
+    return url;
+}
+
+sysrepo::ErrorCode configureJournalUpload(velia::Log log, const std::optional<std::string>& url, const std::filesystem::path& envFile, const velia::system::JournalUpload::RestartCb& restartCb)
+{
+    std::optional<std::string> oldContent;
+    std::optional<std::string> newContent;
+
+    try {
+        oldContent = velia::utils::readFileToString(envFile);
+    } catch (const std::invalid_argument&) {
+        // if the file does not exist, oldContent will still be nullopt
+    }
+
+    if (url) {
+        newContent = "DESTINATION=" + *url + '\n';
+    }
+
+    if (oldContent != newContent) {
+        if (newContent) {
+            std::ofstream ofs(envFile);
+            ofs << *newContent;
+
+            newContent->pop_back(); // remove the \n
+            log->trace("systemd-journal-upload.service environment file {} set to {}", std::string(envFile), *newContent);
+        } else {
+            std::filesystem::remove(envFile);
+            log->trace("systemd-journal-upload.service environment file {} removed", std::string(envFile));
+        }
+
+        restartCb(log);
+    }
+
+    return sysrepo::ErrorCode::Ok;
+}
+
+sysrepo::Subscription journalUploadSubscription(velia::Log log, sysrepo::Session session, const std::filesystem::path& envFile, const velia::system::JournalUpload::RestartCb& restartCb)
+{
+    auto sub = session.onModuleChange(
+        "czechlight-system",
+        [log, envFile, restartCb](auto session, auto, auto, auto, auto, auto) {
+            return configureJournalUpload(log, extractURL(session), envFile, restartCb);
+        },
+        std::nullopt,
+        0,
+        sysrepo::SubscribeOptions::DoneOnly | sysrepo::SubscribeOptions::Enabled);
+
+    /*
+     * In case someone removes the presence container between sysrepo loads the data and this module startup we won't get a change (Deleted) from sysrepo and the file won't be written.
+     * Therefore, first register the callback and than call the configure manually.
+     * The configure function does not restart the service unless the configuration file content changes so this should not trigger the unit restart.
+     */
+    configureJournalUpload(log, extractURL(session), envFile, restartCb);
+
+    return sub;
+}
+}
+
+namespace velia::system {
+
+JournalUpload::JournalUpload(sysrepo::Session session, const std::filesystem::path& envFile, const RestartCb& restartCb)
+    : m_log(spdlog::get("system"))
+    , m_srSub(journalUploadSubscription(m_log, session, envFile, restartCb))
+{
+}
+
+}

tests/sysrepo_system-journalupload.cpp
@@ -1,0 +1,186 @@
+#include "trompeloeil_doctest.h"
+#include <filesystem>
+#include <sysrepo-cpp/utils/exception.hpp>
+#include "system/JournalUpload.h"
+#include "test_log_setup.h"
+#include "test_sysrepo_helpers.h"
+#include "tests/configure.cmake.h"
+#include "utils/io.h"
+
+#define EXPECT_RESTART_UNIT expectations.emplace_back(NAMED_REQUIRE_CALL(restartMock, restartCalled()).IN_SEQUENCE(seq1));
+
+using namespace std::literals;
+
+struct RestartMock {
+    MAKE_CONST_MOCK0(restartCalled, void());
+};
+
+TEST_CASE("Journal upload settings")
+{
+    trompeloeil::sequence seq1;
+    std::vector<std::unique_ptr<trompeloeil::expectation>> expectations;
+
+    RestartMock restartMock;
+
+    TEST_SYSREPO_INIT_LOGS;
+    TEST_SYSREPO_INIT;
+    TEST_SYSREPO_INIT_CLIENT;
+    client.sendRPC(client.getContext().newPath("/ietf-factory-default:factory-reset"));
+
+    auto fakeEnvFile = std::filesystem::path{CMAKE_CURRENT_BINARY_DIR} / "tests/journal-upload/env";
+    std::filesystem::remove(fakeEnvFile);
+    std::filesystem::create_directory(fakeEnvFile.parent_path());
+
+    std::unique_ptr<velia::system::JournalUpload> syslog;
+
+    auto restartCb = [&restartMock](auto) {
+        restartMock.restartCalled();
+    };
+
+    std::string expectedContent;
+
+    SECTION("Initialization")
+    {
+        SECTION("Presence container")
+        {
+            client.setItem("/czechlight-system:journal-upload/host", "upload.example.com");
+            client.applyChanges();
+
+            SECTION("Env file exists")
+            {
+                std::ofstream ofs(fakeEnvFile);
+                ofs << "DESTINATION=192.0.2.254\n";
+            }
+
+            SECTION("No env file")
+            {
+            }
+
+            expectedContent = "DESTINATION=https://upload.example.com:19532\n";
+            EXPECT_RESTART_UNIT;
+
+            syslog = std::make_unique<velia::system::JournalUpload>(srSess, fakeEnvFile, restartCb);
+            waitForCompletionAndBitMore(seq1);
+            REQUIRE(std::filesystem::exists(fakeEnvFile));
+            REQUIRE(velia::utils::readFileToString(fakeEnvFile) == expectedContent);
+        }
+
+        SECTION("No presence container")
+        {
+            SECTION("Env file exists")
+            {
+                std::ofstream ofs(fakeEnvFile);
+                ofs << "DESTINATION=192.0.2.254\n";
+                EXPECT_RESTART_UNIT;
+            }
+
+            SECTION("No env file")
+            {
+                // no restart expected, service does not start if the file is not there
+            }
+
+            syslog = std::make_unique<velia::system::JournalUpload>(srSess, fakeEnvFile, restartCb);
+            waitForCompletionAndBitMore(seq1);
+            REQUIRE(!std::filesystem::exists(fakeEnvFile));
+        }
+    }
+
+    SECTION("Responding to changes")
+    {
+        syslog = std::make_unique<velia::system::JournalUpload>(srSess, fakeEnvFile, restartCb);
+
+        SECTION("IPv6")
+        {
+            SECTION("With zone")
+            {
+                client.setItem("/czechlight-system:journal-upload/host", "::1%lo");
+                expectedContent = "DESTINATION=https://[::1%lo]:19532\n";
+            }
+
+            SECTION("Longer address")
+            {
+                client.setItem("/czechlight-system:journal-upload/host", "2001:0db8:0001::0ab9:C0A8:0102"); // it seems that libyang normalizes the address
+                expectedContent = "DESTINATION=https://[2001:db8:1::ab9:c0a8:102]:19532\n";
+            }
+
+            EXPECT_RESTART_UNIT;
+            client.applyChanges();
+        }
+
+        SECTION("Setting all leafs")
+        {
+            client.setItem("/czechlight-system:journal-upload/protocol", "http");
+            client.setItem("/czechlight-system:journal-upload/port", "1234");
+            client.setItem("/czechlight-system:journal-upload/host", "192.0.2.111");
+
+            EXPECT_RESTART_UNIT;
+            client.applyChanges();
+
+            expectedContent = "DESTINATION=http://192.0.2.111:1234\n";
+        }
+
+        SECTION("Changing one leaf triggers restart")
+        {
+            client.setItem("/czechlight-system:journal-upload/host", "192.0.2.2");
+            client.setItem("/czechlight-system:journal-upload/protocol", "http");
+            client.setItem("/czechlight-system:journal-upload/port", "1234");
+
+            EXPECT_RESTART_UNIT;
+            client.applyChanges();
+
+            client.setItem("/czechlight-system:journal-upload/protocol", "https");
+
+            EXPECT_RESTART_UNIT;
+            client.applyChanges();
+
+            expectedContent = "DESTINATION=https://192.0.2.2:1234\n";
+        }
+
+        waitForCompletionAndBitMore(seq1);
+        REQUIRE(std::filesystem::exists(fakeEnvFile));
+        REQUIRE(velia::utils::readFileToString(fakeEnvFile) == expectedContent);
+    }
+
+    SECTION("Disabling service")
+    {
+        client.setItem("/czechlight-system:journal-upload/host", "127.0.0.1");
+        client.applyChanges();
+
+        EXPECT_RESTART_UNIT;
+        syslog = std::make_unique<velia::system::JournalUpload>(srSess, fakeEnvFile, restartCb);
+
+        EXPECT_RESTART_UNIT;
+        client.deleteItem("/czechlight-system:journal-upload");
+        client.applyChanges();
+
+        waitForCompletionAndBitMore(seq1);
+        REQUIRE(!std::filesystem::exists(fakeEnvFile));
+    }
+
+    SECTION("YANG model")
+    {
+        SECTION("Host leaf is mandatory")
+        {
+            client.setItem("/czechlight-system:journal-upload", std::nullopt);
+            REQUIRE_THROWS_AS(client.applyChanges(), sysrepo::ErrorWithCode);
+        }
+
+        SECTION("Invalid protocol value")
+        {
+            REQUIRE_THROWS_AS(client.setItem("/czechlight-system:journal-upload/protocol", "imap"), sysrepo::ErrorWithCode);
+        }
+
+        SECTION("Invalid host")
+        {
+            REQUIRE_THROWS_AS(client.setItem("/czechlight-system:journal-upload/host", std::string(5000, 'a')), sysrepo::ErrorWithCode);
+        }
+
+        SECTION("WS noise")
+        {
+            REQUIRE_THROWS_AS(client.setItem("/czechlight-system:journal-upload/host", "ahoj.net\nVAR=val"), sysrepo::ErrorWithCode);
+            REQUIRE_THROWS_AS(client.setItem("/czechlight-system:journal-upload/host", "\n"), sysrepo::ErrorWithCode);
+            REQUIRE_THROWS_AS(client.setItem("/czechlight-system:journal-upload/host", " "), sysrepo::ErrorWithCode);
+            REQUIRE_THROWS_AS(client.setItem("/czechlight-system:journal-upload/host", ""), sysrepo::ErrorWithCode);
+        }
+    }
+}

yang/czechlight-system@2022-07-08.yang
@@ -5,6 +5,10 @@ module czechlight-system {
 
   prefix "cla-sys";
 
+  import ietf-inet-types {
+    prefix inet;
+  }
+
   import ietf-yang-types {
     prefix yang;
   }
@@ -262,6 +266,36 @@ module czechlight-system {
           }
         }
       }
+    }
+  }
+
+  container journal-upload {
+    presence "Enable systemd-journal-upload service";
+    description "Configures systemd-journal-upload.service. If the container is present,
+                 then systemd-journal-upload service is set up with the URL composed of
+                 the leafs in this container. See --url in
+                 https://www.freedesktop.org/software/systemd/man/252/systemd-journal-upload.service.html";
+
+    leaf protocol {
+      type enumeration {
+        enum "http";
+        enum "https";
+      }
+
+      default "https";
+      description "Sets the URL protocol. See --url in https://www.freedesktop.org/software/systemd/man/252/systemd-journal-upload.service.html";
+    }
+
+    leaf host {
+      mandatory true;
+      type inet:host;
+      description "Sets the host. See --url in https://www.freedesktop.org/software/systemd/man/252/systemd-journal-upload.service.html";
+    }
+
+    leaf port {
+      type inet:port-number;
+      default "19532";
+      description "Sets the port. See --url in https://www.freedesktop.org/software/systemd/man/252/systemd-journal-upload.service.html";
     }
   }
 

commit dc69b12befb827f102bfa64b33ab42de0cb9fb10

cleanup: remove unused header
Fixes: e566fd42 ("allow to configure remote journal upload")
Change-Id: Iefef74c17da3ac3ffe86e6583791b08cafbee576

src/system/JournalUpload.h
@@ -8,7 +8,6 @@
 #pragma once
 
 #include <sysrepo-cpp/Subscription.hpp>
-#include "LLDP.h"
 #include "utils/log-fwd.h"
 
 namespace velia::system {
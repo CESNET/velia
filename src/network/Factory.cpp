f12cd22e1e2bde2e8e65c6692065ac8c79096347
Tomáš Pecka <tomas.pecka@cesnet.cz>
2025-06-03 12:26:02 +0200

    network: refactor split Factory.h into header and cpp file
    My linter keeps warning me about ODR violations.
    
    Change-Id: I7bc2ed910a0ee8d78e573f105bf207eb77f66318

---
+++ src/network/Factory.cpp
@@ -0,0 +1,25 @@
+#include "network/Factory.h"
+
+namespace velia::network {
+Services create(
+    sysrepo::Connection conn,
+    const std::filesystem::path& persistentNetworkDirectory,
+    const std::filesystem::path& runtimeNetworkDirectory,
+    const std::vector<std::string>& managedLinks,
+    IETFInterfacesConfig::reload_cb_t runningNetworkReloadCB,
+    LLDPDataProvider::data_callback_t lldpCallback,
+    LLDPDataProvider::LocalData lldpLocalData)
+{
+    std::filesystem::create_directories(runtimeNetworkDirectory);
+    std::filesystem::create_directories(persistentNetworkDirectory);
+    auto running = conn.sessionStart(sysrepo::Datastore::Running);
+    return {
+        // IETFInterfaces has a background thread which acceses the session at random times
+        .opsData = velia::network::IETFInterfaces{conn.sessionStart(sysrepo::Datastore::Operational)},
+        .startupConfig = IETFInterfacesConfig{conn.sessionStart(sysrepo::Datastore::Startup), persistentNetworkDirectory, managedLinks, [](const auto&) { }},
+        .runtimeConfig = IETFInterfacesConfig{running, runtimeNetworkDirectory, managedLinks, std::move(runningNetworkReloadCB)},
+        .lldp = LLDPSysrepo{running,
+                            std::make_shared<velia::network::LLDPDataProvider>(std::move(lldpCallback), std::move(lldpLocalData))},
+    };
+}
+}
--- src/network/Factory.h
+++ src/network/Factory.h
@@ -20,18 +20,5 @@ Services create(
     const std::vector<std::string>& managedLinks,
     IETFInterfacesConfig::reload_cb_t runningNetworkReloadCB,
     LLDPDataProvider::data_callback_t lldpCallback,
-    LLDPDataProvider::LocalData lldpLocalData)
-{
-    std::filesystem::create_directories(runtimeNetworkDirectory);
-    std::filesystem::create_directories(persistentNetworkDirectory);
-    auto running = conn.sessionStart(sysrepo::Datastore::Running);
-    return {
-        // IETFInterfaces has a background thread which acceses the session at random times
-        .opsData = velia::network::IETFInterfaces{conn.sessionStart(sysrepo::Datastore::Operational)},
-        .startupConfig = IETFInterfacesConfig{conn.sessionStart(sysrepo::Datastore::Startup), persistentNetworkDirectory, managedLinks, [](const auto&) {}},
-        .runtimeConfig = IETFInterfacesConfig{running, runtimeNetworkDirectory, managedLinks, std::move(runningNetworkReloadCB)},
-        .lldp = LLDPSysrepo{running,
-                            std::make_shared<velia::network::LLDPDataProvider>(std::move(lldpCallback), std::move(lldpLocalData))},
-    };
-}
+    LLDPDataProvider::LocalData lldpLocalData);
 }

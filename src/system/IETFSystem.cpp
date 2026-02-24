CESNET/velia

commit 79344c872ac62562063a4ac24e9421382a2a8b03

system: Fill DNS servers into ietf-system
Implement the "config false" part of DNS setting in ietf-system YANG
model [1].

The model expects a list of DNS resolvers configured in the system. We
obtain that list from systemd-resolved (via its D-Bus interface [2,3]).

Systemd-resolved returns also interface-specific DNS resolvers (e.g.
those obtained via some network autoconfiguration on a specific
interface). Please note that the implementation in this commit lists
these as well (so that they are visible to the NETCONF client). The YANG
model, however, does not allow us to differentiate among system-wide and
interface-specific resolvers.
Therefore, the list does not reflect the real situation in the target
device as the interface-specific resolvers have priority over those
configured as system-wide ([3, see Dns= clause]).

[1] https://datatracker.ietf.org/doc/html/rfc7317
[2] https://www.freedesktop.org/software/systemd/man/org.freedesktop.resolve1.html
[3] https://www.freedesktop.org/software/systemd/man/resolved.conf.html

Change-Id: I8e085223c49dad97e0ce7a20e48305c7eb0f5283

src/system/IETFSystem.cpp
@@ -5,6 +5,7 @@
  *
  */
 
+#include <arpa/inet.h>
 #include <boost/algorithm/string/predicate.hpp>
 #include <fstream>
 #include <optional>
@@ -23,6 +24,7 @@ namespace {
 const auto IETF_SYSTEM_MODULE_NAME = "ietf-system"s;
 const auto IETF_SYSTEM_STATE_MODULE_PREFIX = "/"s + IETF_SYSTEM_MODULE_NAME + ":system-state/"s;
 const auto IETF_SYSTEM_HOSTNAME_PATH = "/ietf-system:system/hostname";
+const auto IETF_SYSTEM_DNS_PATH = "/ietf-system:system/dns-resolver";
 const auto IETF_SYSTEM_STATE_CLOCK_PATH = "/ietf-system:system-state/clock";
 
 /** @brief Returns key=value pairs from (e.g. /etc/os-release) as a std::map */
@@ -71,6 +73,49 @@ std::optional<std::string> getHostnameFromChange(const std::shared_ptr<sysrepo::
     }
 
     return res;
+}
+
+/** @brief Returns list of IP addresses (coded as a string) that serve as the DNS servers.
+ *
+ * We query the addresses from systemd-resolved D-Bus interface (see https://www.freedesktop.org/software/systemd/man/org.freedesktop.resolve1.html#Properties
+ * and possibly also https://www.freedesktop.org/software/systemd/man/resolved.conf.html).
+ * We use the value of DnsEx property on the Manager object. In case that DnsEx is empty we fallback to FallbackDnsEx property.
+ *
+ * Note that the returns not only the system-wide setting, but also the DNS resolvers that are configured per-interface. We chose not to ignore them despite ietf-system
+ * YANG model inability to distinguish between system-wide and per-interface type. Hence the resolver is listed as a system-wide one.
+ */
+std::vector<std::string> getDNSResolvers(sdbus::IConnection& connection, const std::string& dbusName)
+{
+    static const auto DBUS_RESOLVE1_MANAGER_PATH = "/org/freedesktop/resolve1";
+    static const auto DBUS_RESOLVE1_MANAGER_INTERFACE = "org.freedesktop.resolve1.Manager";
+
+    auto proxy = sdbus::createProxy(connection, dbusName, DBUS_RESOLVE1_MANAGER_PATH);
+
+    for (const auto& propertyName : {"DNSEx", "FallbackDNSEx"}) {
+        sdbus::Variant store = proxy->getProperty(propertyName).onInterface(DBUS_RESOLVE1_MANAGER_INTERFACE);
+
+        // DBus type of the DNSEx and FallbackDNSEx properties is "a(iiayqs)" ~ Array of [ Struct of (Int32, Int32, Array of [Byte], Uint16, String) ]
+        // i.e., <ifindex (0 for system-wide), addrtype, address as a bytearray, port (0 for unspecified), server name>,
+        auto replyObjects = store.get<std::vector<sdbus::Struct<int32_t, int32_t, std::vector<uint8_t>, uint16_t, std::string>>>();
+
+        if (!replyObjects.empty() > 0) {
+            std::vector<std::string> res;
+
+            for (const auto& e : replyObjects) {
+                auto addrType = e.get<1>();
+                auto addrBytes = e.get<2>();
+
+                std::array<char, std::max(INET_ADDRSTRLEN, INET6_ADDRSTRLEN)> buf{};
+                inet_ntop(addrType, addrBytes.data(), buf.data(), buf.size());
+
+                res.emplace_back(buf.data());
+            }
+
+            return res;
+        }
+    }
+
+    return {};
 }
 }
 
@@ -178,12 +223,31 @@ void IETFSystem::initClock()
             SR_SUBSCR_OPER_MERGE);
 }
 
+/** @short DNS resolver callbacks */
+void IETFSystem::initDNS(sdbus::IConnection& connection, const std::string& dbusName) {
+    sysrepo::OperGetItemsCb dnsOper = [&connection, dbusName] (auto session, auto, auto, auto, auto, auto& parent) {
+        std::map<std::string, std::string> values;
+
+        /* RFC 7317 specifies that key leaf 'name' contains "An arbitrary name for the DNS server".
+           We use the IP address which is unique. If the server is returned multiple times (e.g. once as system-wide and once
+           for some specific ifindex, it doesn't matter that it is listed only once. */
+        for (const auto& e : getDNSResolvers(connection, dbusName)) {
+            values[IETF_SYSTEM_DNS_PATH + "/server[name='"s + e + "']/udp-and-tcp/address"] = e;
+        }
+
+        utils::valuesToYang(values, {}, session, parent);
+        return SR_ERR_OK;
+    };
+
+    m_srSubscribe->oper_get_items_subscribe(IETF_SYSTEM_MODULE_NAME.c_str(), dnsOper, IETF_SYSTEM_DNS_PATH);
+}
+
 /** This class handles multiple system properties and publishes them via the ietf-system model:
  * - OS-identification data from osRelease file
  * - Rebooting
  * - Hostname
  */
-IETFSystem::IETFSystem(std::shared_ptr<::sysrepo::Session> srSession, const std::filesystem::path& osRelease)
+IETFSystem::IETFSystem(std::shared_ptr<::sysrepo::Session> srSession, const std::filesystem::path& osRelease, sdbus::IConnection& connection, const std::string& dbusName)
     : m_srSession(std::move(srSession))
     , m_srSubscribe(std::make_shared<::sysrepo::Subscribe>(m_srSession))
     , m_log(spdlog::get("system"))
@@ -193,5 +257,6 @@ IETFSystem::IETFSystem(std::shared_ptr<::sysrepo::Session> srSession, const std:
     initHostname();
     initDummies();
     initClock();
+    initDNS(connection, dbusName);
 }
 }

CMakeLists.txt
@@ -320,6 +320,8 @@ if(BUILD_TESTING)
     add_library(DbusTesting STATIC
         tests/dbus-helpers/dbus_rauc_server.cpp
         tests/dbus-helpers/dbus_rauc_server.h
+        tests/dbus-helpers/dbus_resolve1_server.cpp
+        tests/dbus-helpers/dbus_resolve1_server.h
         tests/dbus-helpers/dbus_semaphore_server.cpp
         tests/dbus-helpers/dbus_semaphore_server.h
         tests/dbus-helpers/dbus_systemd_server.cpp

src/main-system.cpp
@@ -63,7 +63,7 @@ int main(int argc, char* argv[])
         DBUS_EVENTLOOP_START
 
         // initialize ietf-system
-        auto sysrepoIETFSystem = velia::system::IETFSystem(srSess, "/etc/os-release");
+        auto sysrepoIETFSystem = velia::system::IETFSystem(srSess, "/etc/os-release", *g_dbusConnection, "org.freedesktop.resolve1");
 
         auto dbusConnection = sdbus::createConnection(); // second connection for RAUC (for calling methods).
         dbusConnection->enterEventLoopAsync();

src/system/IETFSystem.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <filesystem>
+#include <sdbus-c++/sdbus-c++.h>
 #include <sysrepo-cpp/Session.hpp>
 #include "utils/log-fwd.h"
 
@@ -14,7 +15,7 @@ namespace velia::system {
 
 class IETFSystem {
 public:
-    IETFSystem(std::shared_ptr<::sysrepo::Session> srSession, const std::filesystem::path& osRelease);
+    IETFSystem(std::shared_ptr<::sysrepo::Session> srSession, const std::filesystem::path& osRelease, sdbus::IConnection& dbusConnection, const std::string& dbusName);
 
 private:
     void initStaticProperties(const std::filesystem::path& osRelease);
@@ -22,6 +23,7 @@ private:
     void initHostname();
     void initDummies();
     void initClock();
+    void initDNS(sdbus::IConnection& connection, const std::string& dbusName);
 
     std::shared_ptr<::sysrepo::Session> m_srSession;
     std::shared_ptr<::sysrepo::Subscribe> m_srSubscribe;

tests/dbus-helpers/dbus_resolve1_server.cpp
@@ -1,0 +1,30 @@
+#include <arpa/inet.h>
+#include <sdbus-c++/sdbus-c++.h>
+#include "dbus_resolve1_server.h"
+#include "utils/log-init.h"
+
+using namespace std::literals;
+
+namespace {
+static const std::string MANAGER_INTERFACE = "org.freedesktop.resolve1.Manager";
+static const std::string MANAGER_PATH = "/org/freedesktop/resolve1";
+}
+
+DbusResolve1Server::DbusResolve1Server(sdbus::IConnection& connection)
+    : m_manager(sdbus::createObject(connection, MANAGER_PATH))
+{
+    // create manager object
+    m_manager->registerProperty("DNSEx").onInterface(MANAGER_INTERFACE).withGetter([this]() { return m_DNSEx; });
+    m_manager->registerProperty("FallbackDNSEx").onInterface(MANAGER_INTERFACE).withGetter([this]() { return m_FallbackDNSEx; });
+    m_manager->finishRegistration();
+}
+
+void DbusResolve1Server::setDNSEx(std::vector<DNSServer> servers)
+{
+    m_DNSEx = std::move(servers);
+}
+
+void DbusResolve1Server::setFallbackDNSEx(std::vector<DNSServer> servers)
+{
+    m_FallbackDNSEx = std::move(servers);
+}

tests/dbus-helpers/dbus_resolve1_server.h
@@ -1,0 +1,24 @@
+#pragma once
+
+#include <memory>
+#include <mutex>
+#include <sdbus-c++/sdbus-c++.h>
+#include <string>
+
+/** @brief Mimics a subset of the systemd's resolve1 DBus server behaviour
+ *
+ * https://www.freedesktop.org/software/systemd/man/org.freedesktop.resolve1.html
+ * */
+class DbusResolve1Server {
+public:
+    using DNSServer = sdbus::Struct<int32_t, int32_t, std::vector<uint8_t>, uint16_t, std::string>;
+
+    explicit DbusResolve1Server(sdbus::IConnection& connection);
+    void setDNSEx(std::vector<DNSServer> servers);
+    void setFallbackDNSEx(std::vector<DNSServer> servers);
+
+
+private:
+    std::unique_ptr<sdbus::IObject> m_manager;
+    std::vector<DNSServer> m_DNSEx, m_FallbackDNSEx;
+};

tests/sysrepo_system-ietfsystem.cpp
@@ -1,4 +1,7 @@
 #include "trompeloeil_doctest.h"
+#include <arpa/inet.h>
+#include <sdbus-c++/sdbus-c++.h>
+#include "dbus-helpers/dbus_resolve1_server.h"
 #include "pretty_printers.h"
 #include "system/IETFSystem.h"
 #include "test_log_setup.h"
@@ -14,6 +17,14 @@ TEST_CASE("Sysrepo ietf-system")
     TEST_SYSREPO_INIT_LOGS;
     TEST_SYSREPO_INIT;
     TEST_SYSREPO_INIT_CLIENT;
+
+    auto dbusConnServer = sdbus::createSessionBusConnection();
+    auto dbusConnClient = sdbus::createSessionBusConnection();
+
+    dbusConnServer->enterEventLoopAsync();
+    dbusConnClient->enterEventLoopAsync();
+
+    DbusResolve1Server dbusServer(*dbusConnServer);
 
     SECTION("Test system-state")
     {
@@ -54,19 +65,19 @@ TEST_CASE("Sysrepo ietf-system")
                 };
             }
 
-            auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess, file);
+            auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess, file, *dbusConnClient, dbusConnServer->getUniqueName());
             REQUIRE(dataFromSysrepo(client, modulePrefix + "/platform", SR_DS_OPERATIONAL) == expected);
         }
 
         SECTION("Invalid data (missing VERSION and NAME keys)")
         {
-            REQUIRE_THROWS_AS(std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/missing-keys"), std::out_of_range);
+            REQUIRE_THROWS_AS(std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/missing-keys", *dbusConnClient, dbusConnServer->getUniqueName()), std::out_of_range);
         }
     }
 
     SECTION("dummy values")
     {
-        auto sys = std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release");
+        auto sys = std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release", *dbusConnClient, dbusConnServer->getUniqueName());
         const char* xpath;
 
         SECTION("location") {
@@ -88,15 +99,68 @@ TEST_CASE("Sysrepo ietf-system")
 
     SECTION("clock")
     {
-        auto sys = std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release");
+        auto sys = std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release", *dbusConnClient, dbusConnServer->getUniqueName());
         client->session_switch_ds(SR_DS_OPERATIONAL);
         REQUIRE(!!client->get_item("/ietf-system:system-state/clock/current-datetime"));
+    }
+
+    SECTION("DNS resolvers")
+    {
+        auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release", *dbusConnClient, dbusConnServer->getUniqueName());
+        std::map<std::string, std::string> expected;
+
+        dbusServer.setFallbackDNSEx({
+            {0, AF_INET, {8, 8, 8, 8}, 0, "prvni.googlovsky.dns"},
+            {0, AF_INET, {8, 8, 4, 4}, 0, "druhy.googlovsky.dns"},
+            {2, AF_INET6, {0x20, 0x01, 0x48, 0x60, 0x48, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x88, 0x88}, 0, "this.was.in.my.resolved"},
+        });
+
+        SECTION("Both DNS and Fallback DNS") {
+            dbusServer.setDNSEx({
+                {0, AF_INET, {127, 0, 0, 1}, 0, "ahoj.com"},
+                {2, AF_INET, {127, 0, 0, 1}, 0, "czech.light"},
+                {2, AF_INET6, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, 53, "idk.net"},
+            });
+
+            expected = {
+                {"/options", ""},
+                {"/server[name='127.0.0.1']", ""},
+                {"/server[name='127.0.0.1']/name", "127.0.0.1"},
+                {"/server[name='127.0.0.1']/udp-and-tcp", ""},
+                {"/server[name='127.0.0.1']/udp-and-tcp/address", "127.0.0.1"},
+                {"/server[name='::1']", ""},
+                {"/server[name='::1']/name", "::1"},
+                {"/server[name='::1']/udp-and-tcp", ""},
+                {"/server[name='::1']/udp-and-tcp/address", "::1"},
+            };
+        }
+
+        SECTION("FallbackDNS only")
+        {
+            expected = {
+                {"/options", ""},
+                {"/server[name='2001:4860:4860::8888']", ""},
+                {"/server[name='2001:4860:4860::8888']/name", "2001:4860:4860::8888"},
+                {"/server[name='2001:4860:4860::8888']/udp-and-tcp", ""},
+                {"/server[name='2001:4860:4860::8888']/udp-and-tcp/address", "2001:4860:4860::8888"},
+                {"/server[name='8.8.4.4']", ""},
+                {"/server[name='8.8.4.4']/name", "8.8.4.4"},
+                {"/server[name='8.8.4.4']/udp-and-tcp", ""},
+                {"/server[name='8.8.4.4']/udp-and-tcp/address", "8.8.4.4"},
+                {"/server[name='8.8.8.8']", ""},
+                {"/server[name='8.8.8.8']/name", "8.8.8.8"},
+                {"/server[name='8.8.8.8']/udp-and-tcp", ""},
+                {"/server[name='8.8.8.8']/udp-and-tcp/address", "8.8.8.8"},
+            };
+        }
+
+        REQUIRE(dataFromSysrepo(client, "/ietf-system:system/dns-resolver", SR_DS_OPERATIONAL) == expected);
     }
 
 #ifdef TEST_RPC_SYSTEM_REBOOT
     SECTION("RPC system-restart")
     {
-        auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release");
+        auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess, CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release", dbusConnection);
 
         auto rpcInput = std::make_shared<sysrepo::Vals>(0);
         auto res = client->rpc_send("/ietf-system:system-restart", rpcInput);

yang/czechlight-system@2021-01-13.yang
@@ -247,4 +247,5 @@ module czechlight-system {
   }
 
   deviation /sys:system-shutdown { deviate not-supported; }
+  deviation /sys:system/sys:dns-resolver { deviate add { config false; } }
 }

commit c24507ae61de7da7ba4a30abf6e52300b176e6dd

refactor: make a simple condition more readable
Wow, I wonder how we missed this during code review.

Fixes: 79344c872ac62562063a4ac24e9421382a2a8b03 ("system: Fill DNS servers into ietf-system")
Change-Id: I8a0c446ec9c2d10493d4cf7e87c6972c61b599a1

src/system/IETFSystem.cpp
@@ -96,7 +96,7 @@ std::vector<std::string> getDNSResolvers(sdbus::IConnection& connection, const s
         // i.e., <ifindex (0 for system-wide), addrtype, address as a bytearray, port (0 for unspecified), server name>,
         auto replyObjects = store.get<std::vector<sdbus::Struct<int32_t, int32_t, std::vector<uint8_t>, uint16_t, std::string>>>();
 
-        if (!replyObjects.empty() > 0) {
+        if (!replyObjects.empty()) {
             std::vector<std::string> res;
 
             for (const auto& e : replyObjects) {
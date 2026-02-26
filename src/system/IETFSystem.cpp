CESNET/velia

commit 3a6a9b82b5dc78c4d938012d6dde4e98b909be2d

system: read-only report of NTP configuration
This patch adds NTP setting data to the ietf-system. As a first step,
this patch adds a deviation that the data are config false.

(As a second step, patches that will allow configuring both NTP and DNS
by user will be added.)

Change-Id: I3f6f8ac14439e43e45739a96132c7fa0199464ab

src/system/IETFSystem.cpp
@@ -24,6 +24,7 @@ const auto IETF_SYSTEM_MODULE_NAME = "ietf-system"s;
 const auto IETF_SYSTEM_STATE_MODULE_PREFIX = "/"s + IETF_SYSTEM_MODULE_NAME + ":system-state/"s;
 const auto IETF_SYSTEM_HOSTNAME_PATH = "/ietf-system:system/hostname";
 const auto IETF_SYSTEM_DNS_PATH = "/ietf-system:system/dns-resolver";
+const auto IETF_SYSTEM_NTP_PATH = "/ietf-system:system/ntp";
 const auto IETF_SYSTEM_STATE_CLOCK_PATH = "/ietf-system:system-state/clock";
 
 /** @brief Returns key=value pairs from (e.g. /etc/os-release) as a std::map */
@@ -264,6 +265,44 @@ void IETFSystem::initDNS(sdbus::IConnection& connection, const std::string& dbus
     m_srSubscribe->onOperGet(IETF_SYSTEM_MODULE_NAME, dnsOper, IETF_SYSTEM_DNS_PATH);
 }
 
+/** @short NTP callbacks */
+void IETFSystem::initNTP(sdbus::IConnection& connection, const std::string& dbusName)
+{
+    sysrepo::OperGetCb operCb = [&connection, dbusName](auto session, auto, auto, auto, auto, auto, auto& parent) {
+        constexpr auto DBUS_TIMESYNC1_MANAGER_PATH = "/org/freedesktop/timesync1";
+        constexpr auto DBUS_TIMESYNC1_MANAGER_INTERFACE = "org.freedesktop.timesync1.Manager";
+        constexpr auto DBUS_TIMEDATE1_MANAGER_PATH = "/org/freedesktop/timedate1";
+        constexpr auto DBUS_TIMEDATE1_MANAGER_INTERFACE = "org.freedesktop.timedate1.Manager";
+
+        utils::YANGData values;
+
+        auto proxy = sdbus::createProxy(connection, dbusName, DBUS_TIMEDATE1_MANAGER_PATH);
+        auto ntpServiceAvailable = proxy->getProperty("CanNTP").onInterface(DBUS_TIMEDATE1_MANAGER_INTERFACE).get<bool>();
+        auto ntpServiceEnabled = proxy->getProperty("NTP").onInterface(DBUS_TIMEDATE1_MANAGER_INTERFACE).get<bool>();
+        values.emplace_back(IETF_SYSTEM_NTP_PATH + "/enabled"s, (ntpServiceAvailable && ntpServiceEnabled) ? "true" : "false");
+
+        proxy = sdbus::createProxy(connection, dbusName, DBUS_TIMESYNC1_MANAGER_PATH);
+        std::vector<std::string> ntpServers;
+        for (const auto& propertyName : {"NTPServers", "FallbackNTPServers"}) {
+            sdbus::Variant store = proxy->getProperty(propertyName).onInterface(DBUS_TIMESYNC1_MANAGER_INTERFACE);
+            ntpServers = store.get<std::vector<std::string>>();
+
+            if (!ntpServers.empty()) {
+                break;
+            }
+        }
+
+        for (const auto& server : ntpServers) {
+            values.emplace_back(IETF_SYSTEM_NTP_PATH + "/server[name='"s + server + "']/udp/address", server);
+        }
+
+        utils::valuesToYang(values, {}, {}, session, parent);
+        return sysrepo::ErrorCode::Ok;
+    };
+
+    m_srSubscribe->onOperGet(IETF_SYSTEM_MODULE_NAME, operCb, IETF_SYSTEM_NTP_PATH);
+}
+
 /** This class handles multiple system properties and publishes them via the ietf-system model:
  * - OS-identification data from osRelease file
  * - Rebooting
@@ -280,5 +319,6 @@ IETFSystem::IETFSystem(::sysrepo::Session srSession, const std::filesystem::path
     initDummies();
     initClock(procStat);
     initDNS(connection, dbusName);
+    initNTP(connection, dbusName);
 }
 }

CMakeLists.txt
@@ -495,7 +495,7 @@ if(BUILD_TESTING)
     set(fixture_sysrepo-czechlight-lldp --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/czechlight-lldp@2026-01-09.yang)
     velia_test(NAME sysrepo_network-lldp LIBRARIES velia-network DbusTesting FIXTURE fixture_sysrepo-czechlight-lldp)
 
-    set(fixture_sysrepo-ietf-system --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-system@2014-08-06.yang)
+    set(fixture_sysrepo-ietf-system --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-system@2014-08-06.yang --enable-feature ntp)
     velia_test(NAME sysrepo_system-ietfsystem LIBRARIES velia-system DbusTesting date::date-tz FIXTURE fixture_sysrepo-ietf-system)
 
     set(fixture_sysrepo-czechlight-system --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/czechlight-system@2022-07-08.yang --init-data ${CMAKE_CURRENT_SOURCE_DIR}/yang/czechlight-authentication.json)

src/system/IETFSystem.h
@@ -24,6 +24,7 @@ private:
     void initDummies();
     void initClock(const std::filesystem::path& procStatPath);
     void initDNS(sdbus::IConnection& connection, const std::string& dbusName);
+    void initNTP(sdbus::IConnection& connection, const std::string& dbusName);
 
     ::sysrepo::Session m_srSession;
     std::optional<::sysrepo::Subscription> m_srSubscribe;

tests/dbus-helpers/dbus_systemd_server.cpp
@@ -8,15 +8,21 @@ namespace {
 constexpr auto ifaceSystemd1Unit = "org.freedesktop.systemd1.Unit";
 constexpr auto ifaceSystemd1Manager = "org.freedesktop.systemd1.Manager";
 constexpr auto ifaceResolve1Manager = "org.freedesktop.resolve1.Manager";
+constexpr auto ifaceTimesync1Manager = "org.freedesktop.timesync1.Manager";
+constexpr auto ifaceTimedate1Manager = "org.freedesktop.timedate1.Manager";
 
 constexpr auto pathSystemd1 = "/org/freedesktop/systemd1";
 constexpr auto pathResolve1 = "/org/freedesktop/resolve1";
+constexpr auto pathTimesync1 = "/org/freedesktop/timesync1";
+constexpr auto pathTimedate1 = "/org/freedesktop/timedate1";
 }
 
 /** @brief Create a DBus server on the connection */
 DbusSystemdServer::DbusSystemdServer(sdbus::IConnection& connection)
     : m_systemd1Manager(sdbus::createObject(connection, pathSystemd1))
     , m_resolve1Manager(sdbus::createObject(connection, pathResolve1))
+    , m_timesync1Manager(sdbus::createObject(connection, pathTimesync1))
+    , m_timedate1Manager(sdbus::createObject(connection, pathTimedate1))
 {
     m_systemd1Manager->registerMethod("Subscribe").onInterface(ifaceSystemd1Manager).implementedAs([] {}).withNoReply(); // no-op for us
     m_systemd1Manager->registerMethod("ListUnits").onInterface(ifaceSystemd1Manager).implementedAs([this]() { return ListUnits(); });
@@ -26,6 +32,14 @@ DbusSystemdServer::DbusSystemdServer(sdbus::IConnection& connection)
     m_resolve1Manager->registerProperty("DNSEx").onInterface(ifaceResolve1Manager).withGetter([this]() { return m_DNSEx; });
     m_resolve1Manager->registerProperty("FallbackDNSEx").onInterface(ifaceResolve1Manager).withGetter([this]() { return m_FallbackDNSEx; });
     m_resolve1Manager->finishRegistration();
+
+    m_timesync1Manager->registerProperty("NTPServers").onInterface(ifaceTimesync1Manager).withGetter([this]() { return m_NTPServers; });
+    m_timesync1Manager->registerProperty("FallbackNTPServers").onInterface(ifaceTimesync1Manager).withGetter([this]() { return m_FallbackNTPServers; });
+    m_timesync1Manager->finishRegistration();
+
+    m_timedate1Manager->registerProperty("CanNTP").onInterface(ifaceTimedate1Manager).withGetter([this]() { return m_canNTP; });
+    m_timedate1Manager->registerProperty("NTP").onInterface(ifaceTimedate1Manager).withGetter([this]() { return m_NTP; });
+    m_timedate1Manager->finishRegistration();
 }
 
 /** @brief Creates a unit inside the test server. Registers dbus object and emits UnitNew signal. **/
@@ -90,4 +104,20 @@ void DbusSystemdServer::setDNSEx(std::vector<DNSServer> servers)
 void DbusSystemdServer::setFallbackDNSEx(std::vector<DNSServer> servers)
 {
     m_FallbackDNSEx = std::move(servers);
+}
+
+void DbusSystemdServer::setNTP(bool canNTP, bool NTP)
+{
+    m_canNTP = canNTP;
+    m_NTP = NTP;
+}
+
+void DbusSystemdServer::setNTPServers(std::vector<std::string> ntpServers)
+{
+    m_NTPServers = std::move(ntpServers);
+}
+
+void DbusSystemdServer::setFallbackNTPServers(std::vector<std::string> ntpServers)
+{
+    m_FallbackNTPServers = std::move(ntpServers);
 }

tests/dbus-helpers/dbus_systemd_server.h
@@ -19,6 +19,10 @@ public:
     void setDNSEx(std::vector<DNSServer> servers);
     void setFallbackDNSEx(std::vector<DNSServer> servers);
 
+    void setNTP(bool canNTP, bool NTP);
+    void setNTPServers(std::vector<std::string> ntpServers);
+    void setFallbackNTPServers(std::vector<std::string> ntpServers);
+
 private:
     struct Unit {
         Unit(std::string unitName, std::unique_ptr<sdbus::IObject> object, std::string activeState, std::string subState);
@@ -31,9 +35,14 @@ private:
 
     std::unique_ptr<sdbus::IObject> m_systemd1Manager;
     std::unique_ptr<sdbus::IObject> m_resolve1Manager;
+    std::unique_ptr<sdbus::IObject> m_timesync1Manager;
+    std::unique_ptr<sdbus::IObject> m_timedate1Manager;
 
     std::map<sdbus::ObjectPath, Unit> m_units;
     std::vector<DNSServer> m_DNSEx, m_FallbackDNSEx;
+
+    std::vector<std::string> m_NTPServers, m_FallbackNTPServers;
+    bool m_canNTP, m_NTP;
 
     std::vector<UnitStruct> ListUnits();
 };

tests/sysrepo_system-ietfsystem.cpp
@@ -219,6 +219,55 @@ TEST_CASE("Sysrepo ietf-system")
         REQUIRE(dataFromSysrepo(client, "/ietf-system:system/dns-resolver", sysrepo::Datastore::Operational) == expected);
     }
 
+    SECTION("NTP")
+    {
+        std::map<std::string, std::string> expected;
+        auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess,
+                                                                   CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",
+                                                                   CMAKE_CURRENT_SOURCE_DIR "/tests/system/proc_stat.ok",
+                                                                   *dbusConnClient,
+                                                                   dbusConnServer->getUniqueName());
+
+        SECTION("NTP enabled and both servers and fallback servers exist")
+        {
+            dbusServer.setNTP(true, true);
+            dbusServer.setNTPServers({"tik.cesnet.cz", "tak.cesnet.cz"});
+            dbusServer.setFallbackNTPServers({"0.arch.pool.ntp.org", "1.arch.pool.ntp.org"});
+
+            expected = {
+                {"/enabled", "true"},
+                {"/server[name='tik.cesnet.cz']", ""},
+                {"/server[name='tik.cesnet.cz']/name", "tik.cesnet.cz"},
+                {"/server[name='tik.cesnet.cz']/udp", ""},
+                {"/server[name='tik.cesnet.cz']/udp/address", "tik.cesnet.cz"},
+                {"/server[name='tak.cesnet.cz']", ""},
+                {"/server[name='tak.cesnet.cz']/name", "tak.cesnet.cz"},
+                {"/server[name='tak.cesnet.cz']/udp", ""},
+                {"/server[name='tak.cesnet.cz']/udp/address", "tak.cesnet.cz"},
+            };
+        }
+
+        SECTION("NTP disabled and only fallback servers set")
+        {
+            dbusServer.setNTP(true, false);
+            dbusServer.setFallbackNTPServers({"0.arch.pool.ntp.org", "1.arch.pool.ntp.org"});
+
+            expected = {
+                {"/enabled", "false"},
+                {"/server[name='0.arch.pool.ntp.org']", ""},
+                {"/server[name='0.arch.pool.ntp.org']/name", "0.arch.pool.ntp.org"},
+                {"/server[name='0.arch.pool.ntp.org']/udp", ""},
+                {"/server[name='0.arch.pool.ntp.org']/udp/address", "0.arch.pool.ntp.org"},
+                {"/server[name='1.arch.pool.ntp.org']", ""},
+                {"/server[name='1.arch.pool.ntp.org']/name", "1.arch.pool.ntp.org"},
+                {"/server[name='1.arch.pool.ntp.org']/udp", ""},
+                {"/server[name='1.arch.pool.ntp.org']/udp/address", "1.arch.pool.ntp.org"},
+            };
+        }
+
+        REQUIRE(dataFromSysrepo(client, "/ietf-system:system/ntp", sysrepo::Datastore::Operational) == expected);
+    }
+
 #ifdef TEST_RPC_SYSTEM_REBOOT
     SECTION("RPC system-restart")
     {

yang/czechlight-system@2022-07-08.yang
@@ -314,6 +314,12 @@ module czechlight-system {
     }
   }
 
+  deviation /sys:system/sys:ntp {
+    deviate add {
+      config false;
+    }
+  }
+
   deviation /hw:hardware/hw:component/hw:parent {
     deviate add {
       config false;

commit a2337d37ca75dd42e79faa5cd8035a889fcf5c55

ietf-system: the `ntp` feature is now mandatory
Fixes: 3a6a9b82 system: read-only report of NTP configuration
Change-Id: Id4d8b40b94679f7b9be7cc62624514c2a28fe716

src/system/IETFSystem.cpp
@@ -268,6 +268,7 @@ void IETFSystem::initDNS(sdbus::IConnection& connection, const std::string& dbus
 /** @short NTP callbacks */
 void IETFSystem::initNTP(sdbus::IConnection& connection, const std::string& dbusName)
 {
+    utils::ensureModuleImplemented(m_srSession, IETF_SYSTEM_MODULE_NAME, "2014-08-06", {{"ntp"}});
     sysrepo::OperGetCb operCb = [&connection, dbusName](auto session, auto, auto, auto, auto, auto, auto& parent) {
         constexpr auto DBUS_TIMESYNC1_MANAGER_PATH = "/org/freedesktop/timesync1";
         constexpr auto DBUS_TIMESYNC1_MANAGER_INTERFACE = "org.freedesktop.timesync1.Manager";

src/utils/sysrepo.cpp
@@ -136,10 +136,16 @@ void valuesPush(sysrepo::Session session, const YANGData& values, const std::vec
 }
 
 /** @brief Checks whether a module is implemented in Sysrepo and throws if not. */
-void ensureModuleImplemented(::sysrepo::Session session, const std::string& module, const std::string& revision)
+void ensureModuleImplemented(::sysrepo::Session session, const std::string& module, const std::string& revision, const std::vector<std::string>& features)
 {
-    if (auto mod = session.getContext().getModule(module, revision); !mod || !mod->implemented()) {
+    auto mod = session.getContext().getModule(module, revision);
+    if (!mod || !mod->implemented()) {
         throw std::runtime_error(module + "@" + revision + " is not implemented in sysrepo.");
+    }
+    for (const auto& feature : features) {
+        if (!mod->featureEnabled(feature)) {
+            throw std::runtime_error{"Feature '" + feature + "' is not enabled in module " + module};
+        }
     }
 }
 YANGPair::YANGPair(std::string xpath, std::string value)

src/utils/sysrepo.h
@@ -26,7 +26,7 @@ void valuesToYang(const YANGData& values, const std::vector<std::string>& foreig
 void valuesPush(::sysrepo::Session session, const YANGData& values, const std::vector<std::string>& foreignRemovals, const std::vector<std::string>& ourRemovals);
 
 void initLogsSysrepo();
-void ensureModuleImplemented(::sysrepo::Session session, const std::string& module, const std::string& revision);
+void ensureModuleImplemented(::sysrepo::Session session, const std::string& module, const std::string& revision, const std::vector<std::string>& features = {});
 
 void setErrors(::sysrepo::Session session, const std::string& msg);
 
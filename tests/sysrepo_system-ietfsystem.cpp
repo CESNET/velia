86e0bb2df67222ecac6e1401e08048168910638a
Tomáš Pecka <tomas.pecka@cesnet.cz>
2026-03-06 13:56:42 +0100

    system: allow setting NTP servers
    This patch adds support for setting NTP servers using ietf-system model.
    Velia expects systemd-timesyncd to run manage the NTP client in the
    system because it only takes the NTP server config from sysrepo and
    produces a corresponding systemd-timesyncd configuration [1].
    
    We have to write the NTP settings to the drop-in conf file
    /run/systemd/timesyncd.conf.d/timesyncd.conf because writing to
    /run/systemd/timesyncd.conf has no effect as it is a lower priority
    config file than /etc/systemd/timesyncd.conf [1].
    
    [1] https://www.freedesktop.org/software/systemd/man/latest/timesyncd.conf.html
    
    Change-Id: I8d09cf2f8e8c6bae2578e84af775c65a7cde23fc

--- tests/sysrepo_system-ietfsystem.cpp
+++ tests/sysrepo_system-ietfsystem.cpp
@@ -8,8 +8,13 @@
 #include "test_log_setup.h"
 #include "tests/configure.cmake.h"
 #include "tests/sysrepo-helpers/common.h"
+#include "utils/io.h"
 
 using namespace std::literals;
+
+struct SystemctlMock {
+    MAKE_CONST_MOCK0(reloadTimesync, void());
+};
 
 TEST_CASE("Sysrepo ietf-system")
 {
@@ -26,9 +31,23 @@ TEST_CASE("Sysrepo ietf-system")
     dbusConnClient->enterEventLoopAsync();
 
     DbusSystemdServer dbusServer(*dbusConnServer);
-    velia::system::IETFSystem::SystemdConfigData resolve{.busName = dbusConnServer->getUniqueName()};
-    velia::system::IETFSystem::SystemdConfigData timesync{.busName = dbusConnServer->getUniqueName()};
-    velia::system::IETFSystem::SystemdConfigData timedate{.busName = dbusConnServer->getUniqueName()};
+    SystemctlMock systemctlMock;
+
+    velia::system::IETFSystem::SystemdConfigData resolve{
+        .busName = dbusConnServer->getUniqueName(),
+        .dropinDir = std::nullopt,
+        .reload = std::nullopt,
+    };
+    velia::system::IETFSystem::SystemdConfigData timesync{
+        .busName = dbusConnServer->getUniqueName(),
+        .dropinDir = CMAKE_CURRENT_BINARY_DIR "tests/timesyncd.conf.d",
+        .reload = [&systemctlMock] { systemctlMock.reloadTimesync(); },
+    };
+    velia::system::IETFSystem::SystemdConfigData timedate{
+        .busName = dbusConnServer->getUniqueName(),
+        .dropinDir = std::nullopt,
+        .reload = std::nullopt,
+    };
 
     SECTION("Test system-state")
     {
@@ -70,6 +89,7 @@ TEST_CASE("Sysrepo ietf-system")
                 };
             }
 
+            REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
             auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                        osReleaseFile,
                                                                        procStatFile,
@@ -97,6 +117,7 @@ TEST_CASE("Sysrepo ietf-system")
 
     SECTION("dummy values")
     {
+        REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
         auto sys = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",
                                                                CMAKE_CURRENT_SOURCE_DIR "/tests/system/proc_stat.ok",
@@ -128,6 +149,7 @@ TEST_CASE("Sysrepo ietf-system")
         // in its own scope, otherwise the tests below this will fail with something like:
         // Couldn't create RPC/action subscription: RPC subscription for "/ietf-system:system-restart" with priority 0 already exists.
         {
+            REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
             auto sys = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/proc_stat.ok",
@@ -181,6 +203,7 @@ TEST_CASE("Sysrepo ietf-system")
 
     SECTION("DNS resolvers")
     {
+        REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
         auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/proc_stat.ok",
@@ -241,6 +264,7 @@ TEST_CASE("Sysrepo ietf-system")
     SECTION("NTP")
     {
         std::map<std::string, std::string> expected;
+        REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
         auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/proc_stat.ok",
@@ -253,7 +277,7 @@ TEST_CASE("Sysrepo ietf-system")
         {
             dbusServer.setNTP(true);
             dbusServer.setSystemNTPServers({"tik.cesnet.cz", "tak.cesnet.cz"});
-            dbusServer.setLinkNTPServers({"10.0.0.1", "10.0.0.2", "10.0.0.3", "10.0.0.4", "10.0.0.5", "10.0.0.6", "10.0.0.7", "10.0.0.8"});
+            dbusServer.setLinkNTPServers({"2001:db8::1", "10.0.0.2", "10.0.0.3", "10.0.0.4", "10.0.0.5", "10.0.0.6", "10.0.0.7", "10.0.0.8"});
             dbusServer.setFallbackNTPServers({"0.arch.pool.ntp.org", "1.arch.pool.ntp.org"});
 
             expected = {
@@ -269,7 +293,7 @@ TEST_CASE("Sysrepo ietf-system")
                 {"/server[name='03']", ""},
                 {"/server[name='03']/name", "03"},
                 {"/server[name='03']/udp", ""},
-                {"/server[name='03']/udp/address", "10.0.0.1"},
+                {"/server[name='03']/udp/address", "2001:db8::1"},
                 {"/server[name='04']", ""},
                 {"/server[name='04']/name", "04"},
                 {"/server[name='04']/udp", ""},
@@ -328,6 +352,68 @@ TEST_CASE("Sysrepo ietf-system")
             expected = {
                 {"/enabled", "false"},
             };
+        }
+
+        SECTION("Generating NTP config")
+        {
+            dbusServer.setNTP(true);
+            dbusServer.setFallbackNTPServers({"0.arch.pool.ntp.org", "1.arch.pool.ntp.org"});
+
+            srSess.switchDatastore(sysrepo::Datastore::Running);
+
+            SECTION("No servers")
+            {
+                dbusServer.setSystemNTPServers({});
+
+                srSess.applyChanges();
+                REQUIRE(velia::utils::readFileToString(*timesync.dropinDir / "timesyncd.conf") == R"(# Autogenerated by velia. Do not edit.
+[Time]
+NTP=
+)");
+                expected = {
+                    {"/enabled", "true"},
+                    {"/server[name='1']", ""},
+                    {"/server[name='1']/name", "1"},
+                    {"/server[name='1']/udp", ""},
+                    {"/server[name='1']/udp/address", "0.arch.pool.ntp.org"},
+                    {"/server[name='2']", ""},
+                    {"/server[name='2']/name", "2"},
+                    {"/server[name='2']/udp", ""},
+                    {"/server[name='2']/udp/address", "1.arch.pool.ntp.org"},
+                };
+            }
+
+            SECTION("Some servers")
+            {
+                srSess.setItem("/ietf-system:system/ntp/server[name='1']/udp/address", "tik.cesnet.cz");
+                srSess.setItem("/ietf-system:system/ntp/server[name='2']/udp/address", "195.113.144.201");
+                srSess.setItem("/ietf-system:system/ntp/server[name='3']/udp/address", "2001:718:1:1::144:201");
+                REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
+                srSess.applyChanges();
+                REQUIRE(velia::utils::readFileToString(*timesync.dropinDir / "timesyncd.conf") == R"(# Autogenerated by velia. Do not edit.
+[Time]
+NTP=tik.cesnet.cz 195.113.144.201 2001:718:1:1::144:201
+)");
+
+                // dbus will return these data as system ntp servers
+                dbusServer.setSystemNTPServers({"195.113.144.201", "tik.cesnet.cz", "2001:718:1:1::144:201"});
+
+                expected = {
+                    {"/enabled", "true"},
+                    {"/server[name='1']", ""},
+                    {"/server[name='1']/name", "1"},
+                    {"/server[name='1']/udp", ""},
+                    {"/server[name='1']/udp/address", "195.113.144.201"},
+                    {"/server[name='2']", ""},
+                    {"/server[name='2']/name", "2"},
+                    {"/server[name='2']/udp", ""},
+                    {"/server[name='2']/udp/address", "tik.cesnet.cz"},
+                    {"/server[name='3']", ""},
+                    {"/server[name='3']/name", "3"},
+                    {"/server[name='3']/udp", ""},
+                    {"/server[name='3']/udp/address", "2001:718:1:1::144:201"},
+                };
+            }
         }
 
         REQUIRE(dataFromSysrepo(client, "/ietf-system:system/ntp", sysrepo::Datastore::Operational) == expected);

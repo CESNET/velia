06b944168c8c02356c12ef26fe7e4340dc2ad10c
Tomáš Pecka <tomas.pecka@cesnet.cz>
2026-03-13 00:15:45 +0100

    utils: add a helper for formating host and port for systemd config files
    This is a helper function to format host and port in the form of
    "host:port" with respect to IPv6 addresses (if host is an IPv6
    address and port is specified, then it will be enclosed in square brackets).
    
    This is mainly a preparation for the next patch as we currently have
    code doing this in JournalUpload but we will need it in other places
    as well.
    
    Change-Id: Idfdb8df3c141a4b19224cb08b689ea9808162961

--- src/utils/libyang.h
+++ src/utils/libyang.h
@@ -30,4 +30,11 @@ std::string asString(const libyang::DataNode& node);
  * Throws if there is more than one matching node. Returns std::nullopt if no node matches.
  */
 std::optional<libyang::DataNode> getUniqueSubtree(const libyang::DataNode& start, const std::string& path);
+
+/** @brief Format inet:host and port with respect to IPv6 addresses for systemd config files
+ *
+ * If the host is an IPv6 address and portNode is provided, then the address will be enclosed in square brackets.
+ * If portNode is provided, it will be appended after a colon.
+ */
+std::string formatHostPort(const libyang::DataNode& node, const std::string& hostNode, const std::optional<std::string>& portNode);
 }

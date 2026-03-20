CESNET/velia

commit c3dd9edb7416960f777402359f0b6bf8b810793d

system: allow setting DNS servers
So far we only reported current DNS servers as reported by
systemd-resolved.

Now we subscribe to changes in the DNS resolver configuration and write
them to the systemd-resolved config file [1], which is then reloaded.
Startup config is written to a certain file directly.

Also narrow the YANG deviation from dns-resolver to dns-resolver/options,
so that the server list becomes writable (config true).

[1] https://www.freedesktop.org/software/systemd/man/latest/resolved.conf.html

Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/9159
Change-Id: Ie5fa4a569ceed22ba2c4c5965bed7ab656faf75b

src/system/IETFSystem.cpp
@@ -97,13 +97,17 @@ std::string createSystemdDropinConfig(const std::string& section, const std::str
     return oss.str();
 }
 
-std::vector<std::string> getNTPServersFromChange(const std::string& serverPath, const sysrepo::Session& session)
+std::vector<std::string> getNTPOrDNSServersFromChange(const std::string& serverPath, const sysrepo::Session& session)
 {
     std::vector<std::string> addresses;
 
     if (auto data = session.getData(serverPath)) {
+        // NTP port leaf is preesnt only if feature is enabled. However as of 2026-03, systemd-timesyncd does not support custom ports, so we do not enable it
+        auto serverPathChildren = session.getContext().findPath(serverPath).childInstantiables();
+        bool hasPortLeaf = std::find_if(serverPathChildren.begin(), serverPathChildren.end(), [](const auto& node) { return node.name() == "port"; }) != serverPathChildren.end();
+
         for (const auto& server : data->findXPath(serverPath)) {
-            const auto address = velia::utils::formatHostPort(server, "address", std::nullopt);
+            const auto address = velia::utils::formatHostPort(server, "address", hasPortLeaf ? std::make_optional("port") : std::nullopt);
             spdlog::trace("Adding server '{}' from {} {} data", address, serverPath, session.activeDatastore() == sysrepo::Datastore::Running ? "running" : "startup");
             addresses.emplace_back(std::move(address));
         }
@@ -113,7 +117,12 @@ std::vector<std::string> getNTPServersFromChange(const std::string& serverPath, 
 
 std::string createTimesyncdConfig(const sysrepo::Session& session)
 {
-    return createSystemdDropinConfig("Time", "NTP", getNTPServersFromChange(IETF_SYSTEM_NTP_PATH + "/server/udp"s, session));
+    return createSystemdDropinConfig("Time", "NTP", getNTPOrDNSServersFromChange(IETF_SYSTEM_NTP_PATH + "/server/udp"s, session));
+}
+
+std::string createResolvedConfig(const sysrepo::Session& session)
+{
+    return createSystemdDropinConfig("Resolve", "DNS", getNTPOrDNSServersFromChange(IETF_SYSTEM_DNS_PATH + "/server/udp-and-tcp"s, session));
 }
 
 struct AddressAndPort {
@@ -311,6 +320,25 @@ void IETFSystem::initDNS(sdbus::IConnection& connection, const SystemdConfigData
         return sysrepo::ErrorCode::Ok;
     };
 
+    sysrepo::ModuleChangeCb dnsRunning = [resolve] (auto session, auto, auto, auto, auto, auto) {
+        std::filesystem::create_directories(*resolve.dropinDir);
+        utils::safeWriteFile(*resolve.dropinDir / "resolved.conf", createResolvedConfig(session));
+        if (resolve.reload) {
+            resolve.reload();
+        }
+        return sysrepo::ErrorCode::Ok;
+    };
+
+    sysrepo::ModuleChangeCb dnsStartup = [] (auto session, auto, auto, auto, auto, auto) {
+        utils::safeWriteFile(BACKUP_ETC_SYSTEMD_RESOLVED_CONF_FILE, createResolvedConfig(session));
+        return sysrepo::ErrorCode::Ok;
+    };
+
+    m_srSession.switchDatastore(sysrepo::Datastore::Running);
+    m_srSubscribe->onModuleChange(IETF_SYSTEM_MODULE_NAME, dnsRunning, IETF_SYSTEM_DNS_PATH, 0, sysrepo::SubscribeOptions::DoneOnly | sysrepo::SubscribeOptions::Enabled);
+    m_srSession.switchDatastore(sysrepo::Datastore::Startup);
+    m_srSubscribe->onModuleChange(IETF_SYSTEM_MODULE_NAME, dnsStartup, IETF_SYSTEM_DNS_PATH, 0, sysrepo::SubscribeOptions::DoneOnly);
+    m_srSession.switchDatastore(sysrepo::Datastore::Operational);
     m_srSubscribe->onOperGet(IETF_SYSTEM_MODULE_NAME, dnsOper, IETF_SYSTEM_DNS_PATH);
 }
 

CMakeLists.txt
@@ -248,6 +248,11 @@ if(NOT VELIA_BACKUP_ETC_SYSTEMD_TIMESYNCD_CONF)
     message(FATAL_ERROR "VELIA_BACKUP_ETC_SYSTEMD_TIMESYNCD_CONF not set")
 endif()
 
+set(VELIA_BACKUP_ETC_SYSTEMD_RESOLVED_CONF "/etc/systemd/resolved.conf" CACHE STRING "Path to resolved.conf file")
+if(NOT VELIA_BACKUP_ETC_SYSTEMD_RESOLVED_CONF)
+    message(FATAL_ERROR "VELIA_BACKUP_ETC_SYSTEMD_RESOLVED_CONF not set")
+endif()
+
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/system/system_vars.h.in ${CMAKE_CURRENT_BINARY_DIR}/system_vars.h @ONLY)
 include_directories(${CMAKE_CURRENT_BINARY_DIR})
 

src/main-system.cpp
@@ -82,6 +82,11 @@ int main(int argc, char* argv[])
                                                        *g_dbusConnection,
                                                        IETFSystem::SystemdConfigData{
                                                            .busName = "org.freedesktop.resolve1",
+                                                           .dropinDir = "/run/systemd/resolved.conf.d",
+                                                           .reload = []() {
+                                                               spdlog::get("system")->debug("Reloading systemd-resolved.service");
+                                                               velia::utils::execAndWait(spdlog::get("system"), SYSTEMCTL_EXECUTABLE, {"reload", "systemd-resolved.service"}, "", {});
+                                                           },
                                                        },
                                                        IETFSystem::SystemdConfigData{
                                                            .busName = "org.freedesktop.timesync1",

src/system/system_vars.h.in
@@ -7,6 +7,7 @@
 #define BACKUP_ETC_SHADOW_FILE "@VELIA_BACKUP_ETC_SHADOW@"
 #define BACKUP_ETC_HOSTNAME_FILE "@VELIA_BACKUP_ETC_HOSTNAME@"
 #define BACKUP_ETC_SYSTEMD_TIMESYNCD_CONF_FILE "@VELIA_BACKUP_ETC_SYSTEMD_TIMESYNCD_CONF@"
+#define BACKUP_ETC_SYSTEMD_RESOLVED_CONF_FILE "@VELIA_BACKUP_ETC_SYSTEMD_RESOLVED_CONF@"
 #define SSH_KEYGEN_EXECUTABLE "@SSH_KEYGEN_EXECUTABLE@"
 #define CHPASSWD_EXECUTABLE "@CHPASSWD_EXECUTABLE@"
 #define SYSTEMCTL_EXECUTABLE "@SYSTEMCTL_EXECUTABLE@"

tests/sysrepo_system-ietfsystem.cpp
@@ -14,6 +14,7 @@ using namespace std::literals;
 
 struct SystemctlMock {
     MAKE_CONST_MOCK0(reloadTimesync, void());
+    MAKE_CONST_MOCK0(reloadResolve, void());
 };
 
 TEST_CASE("Sysrepo ietf-system")
@@ -35,6 +36,8 @@ TEST_CASE("Sysrepo ietf-system")
 
     velia::system::IETFSystem::SystemdConfigData resolve{
         .busName = dbusConnServer->getUniqueName(),
+        .dropinDir = CMAKE_CURRENT_BINARY_DIR "tests/resolved.conf.d",
+        .reload = [&systemctlMock] { systemctlMock.reloadResolve(); },
     };
     velia::system::IETFSystem::SystemdConfigData timesync{
         .busName = dbusConnServer->getUniqueName(),
@@ -85,6 +88,7 @@ TEST_CASE("Sysrepo ietf-system")
                 };
             }
 
+            REQUIRE_CALL(systemctlMock, reloadResolve()).IN_SEQUENCE(seq1);
             REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
             auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                        osReleaseFile,
@@ -113,6 +117,7 @@ TEST_CASE("Sysrepo ietf-system")
 
     SECTION("dummy values")
     {
+        REQUIRE_CALL(systemctlMock, reloadResolve()).IN_SEQUENCE(seq1);
         REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
         auto sys = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",
@@ -145,6 +150,7 @@ TEST_CASE("Sysrepo ietf-system")
         // in its own scope, otherwise the tests below this will fail with something like:
         // Couldn't create RPC/action subscription: RPC subscription for "/ietf-system:system-restart" with priority 0 already exists.
         {
+            REQUIRE_CALL(systemctlMock, reloadResolve()).IN_SEQUENCE(seq1);
             REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
             auto sys = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",
@@ -199,6 +205,7 @@ TEST_CASE("Sysrepo ietf-system")
 
     SECTION("DNS resolvers")
     {
+        REQUIRE_CALL(systemctlMock, reloadResolve()).IN_SEQUENCE(seq1);
         REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
         auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",
@@ -265,12 +272,86 @@ TEST_CASE("Sysrepo ietf-system")
             };
         }
 
+        SECTION("Writing config")
+        {
+            SECTION("No DNS servers")
+            {
+                REQUIRE(velia::utils::readFileToString(*resolve.dropinDir / "resolved.conf") == R"(# Autogenerated by velia. Do not edit.
+[Resolve]
+DNS=
+)");
+                // fallback servers
+                expected = {
+                    {"/options", ""},
+                    {"/server[name='1']", ""},
+                    {"/server[name='1']/name", "1"},
+                    {"/server[name='1']/udp-and-tcp", ""},
+                    {"/server[name='1']/udp-and-tcp/address", "8.8.8.8"},
+                    {"/server[name='2']", ""},
+                    {"/server[name='2']/name", "2"},
+                    {"/server[name='2']/udp-and-tcp", ""},
+                    {"/server[name='2']/udp-and-tcp/address", "8.8.4.4"},
+                    {"/server[name='3']", ""},
+                    {"/server[name='3']/name", "3"},
+                    {"/server[name='3']/udp-and-tcp", ""},
+                    {"/server[name='3']/udp-and-tcp/address", "2001:4860:4860::8888"},
+                };
+            }
+
+            SECTION("Some DNS servers")
+            {
+                srSess.switchDatastore(sysrepo::Datastore::Running);
+                srSess.setItem("/ietf-system:system/dns-resolver/server[name='dns4_1']/udp-and-tcp/address", "8.8.8.8");
+                srSess.setItem("/ietf-system:system/dns-resolver/server[name='dns4_2']/udp-and-tcp/address", "8.8.8.8");
+                srSess.setItem("/ietf-system:system/dns-resolver/server[name='dns4_2']/udp-and-tcp/port", "123");
+                srSess.setItem("/ietf-system:system/dns-resolver/server[name='dns6_1']/udp-and-tcp/address", "2606:4700:4700::1111");
+                srSess.setItem("/ietf-system:system/dns-resolver/server[name='dns6_2']/udp-and-tcp/address", "2606:4700:4700::1111");
+                srSess.setItem("/ietf-system:system/dns-resolver/server[name='dns6_2']/udp-and-tcp/port", "123");
+
+                REQUIRE_CALL(systemctlMock, reloadResolve()).IN_SEQUENCE(seq1);
+                srSess.applyChanges();
+                REQUIRE(velia::utils::readFileToString(*resolve.dropinDir / "resolved.conf") == R"(# Autogenerated by velia. Do not edit.
+[Resolve]
+DNS=8.8.8.8:53 8.8.8.8:123 [2606:4700:4700::1111]:53 [2606:4700:4700::1111]:123
+)");
+                dbusServer.setDNSEx({
+                    {0, AF_INET, {8, 8, 8, 8}, 0, "czech.light"},
+                    {0, AF_INET, {8, 8, 8, 8}, 123, "czech.light"},
+                    {2, AF_INET6, {0x26, 0x06, 0x47, 0x00, 0x47, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11, 0x11}, 0, "czech.light"},
+                    {2, AF_INET6, {0x26, 0x06, 0x47, 0x00, 0x47, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11, 0x11}, 123, "czech.light"},
+                });
+
+                expected = {
+                    {"/options", ""},
+                    {"/server[name='1']", ""},
+                    {"/server[name='1']/name", "1"},
+                    {"/server[name='1']/udp-and-tcp", ""},
+                    {"/server[name='1']/udp-and-tcp/address", "8.8.8.8"},
+                    {"/server[name='2']", ""},
+                    {"/server[name='2']/name", "2"},
+                    {"/server[name='2']/udp-and-tcp", ""},
+                    {"/server[name='2']/udp-and-tcp/address", "8.8.8.8"},
+                    {"/server[name='2']/udp-and-tcp/port", "123"},
+                    {"/server[name='3']", ""},
+                    {"/server[name='3']/name", "3"},
+                    {"/server[name='3']/udp-and-tcp", ""},
+                    {"/server[name='3']/udp-and-tcp/address", "2606:4700:4700::1111"},
+                    {"/server[name='4']", ""},
+                    {"/server[name='4']/name", "4"},
+                    {"/server[name='4']/udp-and-tcp", ""},
+                    {"/server[name='4']/udp-and-tcp/address", "2606:4700:4700::1111"},
+                    {"/server[name='4']/udp-and-tcp/port", "123"},
+                };
+            }
+        }
+
         REQUIRE(dataFromSysrepo(client, "/ietf-system:system/dns-resolver", sysrepo::Datastore::Operational) == expected);
     }
 
     SECTION("NTP")
     {
         std::map<std::string, std::string> expected;
+        REQUIRE_CALL(systemctlMock, reloadResolve()).IN_SEQUENCE(seq1);
         REQUIRE_CALL(systemctlMock, reloadTimesync()).IN_SEQUENCE(seq1);
         auto sysrepo = std::make_shared<velia::system::IETFSystem>(srSess,
                                                                    CMAKE_CURRENT_SOURCE_DIR "/tests/system/os-release",

yang/czechlight-system@2022-07-08.yang
@@ -308,7 +308,7 @@ module czechlight-system {
     deviate not-supported;
   }
 
-  deviation /sys:system/sys:dns-resolver {
+  deviation /sys:system/sys:dns-resolver/sys:options {
     deviate add {
       config false;
     }

commit 1f631b15062f52df5df6e054793a2bddc730df20

fix comment typo
Fixes: c3dd9edb ("system: allow setting DNS servers")
Change-Id: I970572327af5572c0456df40c336ff0f1733cfb5

src/system/IETFSystem.cpp
@@ -102,7 +102,7 @@ std::vector<std::string> getNTPOrDNSServersFromChange(const std::string& serverP
     std::vector<std::string> addresses;
 
     if (auto data = session.getData(serverPath)) {
-        // NTP port leaf is preesnt only if feature is enabled. However as of 2026-03, systemd-timesyncd does not support custom ports, so we do not enable it
+        // NTP port leaf is present only if feature is enabled. However as of 2026-03, systemd-timesyncd does not support custom ports, so we do not enable it
         auto serverPathChildren = session.getContext().findPath(serverPath).childInstantiables();
         bool hasPortLeaf = std::find_if(serverPathChildren.begin(), serverPathChildren.end(), [](const auto& node) { return node.name() == "port"; }) != serverPathChildren.end();
 